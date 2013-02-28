0e9e79087d06de5652bc756090eb2e9e44071d31
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2013-02-28 09:11:28 +0000

    Services view is now sortable
    git-svn-id: https://xbox/svnroot/Qorus/trunk@3176 e9028eec-4831-4325-a917-4dc279fcb47c

--- js/views/services/services.js
+++ js/views/services/services.js
@@ -1,25 +1,18 @@
 define([
   'jquery',
   'underscore',
-  'backbone',
+  'qorus/qorus',
   // Pull in the Collection module from above
   'collections/services',
   'text!/templates/service/list.html'
-], function($, _, Backbone, ServiceCollection, servicesTemplate){
-  var ServiceListView = Backbone.View.extend({
+], function($, _, Qorus, Collection, Template){
+  var ListView = Qorus.ListView.extend({
     el: $("#content"),
     initialize: function(){
-      this.collection = new ServiceCollection();
-      // Compile the template using Underscores micro-templating
-	  var view = this;
-	  this.collection.fetch({
-		  success: function(collection){
-		      var compiledTemplate = _.template( servicesTemplate, { services: collection.models } );
-		      view.$el.html(compiledTemplate);
-		  }
-	  });
+        this.template = Template;
+        ListView.__super__.initialize.call(this, Collection);
     }
   });
   // Returning instantiated views can be quite useful for having "state"
-  return ServiceListView;
+  return ListView;
 });
