67cb6afe135365450ec8cb72871648a2f81dd82c
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2013-03-06 14:59:54 +0000

    Design tweaks
    git-svn-id: https://xbox/svnroot/Qorus/trunk@3204 e9028eec-4831-4325-a917-4dc279fcb47c

--- js/settings.js
+++ js/settings.js
@@ -7,7 +7,7 @@ define([
   var settings = {
       DATE_FORMAT: 'YYYY-MM-DD HH:mm:ss.SSS ddd ZZ',
       DATE_DISPLAY: 'YYYY-MM-DD HH:mm:ss',    
-      DATE_FROM: '1980-01-01'
+      DATE_FROM: '1970-01-01'
   }
   return settings;
 });
