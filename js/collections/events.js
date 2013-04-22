1418c109544eb85677efa0c6242f71d9f7c003a2
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2013-04-22 12:41:40 +0000

    Updated connection messages
    git-svn-id: https://xbox/svnroot/Qorus/trunk@3413 e9028eec-4831-4325-a917-4dc279fcb47c

--- js/collections/events.js
+++ js/collections/events.js
@@ -7,17 +7,24 @@ define([
   var Collection = Qorus.SortedCollection.extend({
     model: Model,
     log_size: 1000,
+    counter: 0,
     initialize: function (opts) {
       _.bindAll(this, 'wsAdd', 'wsRetry', 'wsOpen', 'wsOpened');
       this.sort_key = 'time';
       this.sort_order = 'des';
       this.sort_history = [''];
       this.wsOpen();
-      this.on('ws-opened', function () { 
-        $.globalMessenger().post({ message: "Connected to Qorus instance!", type: "info", hideAfter: 10, id: 'ws-connection' }); 
+      this.on('ws-opened', function (col) { 
+        var msg = "Connected to Qorus instance";
+        
+        if(col.counter > 1){
+          msg = 'We are back!';
+        }
+        
+        $.globalMessenger().post({ message: msg, type: "success", hideAfter: 10, id: 'ws-connection' }); 
       });
       this.on('ws-closed', function () { 
-        $.globalMessenger().post({ message: "Connection Failed", type: "error", id: 'ws-connection' }); 
+        $.globalMessenger().post({ message: "Qorus instance is down!", type: "error", id: 'ws-connection' }); 
       });
       Qorus.SortedCollection.__super__.initialize.call(this, opts);
     },
@@ -40,11 +47,11 @@ define([
     wsOpen: function () {
       this.socket = new WebSocket("ws://localhost:8001");
       this.socket.onmessage = this.wsAdd;
-      this.socket.on
       this.socket.onclose = this.wsRetry;
       this.socket.onopen = this.wsOpened;
     },
     wsOpened: function () {
+      this.counter++;
       this.trigger('ws-opened', this); 
     },
     wsRetry: function () {
