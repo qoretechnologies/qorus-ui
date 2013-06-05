d3b6da33f7622e99cd53ec7b8b59dc54dfb7d5f9
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2013-06-05 15:03:01 +0000

    Added services toolbar
    git-svn-id: https://xbox/svnroot/Qorus/trunk@3655 e9028eec-4831-4325-a917-4dc279fcb47c

---
+++ js/views/toolbars/services_toolbar.js
@@ -0,0 +1,60 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'qorus/qorus',
+  'text!../../../templates/service/toolbars/service_toolbar.html',
+  'datepicker',
+  'moment',
+  'bootstrap.multiselect'
+], function($, _, Backbone, Qorus, Template, date, moment){
+  var Toolbar = Qorus.View.extend({
+    context: {
+    },
+    
+    events: {
+      // "click button.date": "setDate",
+      // "click button[data-action='open']": "navigateTo"
+    },
+    
+    initialize: function (opts) {
+      _.bindAll(this);
+      Toolbar.__super__.initialize.call(this, opts);
+      
+      this.context.date = this.options.date;
+      this.template = Template;
+    },
+    
+    onRender: function () {
+      this.datePicker();
+      $('.sticky').sticky();
+      $('[data-toggle="tooltip"]').tooltip({ container: 'body' });
+    },
+    
+    // filter by date init
+    datePicker: function () {
+      var view = this;
+      this.dp = $('.dp').datetimepicker({
+          format: 'yyyy-mm-dd hh:mm:ss'
+      });
+      this.dp.on('changeDate', function (e) {
+        view.onDateChanged(e.date.toISOString(), {});
+      });
+    },
+    
+    onDateChanged: function (date) {
+      $('.datetimepicker').remove();
+      this.dp.remove();
+      Backbone.history.navigate('/workflows/' + moment(date).utc()
+          .format('YYYY-MM-DD HH:mm:ss'), {trigger: true});
+    },
+    
+    navigateTo: function (e) {
+      var el = $(e.currentTarget);
+      if (el.data('url')) {
+        Backbone.history.navigate(el.data('url'), {trigger: true});       
+      }
+    }
+  });
+  return Toolbar;
+});
