18460adc32dc87eced6885487b39718bbdbea774
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2013-06-07 14:20:24 +0000

    Fixed search view
    git-svn-id: https://xbox/svnroot/Qorus/trunk@3670 e9028eec-4831-4325-a917-4dc279fcb47c

--- js/views/workflows/search.js
+++ js/views/workflows/search.js
@@ -4,16 +4,27 @@ define([
   'qorus/qorus',
   'settings',
   'models/workflow',
+  'collections/orders',
   'text!../../../templates/search/detail.html',
+  'text!../../../templates/workflow/orders/table.html',
+  'text!../../../templates/workflow/orders/row.html',
   'views/workflows/instances',
   'views/workflows/orders',
   'views/common/bottom_bar',
   'views/toolbars/search_toolbar',
   'views/workflows/order'
-], function ($, _, Qorus, settings, Workflow, Template, InstanceListView, 
+], function ($, _, Qorus, settings, Workflow, Collection, Template, TableTpl, RowTpl, InstanceListView, 
   OrderListView, BottomBarView, OrdersToolbar, OrderView) {
+  var context = {
+    action_css: {
+      'block': 'btn-inverse',
+      'cancel': 'btn-danger',
+      'retry': 'btn-success'
+    }
+  };
     
-  var ModelView = Qorus.View.extend({
+  var View = Qorus.ListView.extend({
+    context: context,
     url: function () {
      return '/search'; 
     },
@@ -34,15 +45,21 @@ define([
       'submit .form-search': 'search',
       // 'keyup .search-query': 'search'
     },
+    
     initialize: function (opts) {
       this.opts = opts || {};
       this.opts.date = this.opts.date || settings.DATE_FROM;
       
       _.bindAll(this, 'render');
       
+      this.collection = new Collection({ date: this.opts.date, search: this.opts.search });
       this.template = Template;
+      this.collection.on('sync', this.updateContext, this);
+      this.collection.fetch();
       this.createSubviews();
       
+      _.extend(this.options, this.opts);
+      _.extend(this.context, this.opts);
       _.defer(this.render);
     },
     
@@ -51,7 +68,7 @@ define([
       if (ctx){
         _.extend(mctx, ctx);
       }
-      ModelView.__super__.render.call(this, mctx);
+      View.__super__.render.call(this, mctx);
       
       this.setTitle();
       this.onRender();
@@ -59,28 +76,64 @@ define([
     },
     
     onRender: function () {
-      // render instance/order data grid with toolbar
-      var dataview = this.currentDataView();
-      var toolbar = this.subviews.toolbar;
-
-      // // update toolbar url with current order/instance url
-      // toolbar.updateUrl(dataview.url, dataview.options.statuses);
-
-      this.assign('#instances', dataview);
-      this.assign('#toolbar', toolbar);
+      this.assign('#instances', this.subviews.table);
+      this.assign('#toolbar', this.subviews.toolbar);
       this.assign('#bottom-bar', this.subviews.bottombar);
     },
     
-    currentDataView: function () {
-      return this.subviews.orders;
+    runAction: function (e) {
+      e.stopPropagation();
+      var data = e.currentTarget.dataset;
+      if (data.id && data.action) {
+        var inst = this.collection.get(data.id);
+        inst.doAction(data.action); 
+      }
     },
+    
+    nextPage: function () {
+      this.collection.loadNextPage();
+    },
+    
+    updateContext: function () {
+      // update actual pages
+      this.context.page = {
+        current_page: this.collection.page,
+        has_next: this.collection.hasNextPage()
+      };
+      this.render();
+    },
+
+    // fetches the collection from server presorted by key
+    fetchSorted: function (e) {
+      var el = e.currentTarget;
+      // var sort = el.data('sort');
+      // console.log("Fetching sorted", sort);
+      e.stopPropagation();
+    },
+    
+    scroll: function () {
+      var pos = this.$el.height() + this.$el.offset().top - $(window).height();
+      if (pos < 100) {
+        this.nextPage(); 
+        this.$el.children('button[data-pagination]').html("Loading...");
+      }
+    },    
     
     createSubviews: function () {
       // this.subviews.instances = new InstanceListView({ 
       //     date: this.opts.date, workflowid: this.model.id, url: this.url() 
       //   });
 
-      this.subviews.orders = new OrderListView({ date: this.opts.date, search: this.opts.search });
+      // this.subviews.orders = new OrderListView({ date: this.opts.date, search: this.opts.search });
+      
+      this.subviews.table = new Qorus.TableView({ 
+          collection: this.collection, 
+          template: TableTpl,
+          row_template: RowTpl,
+          helpers: this.helpers,
+          context: { url: this.url },
+          dispatcher: Dispatcher
+      });
       this.subviews.bottombar = new BottomBarView({});
       this.subviews.toolbar = new OrdersToolbar(this.opts);
     },
@@ -126,7 +179,12 @@ define([
       var ids = $target.hasClass('.search-query-ids') ? $target.val() : $target.find('.search-query-ids').val();
       var keyvalues = $target.hasClass('.search-query-keyvalues') ? $target.val() : $target.find('.search-query-keyvalues').val();
       Backbone.history.navigate([this.url(), ids, keyvalues].join("/"), { trigger: true });
+    },
+    
+    helpers: {
+      action_css: context.action_css
     }
+    
   });
-  return ModelView;
+  return View;
 });
