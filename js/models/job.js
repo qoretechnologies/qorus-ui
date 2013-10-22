ba1fb0eff41522b57e952baa6ee3893dc6f65ae2
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2013-10-22 12:28:28 +0000

    Fixed job setActive action
    git-svn-id: https://xbox/svnroot/Qorus/trunk@4220 e9028eec-4831-4325-a917-4dc279fcb47c

--- js/models/job.js
+++ js/models/job.js
@@ -14,7 +14,7 @@ define([
     idAttribute: "jobid",
     urlRoot: settings.REST_API_PREFIX + '/jobs/',
     dateAttributes: ['last_executed', 'next'],
-    allowed_actions: ['run', 'reset', 'set-expire', 'schedule', 'expiry_date'],
+    allowed_actions: ['run', 'reset', 'set-expire', 'schedule', 'expiry_date', 'setActive'],
 
 
     initialize: function (options) {
@@ -24,10 +24,10 @@ define([
     doAction: function(action, opts){
       debug.log('doing action', action);
       if(_.indexOf(this.allowed_actions, action) != -1){
-        var id = this.id;
-        var _this = this;
+        var params = opts || {};
+        _.extend(opts, {'action': action });
         debug.log(this.url());
-        $.put(this.url(), {'action': action });     
+        $.put(this.url(), params);
       }
     }
   });
