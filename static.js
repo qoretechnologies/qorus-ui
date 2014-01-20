63389d53234c612637c8b0976158912c8d7fabad
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2014-01-20 11:55:54 +0000

    Added node-static file serving setup
    git-svn-id: https://xbox/svnroot/Qorus/trunk@4573 e9028eec-4831-4325-a917-4dc279fcb47c

---
+++ static.js
@@ -0,0 +1,14 @@
+var static = require('node-static');
+var sys = require('sys');
+
+var fileServer = new static.Server('.');
+
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    fileServer.serve(request, response, function (e, res) {
+        if (e && (e.status === 404)) { // If the file wasn't found
+            fileServer.serveFile('/index.html', 200, {}, request, response);
+        }
+    });
+  }).resume();
+}).listen(8080);
