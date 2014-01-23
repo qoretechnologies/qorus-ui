745597ef93c50e5e3b939f4109c38a23bc25762b
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2014-01-23 19:46:16 +0000

    Added force revalidate
    git-svn-id: https://xbox/svnroot/Qorus/trunk@4584 e9028eec-4831-4325-a917-4dc279fcb47c

--- static.js
+++ static.js
@@ -1,14 +1,19 @@
-var nstatic = require('node-static');
-// var sys = require('sys');
+var node_static = require('node-static');
 
-var fileServer = new nstatic.Server('.', { cache: false });
+//
+// Create a node-static server to serve the current directory
+//
+var file = new node_static.Server('.', { cache: false, headers: {"Cache-Control": "no-cache, must-revalidate"} });
 
 require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    fileServer.serve(request, response, function (e, res) {
-        if (e && (e.status === 404)) { // If the file wasn't found
-            fileServer.serveFile('/index.html', 200, {}, request, response);
+    file.serve(request, response, function (err, res) {
+        if (err) { // An error as occured
+          console.error("> Error serving " + request.url + " - " + err.message);
+          file.serveFile('/index.html', 200, {}, request, response);
+        } else { // The file was served successfully
+            console.log("> " + request.url + " - " + res.message);
         }
     });
-  }).resume();
 }).listen(8080);
+
+console.log("> node-static is listening on http://127.0.0.1:8080");
