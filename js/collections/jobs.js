ba91246e48ba648c49c97955e47bbee8c9038739
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2014-01-31 16:36:28 +0000

    Changed jobs ordering and add force break to alert reason
    git-svn-id: https://xbox/svnroot/Qorus/trunk@4624 e9028eec-4831-4325-a917-4dc279fcb47c

--- js/collections/jobs.js
+++ js/collections/jobs.js
@@ -8,7 +8,14 @@ define([
   var Collection = Qorus.SortedCollection.extend({
     url: settings.REST_API_PREFIX + '/jobs/',
     model: Model,
-    pagination: false
+    pagination: false,
+    
+    initialize: function (opts) {
+      this.sort_key = 'active';
+      this.sort_order = 'des';
+      this.sort_history = ['name',];
+      this.opts = {};
+    }
   });
   // You don't usually return a collection instantiated
   return Collection;
