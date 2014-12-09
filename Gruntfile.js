3fb5ba3ed8e025c93560947ac13da8d4923a7f95
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2014-12-09 17:37:24 +0000

    Added jshint check
    git-svn-id: https://xbox/svnroot/Qorus/trunk@5576 e9028eec-4831-4325-a917-4dc279fcb47c

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,36 @@ module.exports = function (grunt) {
             dir: "dist"
           }
         }
+      },
+      jshint: {
+        files: ['Gruntfile.js', 'src/js/**/*.js', '!src/js/libs/**', '!src/js/app.build.js'],
+        options: {
+          "predef": {
+              "$": true,  
+              "define": true, 
+              "require": true,
+              "jQuery": true, 
+              "later": true,
+              "cronParser": true,
+              "moment": true,
+              "debug": true,
+              "sprintf": true,
+              "Backbone": true,
+              "window": true
+          },
+          "strict": false,
+          "unused": false,
+          "browser": true,
+          "devel": true,
+          "indent": 2,
+          "white": false,
+          "curly": false
+        }
       }
     });
   
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   
-  grunt.registerTask('default', ['requirejs']);
+  grunt.registerTask('default', ['jshint', 'requirejs']);
 };
