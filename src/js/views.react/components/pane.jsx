d9ee869e472c2e705baf0a616fea92a5d07ec040
pchalupny <pchalupny@e9028eec-4831-4325-a917-4dc279fcb47c>
2015-06-26 11:54:50 +0000

    Pane component now accept children not just children as attribute
    git-svn-id: https://xbox/svnroot/Qorus/trunk@6201 e9028eec-4831-4325-a917-4dc279fcb47c

--- src/js/views.react/components/pane.jsx
+++ src/js/views.react/components/pane.jsx
@@ -6,13 +6,13 @@ define(function (require, exports, module) {
       PaneView;
 
   require('jquery-ui');
-  
+
   PaneView = React.createClass({
     getStorageKey: function () {
       var cvkey = this.props.name || 'pane';
       return [module.id.replace(/\//g, '.'), cvkey].join('.');
     },
-  
+
     componentDidMount: function () {
       var $el = this._resizable = $(this.refs.pageSlide.getDOMNode());
       var key = this.getStorageKey();
@@ -31,29 +31,34 @@ define(function (require, exports, module) {
         }
       });
     },
-    
+
     componentWillUnmount: function () {
       if (this._resizable) {
         this._resizable.resizable('destroy');
       }
     },
-  
+
     render: function () {
-      var ContentView = this.props.contentView;
-      var props = _.omit(this.props, ['contentView']);
+      var props = _.omit(this.props, ['contentView', 'children']);
       var style = {
         width: SystemSettings.get(this.getStorageKey()) || defaultWidth
       };
-      
+
+      if (this.props.contentView) {
+        children = <this.props.contentView {...props} />;
+      } else {
+        children = this.props.children;
+      }
+
       return <div className="pageslide left show" ref="pageSlide" style={style}>
                 <a href="#" className="btn btn-small btn-inverse close-view" onClick={this.props.onClose}><i className="icon-remove-sign"></i> Close</a>
                 <div className="content">
-                  <ContentView {...props} />
-                </div>  
+                  { children }
+                </div>
               </div>
       ;
     }
   });
-  
+
   return PaneView;
 });
