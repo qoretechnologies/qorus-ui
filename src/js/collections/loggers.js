ea45c85afbd263bc79538dd67be34a5c938f547b
Petr Chalupny <pchalupny@roura.prague.qoretechnologies.com>
2015-07-29 15:37:55 +0200

    Fixed orders view Loading due collection API change

--- src/js/collections/loggers.js
+++ src/js/collections/loggers.js
@@ -1,18 +1,20 @@
 define(function(require) {
   var settings = require('settings'),
-      Qorus    = require('qorus/qorus'), 
+      Qorus    = require('qorus/qorus'),
       Collection;
-      
+
   Collection = Qorus.SortedCollection.extend({
     url: function() {
       return settings.REST_API_PREFIX + '/logs/';
     },
     initialize: function (models, opts){
     this.opts = opts;
-      
+
       Collection.__super__.initialize.apply(this, arguments);
     }
   });
-  
-  return new Collection().fetch();
+
+  collection =  new Collection();
+  collection.fetch();
+  return collection;
 });
