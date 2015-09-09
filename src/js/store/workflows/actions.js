82f685b675b41c72eca09b34e6d6a4b8c14bb94b
Petr Chalupny <temnoregg@gmail.com>
2015-09-09 22:15:06 +0200

    Removed previous workflows api implementations

--- src/js/store/workflows/actions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-actions';
-import qorusApi from '../../lib/qorus-api';
-
-export const fetch = createAction('FETCH_WORKFLOWS', () => {
-  return qorusApi.workflows.getAll().then(res=> res.body());
-});
