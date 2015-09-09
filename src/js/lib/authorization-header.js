c6aa8a93bdcd550d58fe00a69429460342e03cde
Petr Chalupny <temnoregg@gmail.com>
2015-09-09 22:14:24 +0200

    Moved qorusApi to separate directory

--- src/js/lib/authorization-header.js
+++
@@ -1,8 +0,0 @@
-function make_base_auth(user, password) {
-  var tok = user + ':' + password;
-  var hash = btoa(tok);
-  return "Basic " + hash;
-}
-
-
-export default make_base_auth('admin', 'admin');
--- src/js/qorus/index.js
+++ src/js/qorus/index.js
@@ -1,32 +1,3 @@
-var models = [
-  'alerts',
-  'class',
-  'constants',
-  'errors',
-  'event',
-  'function',
-  'group',
-  'health',
-  'instance',
-  'jobs',
-  'mapper',
-  'notification',
-  'option',
-  'order',
-  'permission',
-  'remote',
-  'result',
-  'roles',
-  'services',
-  'settings',
-  'sqlcache',
-  'step',
-  'system',
-  'users',
-  'valueset',
-  'workflows'
-];
-
 import settings from '../settings';
 
 const url = settings.REST_API_PREFIX;
@@ -39,11 +10,22 @@ export default reduxApi({
   // simple edpoint description
   workflows: {
     url: `${url}/workflows`,
+		transformer: transformers.array
   },
   workflow: {
     url: `${url}/workflows/:id`,
   },
   systemInfo: {
     url: `${url}/system`
+  },
+  users: {
+    url: `${url}/users`,
+		transformer: transformers.array
+  },
+  user: {
+    url: `${url}/users/:id`
+  },
+  currentUser: {
+    url: `${url}/users/?action=current`
   }
  }, adapterFetch(fetch)); // it's nessasary to point using rest backend
