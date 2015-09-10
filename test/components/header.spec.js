9aab006e7cdab73206b6257eb3b9a13ca686adcd
Petr Chalupny <temnoregg@gmail.com>
2015-09-10 21:31:52 +0200

    Fixed Header test

--- test/components/header.spec.js
+++ test/components/header.spec.js
@@ -15,7 +15,7 @@ describe('Testing Header Component', function () {
 
 
     const myDiv = TestUtils.renderIntoDocument(
-      <Header info={{ 'instance-key': 'test-1' }} />
+      <Header info={{ 'instance-key': 'test-1' }} currentUser={{ username: 'panda' }} />
     );
 
     const divText = TestUtils.findRenderedDOMComponentWithTag(myDiv, 'h2');
