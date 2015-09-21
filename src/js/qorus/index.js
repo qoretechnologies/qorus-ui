1712d0d87f05107393b67e9fb7db22009f900661
temnoregg <temnoregg@gmail.com>
2015-09-21 09:38:18 +0200

    dunno

--- src/js/qorus/index.js
+++ src/js/qorus/index.js
@@ -5,6 +5,7 @@ const url = settings.REST_API_PREFIX;
 import 'whatwg-fetch';
 import reduxApi, {transformers} from 'redux-api';
 import adapterFetch from 'redux-api/lib/adapters/fetch';
+import workflowsApi from './workflows';
 // import { normalize, Schema, arrayOf } from 'normalizr';
 
 // function makeBaseAuth(user, pass) {
@@ -31,51 +32,7 @@ import adapterFetch from 'redux-api/lib/adapters/fetch';
 //   return resp;
 // }
 
-const ORDER_STATES = [
-  { name: 'IN-PROGRESS', short: 'I'},
-  { name: 'READY', short: 'Y'},
-  { name: 'SCHEDULED', short: 'S'},
-  { name: 'COMPLETE', short: 'C'},
-  { name: 'INCOMPLETE', short: 'N'},
-  { name: 'ERROR', short: 'E'},
-  { name: 'CANCELED', short: 'Y'},
-  { name: 'RETRY', short: 'R'},
-  { name: 'WAITING', short: 'W'},
-  { name: 'ASYNC-WAITING', short: 'A'},
-  { name: 'EVENT-WAITING', short: 'V'},
-  { name: 'BLOCKED', short: 'B'},
-  { name: 'CRASH', short: 'C'}
-];
-
-let DEFAULTS = {
-  TOTAL: 0
-};
-
-ORDER_STATES.forEach((val) => { DEFAULTS[val.name] = 0; });
-
-export default reduxApi({
-  // simple edpoint description
-  workflows: {
-    url: `${url}/workflows`,
-    transformer: (data) => {
-      if (!data) return [];
-
-      const resp = data.map((item) => {
-        if (!item.id) {
-          item.id = item.workflowid;
-          delete item.workflowid;
-        }
-        return Object.assign({}, DEFAULTS, item);
-      });
-      return resp;
-    }
-    // options: options
-    // transformer: processSchema
-  },
-  workflow: {
-    url: `${url}/workflows/:id`
-    // options: options
-  },
+const qorusApi = {
   systemInfo: {
     url: `${url}/system`
     // options: options
@@ -91,4 +48,14 @@ export default reduxApi({
     url: `${url}/users/?action=current`
     // options: options
   }
-}, adapterFetch(fetch)); // it's nessasary to point using rest backend
+};
+
+
+export default reduxApi(
+  Object.assign(
+    {},
+    qorusApi,
+    workflowsApi
+  ),
+  adapterFetch(fetch)
+); // it's nessasary to point using rest backend
