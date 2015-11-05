49d6718c86e8772b0df8b183f1e8a6a33eaadec9
Filip Zrust <filip.zrust@sinsir.net>
2015-11-05 18:50:18 +0100

    New GroupView component

---
+++ src/js/components/groups.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react';
+import clNs from 'classnames';
+import { pureRender } from './utils';
+
+@pureRender
+class GroupItem extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
+    url: PropTypes.string.isRequired,
+    enabled: PropTypes.bool.isRequired
+  }
+
+  render() {
+    const { name, size, url, enabled } = this.props;
+    var cls = clNs({
+      label: true,
+      'label-info': enabled
+    });
+
+    return (
+      <a href={ url } >
+        <span className={ cls }>{ name } <small>({ size })</small></span>
+      </a>
+    );
+  }
+}
+
+@pureRender
+class GroupsView extends Component {
+  static propTypes = {
+    groups: PropTypes.arrayOf(PropTypes.shape(GroupItem.propTypes)).isRequired
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Groups</h4>
+        {
+          this.props.groups.map(g => (
+            <GroupItem key={ g.name } name={ g.name } size={ g.size } url={ g.url } enabled={ g.enabled } />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default GroupsView;
---
+++ test/components/groups.spec.js
@@ -0,0 +1,70 @@
+require('../testdom.js')('<html><body></body></html>');
+
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import jsdom from 'mocha-jsdom';
+import { expect } from 'chai';
+
+import GroupsView from '../../src/js/components/groups';
+
+
+// XXX Discuss this style as it is definitely different from other specs.
+describe("GroupsView from 'components/groups'", () => {
+  jsdom({ skipWindowCheck: true });
+
+  it('displays group names and sizes', () => {
+    const groups = [
+      { name: 'Test Group 1', size: 0, url: '#', enabled: true },
+      { name: 'Test Group 2', size: 42, url: '#', enabled: true }
+    ];
+    const comp = TestUtils.renderIntoDocument(
+      <GroupsView groups={ groups } />
+    );
+
+    const els = TestUtils.scryRenderedDOMComponentsWithTag(comp, 'a');
+
+    expect(els[0].textContent.replace(/\s+/g, ' ').trim()).to.equal('Test Group 1 (0)');
+    expect(els[1].textContent.replace(/\s+/g, ' ').trim()).to.equal('Test Group 2 (42)');
+  });
+
+  it('highlights enabled group', () => {
+    const groups = [
+      { name: 'Test Group', size: 0, url: '#', enabled: true }
+    ];
+    const comp = TestUtils.renderIntoDocument(
+      <GroupsView groups={ groups } />
+    );
+
+    const els = TestUtils.scryRenderedDOMComponentsWithTag(comp, 'a');
+
+    expect(els[0].firstElementChild.className.split(/\s+/g)).to.include('label-info');
+  });
+
+  it('hides disabled group', () => {
+    const groups = [
+      { name: 'Test Group', size: 0, url: '#', enabled: false }
+    ];
+    const comp = TestUtils.renderIntoDocument(
+      <GroupsView groups={ groups } />
+    );
+
+    const els = TestUtils.scryRenderedDOMComponentsWithTag(comp, 'a');
+
+    expect(els[0].firstElementChild.className.split(/\s+/g)).not.to.include('label-info');
+  });
+
+  it('links to group detail', () => {
+    const groups = [
+      { name: 'Test Group', size: 0, url: '/test-group', enabled: false }
+    ];
+    const comp = TestUtils.renderIntoDocument(
+      <GroupsView groups={ groups } />
+    );
+
+    const els = TestUtils.scryRenderedDOMComponentsWithTag(comp, 'a');
+
+    expect(els[0].href).to.equal('/test-group');
+  });
+
+
+});
