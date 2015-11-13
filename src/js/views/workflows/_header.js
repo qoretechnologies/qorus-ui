16b614a800c0555ed64e60c15bd19efa5ccb067b
Filip Zrust <filip.zrust@sinsir.net>
2015-11-13 06:21:15 +0100

    Wrapper around workflow header to proper style

--- src/js/views/workflows/_header.js
+++ src/js/views/workflows/_header.js
@@ -24,28 +24,30 @@ export default class WorkflowsHeader extends Component {
     const { workflow } = this.props;
 
     return (
-      <div className='row'>
-        <div className='col-xs-12'>
-          <h3 className='pull-left'>
-            <span className='selectable'>{ workflow.normalizedName }</span>
-          </h3>
-        </div>
-        <div className='controls col-xs-12'>
-          <Controls>
-            <Control title='Disable' icon='power-off' labelStyle='success' />
-            <Control title='Reset' icon='refresh' labelStyle='warning' />
-          </Controls>
-          { ' ' }
-          <AutoStart
-            id={ workflow.id }
-            autostart={ workflow.autostart }
-            execCount={ workflow.exec_count }
-            inc={ (id, value) => {
-              dispatch(actions.workflows.setAutostart(id, value))
-            } }
-            dec={ (id, value) => {
-              dispatch(actions.workflows.setAutostart(id, value))
-            } } />
+      <div className='heading'>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <h3 className='pull-left'>
+              <span className='selectable'>{ workflow.normalizedName }</span>
+            </h3>
+          </div>
+          <div className='controls col-xs-12'>
+            <Controls>
+              <Control title='Disable' icon='power-off' labelStyle='success' />
+              <Control title='Reset' icon='refresh' labelStyle='warning' />
+            </Controls>
+            { ' ' }
+            <AutoStart
+              id={ workflow.id }
+              autostart={ workflow.autostart }
+              execCount={ workflow.exec_count }
+              inc={ (id, value) => {
+                dispatch(actions.workflows.setAutostart(id, value))
+              } }
+              dec={ (id, value) => {
+                dispatch(actions.workflows.setAutostart(id, value))
+              } } />
+          </div>
         </div>
       </div>
     );
