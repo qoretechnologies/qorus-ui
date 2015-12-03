39989d628774a47f4cd29694930a42e5ed3b99a1
Filip Zrust <filip.zrust@sinsir.net>
2015-12-03 08:46:28 +0100

    AutoStart event propagation stopped
    Also contains other code improvements in AutoStart component.

--- src/js/components/autostart.js
+++ src/js/components/autostart.js
@@ -1,8 +1,17 @@
 import React, { Component, PropTypes } from 'react';
-import clNs from 'classnames';
-import { preventDefault } from '../utils';
+import classNames from 'classnames';
+import { pureRender } from './utils';
 
-class AutoStart extends Component {
+
+/**
+ * Counter component relying on inc and dec props to change the value.
+ *
+ * It also visualizes difference between autostart (counter value) and
+ * execCount (a value actually used by something). If they are the
+ * same, it is considered healthy.
+ */
+@pureRender
+export default class AutoStart extends Component {
   static propTypes = {
     autostart: PropTypes.number.isRequired,
     execCount: PropTypes.number.isRequired,
@@ -14,8 +23,21 @@ class AutoStart extends Component {
   static defaultProps = {
     autostart: 0,
     execCount: 0,
-    inc: (ctx, val) => val + 1,
-    dec: (ctx, val) => val - 1
+    inc: (ctx, val) => val,
+    dec: (ctx, val) => val
+  }
+
+  /**
+   * Calls fn with context and autostart modified by delta.
+   *
+   * @param {function} fn
+   * @param {number} delta
+   * @param {Event} ev
+   */
+  onClick(fn, delta, ev) {
+    ev.stopPropagation();
+
+    fn(this.props.context, this.props.autostart + delta);
   }
 
   render() {
@@ -24,7 +46,7 @@ class AutoStart extends Component {
     if (!context) throw new Error('Property context must be provided.');
 
     const equals = (autostart === execCount && autostart > 0);
-    const classes = clNs({
+    const classes = classNames({
       'label': true,
       'autostart-change': true,
       'label-default': !equals,
@@ -33,20 +55,22 @@ class AutoStart extends Component {
 
     return (
       <div className='autostart btn-controls btn-group'>
-        <a className='label label-default'
-          onClick={ preventDefault(() => dec(context, autostart - 1)) }>
+        <a
+          className='label label-default'
+          onClick={this.onClick.bind(this, dec, -1)}
+        >
           <i className='fa fa-minus'></i>
-          </a>
-        <a className={ classes } title='Click to edit'>
-          { autostart }
-          </a>
-        <a className='label label-default'
-          onClick={ preventDefault(() => inc(context, autostart + 1)) }>
+        </a>
+        <a className={classes} title='Click to edit'>
+          {autostart}
+        </a>
+        <a
+          className='label label-default'
+          onClick={this.onClick.bind(this, inc, +1)}
+        >
           <i className='fa fa-plus'></i>
         </a>
       </div>
     );
   }
 }
-
-export default AutoStart;
