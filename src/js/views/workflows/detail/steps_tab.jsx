439f3bc432ba968351d9a32765b4dbd54c70c2b7
Filip Zrust <filip.zrust@sinsir.net>
2016-03-04 14:54:40 +0100

    Revert "Collisions of steps diagram paths resolved"
    This reverts commit 2702c52941eec80f9434207f494ed8fee53fced6.
    
    Placing potentially colliding joint lines to different layers has been
    regarded as confusing.

--- src/js/views/workflows/detail/steps_tab.jsx
+++ src/js/views/workflows/detail/steps_tab.jsx
@@ -66,12 +66,6 @@ const BOX_MARGIN = 20;
 
 
 /**
- * Margin between box and area where path joints are positioned.
- */
-const JOINT_MARGIN = 1;
-
-
-/**
  * Box rounded corner radius.
  */
 const BOX_ROUNDED_CORNER = 5;
@@ -795,7 +789,7 @@ export default class StepsTab extends Component {
    * The path has a joint right above bottom of the two
    * boxes. Obviously, this joint is invisible for vertically aligned
    * boxes. Otherwise, it makes sure the path is perpendicular to x-
-   * and y-axis and does not overlap at the joint.
+   * and y-axis.
    *
    * @param {StepArgs} start
    * @param {StepArgs} end
@@ -804,7 +798,6 @@ export default class StepsTab extends Component {
    * @see getBoxVerticalCenter
    * @see getBoxHeight
    * @see BOX_MARGIN
-   * @see JOINT_MARGIN
    */
   renderPath(start, end) {
     const startX = this.getBoxHorizontalCenter(start.colIdx, start.row);
@@ -813,15 +806,10 @@ export default class StepsTab extends Component {
     const endX = this.getBoxHorizontalCenter(end.colIdx, end.row);
     const endY = this.getBoxVerticalCenter(end.rowIdx);
 
-    const jointsOnRow = start.row.length;
-    const jointStep = (BOX_MARGIN - 2 * JOINT_MARGIN) / (jointsOnRow + 1);
-    const jointPos = start.row.findIndex(s => s === start.stepId);
     const joint =
-      endY -
+      Math.max(startY, endY) -
       this.getBoxHeight() / 2 -
-      BOX_MARGIN +
-      JOINT_MARGIN +
-      (jointPos + 1) * jointStep;
+      BOX_MARGIN / 2;
 
     return (
       <path
