373c380f378e724289a505b582c87d7f136fddb6
Filip Witosz <filip.witosz@gmail.com>
2016-03-24 09:47:02 +0100

    Added the dropdown toggle functionality. Moved the dropdown from the render to a separate render function.

--- src/js/views/workflows/toolbar.jsx
+++ src/js/views/workflows/toolbar.jsx
@@ -18,6 +18,12 @@ const SearchFormView = Dummy;
 
 @pureRender
 export default class WorkflowsToolbar extends Component {
+  componentWillMount() {
+    this.setState({
+      showSelectionDropdown: false
+    })
+  }
+
   render() {
     const btnCls = classNames('btn', 'btn-default', 'btn-sm');
 
@@ -28,33 +34,31 @@ export default class WorkflowsToolbar extends Component {
             <button
               className={classNames(btnCls, 'dropdown-toggle')}
               data-toggle="dropdown"
+              onClick={::this.handleSelectionDropdownToggleClick}
+              id="selection-toggle"
             >
-              <i className="fa fa-square-o check-all checker"></i>&nbsp;
+              <i
+                  className="fa fa-square-o check-all checker icon-check-minus"
+              /> &nbsp;
               <span className="caret" />
             </button>
-            <ul className="dropdown-menu above">
-              <li><a href="#" className="check-all">All</a></li>
-              <li><a href="#" className="uncheck-all">None</a></li>
-              <li><a href="#" className="invert">Invert</a></li>
-              <li><a href="#" className="running">Running</a></li>
-              <li><a href="#" className="stopped">Stopped</a></li>
-            </ul>
+            {this.renderSelectionDropdown()}
           </div>
           <div className="btn-group">
             <button className={btnCls}>
-              <i className="fa fa-off"></i> Enable
+              <i className="fa fa-off" /> Enable
             </button>
             <button className={btnCls}>
-              <i className="fa fa-ban-circle"></i> Disable
+              <i className="fa fa-ban-circle" /> Disable
             </button>
             <button className={btnCls}>
-              <i className="fa fa-refresh"></i> Reset
+              <i className="fa fa-refresh" /> Reset
             </button>
             <button className={btnCls}>
-              <i className="fa fa-flag-alt"></i> Hide
+              <i className="fa fa-flag-alt" /> Hide
             </button>
             <button className={btnCls}>
-              <i className="fa fa-flag"></i> Show
+              <i className="fa fa-flag" /> Show
             </button>
           </div>
           <DateFilterView
@@ -76,5 +80,34 @@ export default class WorkflowsToolbar extends Component {
         <div className="datepicker-container"></div>
       </Toolbar>
     );
+  }
+
+  /**
+   * Displays / hides the control dropdown
+   * based on the current state
+   *
+   * @param {object} event
+   */
+  handleSelectionDropdownToggleClick() {
+    this.setState({
+      showSelectionDropdown: !this.state.showSelectionDropdown
+    });
+  }
+
+  renderSelectionDropdown() {
+    if (this.state.showSelectionDropdown) {
+      return <ul
+        className={classNames('dropdown-menu', 'above', 'show')}
+        id="selection-dropdown"
+      >
+        <li><a href="#" className="check-all">All</a></li>
+        <li><a href="#" className="uncheck-all">None</a></li>
+        <li><a href="#" className="invert">Invert</a></li>
+        <li><a href="#" className="running">Running</a></li>
+        <li><a href="#" className="stopped">Stopped</a></li>
+      </ul>;
+    }
+
+    return null;
   }
 }
