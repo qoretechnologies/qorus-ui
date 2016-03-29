9232e089bd82495ebdbb7651b4696dac4cee820b
Filip Witosz <filip.witosz@gmail.com>
2016-03-29 13:38:44 +0200

    Dropdown should be hidden on blur test added.

--- features/step_definitions/workflows_controls_steps.js
+++ features/step_definitions/workflows_controls_steps.js
@@ -1,9 +1,17 @@
 module.exports = function workFlowControlSteps() {
   this.When(/^I click the dropdown toggle$/, function () {
-    return this.browser.pressButton('#selection-toggle');
+    return this.browser.pressButton('#selection');
   });
 
   this.Then(/^the dropdown should be shown$/, function () {
     this.browser.assert.element('#selection-dropdown');
+  });
+
+  this.When(/^I blur the dropdown toggle$/, function () {
+    return this.browser.pressButton('#selection');
+  });
+
+  this.Then(/^the dropdown should be hidden$/, function () {
+    this.browser.assert.elements('#selection-dropdown', 0);
   });
 };
