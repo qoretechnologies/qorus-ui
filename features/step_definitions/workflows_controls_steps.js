f7658b4d055a28fc31e4fd1ccdbf8e67c2353d82
Filip Witosz <filip.witosz@gmail.com>
2016-04-04 15:34:15 +0100

    UA Tests for most of the new toolbar functionalities.

--- features/step_definitions/workflows_controls_steps.js
+++ features/step_definitions/workflows_controls_steps.js
@@ -1,3 +1,10 @@
+const findElementByText = (browser, selector, text) => {
+  "use strict";
+  return browser.
+    queryAll(selector).
+    find(el => el.textContent === text) || null;
+};
+
 module.exports = function workFlowControlSteps() {
   this.When(/^I click the dropdown toggle$/, function () {
     return this.browser.pressButton('#selection');
@@ -13,5 +20,42 @@ module.exports = function workFlowControlSteps() {
 
   this.Then(/^the dropdown should be hidden$/, function () {
     this.browser.assert.elements('#selection-dropdown', 0);
+  });
+
+  this.When(/^I click the checkbox on the dropdown$/, function () {
+    this.browser.click('#selection .fa-square-o')
+  });
+
+  this.Then(/^all of the workflows are selected$/, function () {
+    this.browser.assert.hasClass('td.narrow > i', 'fa-check-square-o');
+  });
+
+  this.When(/^I select one workflow$/, function () {
+    this.browser.click('td.narrow > i.fa-square-o:first-of-type');
+  });
+
+  this.Then(/^the dropdown checkbox should be halfchecked$/, function () {
+    this.browser.assert.hasClass('#selection > i', 'fa-minus-square-o');
+  });
+
+  this.When(/^I click the All item$/, function () {
+    this.browser.click('#selection-dropdown > li > a:first-of-type');
+  });
+
+  this.When(/^I click the Invert item$/, function () {
+    const el = findElementByText(this.browser, '#selection-dropdown > li > a', 'Invert');
+
+    this.browser.click(el);
+  });
+
+  this.When(/^I click the None item$/, function () {
+    const el = findElementByText(this.browser, '#selection-dropdown > li > a', 'None');
+
+    this.browser.click(el);
+  });
+
+  this.Then(/^no workflows are selected$/, function () {
+    this.browser.assert.hasClass('td.narrow > i', 'fa-square-o');
+    this.browser.assert.hasClass('#selection > i', 'fa-square-o');
   });
 };
--- features/worklfows_controls.feature
+++ features/worklfows_controls.feature
@@ -1,10 +1,47 @@
 Feature: Workflow Controls
 
   Scenario: Dropdown is toggled
-    Given I am on workflows listing
+    Given I am on "workflows" listing
+    And "workflows" get loaded
     When I click the dropdown toggle
     Then the dropdown should be shown
 
+  Scenario: Dropdown checkbox is checked
+    Given I am on "workflows" listing
+    And "workflows" get loaded
+    When I click the checkbox on the dropdown
+    Then all of the workflows are selected
+
+  Scenario: Selecting all workflows from dropdown
+    Given I am on workflows listing
+    And workflows get loaded
+    And I click the dropdown toggle
+    When I click the All item
+    Then all of the workflows are selected
+
+  Scenario: Inverting selection of workflows
+    Given I am on workflows listing
+    And workflows get loaded
+    And I click the checkbox on the dropdown
+    And I click the dropdown toggle
+    When I click the Invert item
+    Then no workflows are selected
+
+  Scenario: Deselecting all workflows from dropdown
+    Given I am on workflows listing
+    And workflows get loaded
+    And I click the checkbox on the dropdown
+    And I click the dropdown toggle
+    When I click the None item
+    Then no workflows are selected
+
+  Scenario: Selecting some workflows
+    Given I am on workflows listing
+    And workflows get loaded
+    When I select one workflow
+    Then the dropdown checkbox should be halfchecked
+
+  @no-impl
   Scenario: Dropdown is hidden on toggle blur
     Given I am on workflows listing
     And I click the dropdown toggle
