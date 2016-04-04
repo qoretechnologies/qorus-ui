983f71b85b93759d975b2f509397923d7acdb6a7
Filip Witosz <filip.witosz@gmail.com>
2016-04-04 15:29:27 +0100

    Added new constant file for the checkbox control.

---
+++ src/js/constants/checkbox.js
@@ -0,0 +1,11 @@
+export const CHECKBOX_CLASSES = {
+  UNCHECKED: 'fa-square-o',
+  HALFCHECKED: 'fa-minus-square-o',
+  CHECKED: 'fa-check-square-o',
+};
+
+export const CHECKBOX_STATES = {
+  none: 'UNCHECKED',
+  some: 'HALFCHECKED',
+  all: 'CHECKED',
+};
