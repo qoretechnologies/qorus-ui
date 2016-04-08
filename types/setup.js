72b9acc4f7eff67c8c0710ff188cf1d5fa9716be
Filip Zrust <filip.zrust@sinsir.net>
2016-04-08 16:13:18 +0200

    Mock API fix after JSON Schema Faker update

--- types/setup.js
+++ types/setup.js
@@ -1,22 +1,22 @@
 const jsf = require('json-schema-faker');
 
 
-jsf.formats('qorus-name', gen => (
+jsf.format('qorus-name', gen => (
   gen.randexp('^[a-z][-a-z0-9]{0,31}$')
 ));
 
-jsf.formats('qorus-codename', gen => (
+jsf.format('qorus-codename', gen => (
   gen.randexp('^[A-Z][-A-Z0-9]{0,31}$')
 ));
 
-jsf.formats('qorus-version', gen => (
+jsf.format('qorus-version', gen => (
   gen.randexp('^[1-9]\\.\\d{1,2}$')
 ));
 
-jsf.formats('qorus-system-version', gen => (
+jsf.format('qorus-system-version', gen => (
   gen.randexp('^[1-9]\\.\\d{1,2}\\.\\d{1,2}$')
 ));
 
-jsf.formats('qorus-system-schema', gen => (
+jsf.format('qorus-system-schema', gen => (
   gen.randexp('^[a-z][-a-z0-9]{0,31}@[a-z][_a-z0-9]{0,31}$')
 ));
