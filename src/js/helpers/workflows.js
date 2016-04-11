24a1bdf9989786424828d39a1b306c94ec02e748
Filip Witosz <filip.witosz@gmail.com>
2016-04-11 21:58:13 +0200

    Added new 'helpers' folder that should be used for any reusable non-react related code.

---
+++ src/js/helpers/workflows.js
@@ -0,0 +1,36 @@
+import { WORKFLOW_FILTERS } from '../constants/filters';
+import { includes } from 'lodash';
+
+/**
+ * Helpers for workflows manipulations
+ */
+export default {
+  /**
+   * Checks if the filter is currently set,
+   * returns array with 'all' if not
+   * otherwise returns an array from the url filter
+   *
+   * @param {String} filter
+   * @returns {Array}
+   */
+  filterArray(filter) {
+    return typeof filter === 'undefined' ? [WORKFLOW_FILTERS.ALL] : filter.split(',');
+  },
+  /**
+   * Handles the workflow filter change
+   * Removes the current filter or adds it
+   *
+   * @param {Array} filter
+   * @param {String} target
+   * @returns {Array}
+   */
+  handleFilterChange(filter, target) {
+    if (includes(filter, target)) {
+      filter.splice(filter.indexOf(target), 1);
+    } else {
+      filter.push(target);
+    }
+
+    return filter;
+  }
+};
