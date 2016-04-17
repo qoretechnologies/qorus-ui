cb468538ffd52a8041abe3fb50a298eb4534e10d
Filip Witosz <filip.witosz@gmail.com>
2016-04-17 20:37:14 +0200

    Reworked the helper to make it easier to import just certain function instead of one big object.

--- src/js/helpers/workflows.js
+++ src/js/helpers/workflows.js
@@ -1,46 +1,49 @@
 import { includes } from 'lodash';
 import { WORKFLOW_FILTERS } from '../constants/filters';
+
 /**
- * Helpers for workflows manipulations
+ * Checks if the filter is currently set,
+ * returns empty array if not
+ * otherwise returns an array from the url filter
+ *
+ * @param {String} filter
+ * @returns {Array}
  */
-export default {
-  /**
-   * Checks if the filter is currently set,
-   * returns empty array if not
-   * otherwise returns an array from the url filter
-   *
-   * @param {String} filter
-   * @returns {Array}
-   */
-  filterArray(filter) {
-    return typeof filter === 'undefined' || filter === '' ?
-      [WORKFLOW_FILTERS.ALL] : filter.split(',');
-  },
-  /**
-   * Handles the workflow filter change
-   * Removes the current filter or adds it
-   *
-   * @param {Array} filter
-   * @param {String} target
-   * @returns {Array}
-   */
-  handleFilterChange(filter, target) {
-    const filterArr = this.filterArray(filter);
-
-    if (includes(filterArr, target)) {
-      filterArr.splice(filterArr.indexOf(target), 1);
-
-      if (filterArr.length === 0) {
-        filterArr.push(WORKFLOW_FILTERS.ALL);
-      }
-    } else {
-      filterArr.push(target);
-
-      if (includes(filterArr, WORKFLOW_FILTERS.ALL)) {
-        filterArr.splice(filterArr.indexOf(WORKFLOW_FILTERS.ALL), 1);
-      }
-    }
-
-    return filterArr;
-  },
+const filterArray = (filter) => {
+  return typeof filter === 'undefined' || filter === '' ?
+    [WORKFLOW_FILTERS.ALL] : filter.split(',');
 };
+
+/**
+ * Handles the workflow filter change
+ * Removes the current filter or adds it
+ *
+ * @param {Array} filter
+ * @param {String} target
+ * @returns {Array}
+ */
+const handleFilterChange = (filter, target) => {
+  const filterArr = filterArray(filter);
+
+  if (includes(filterArr, target)) {
+    filterArr.splice(filterArr.indexOf(target), 1);
+
+    if (filterArr.length === 0) {
+      filterArr.push(WORKFLOW_FILTERS.ALL);
+    }
+  } else {
+    filterArr.push(target);
+
+    if (includes(filterArr, WORKFLOW_FILTERS.ALL)) {
+      filterArr.splice(filterArr.indexOf(WORKFLOW_FILTERS.ALL), 1);
+    }
+  }
+
+  return filterArr;
+};
+
+export {
+  filterArray,
+  handleFilterChange,
+};
+
