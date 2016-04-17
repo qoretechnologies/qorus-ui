82a6f5d94920f190cc2a08eb36ce40bbeb10e6a1
Filip Witosz <filip.witosz@gmail.com>
2016-04-17 20:49:03 +0200

    Rework due to helpers change.

--- test/helpers/workflows.spec.js
+++ test/helpers/workflows.spec.js
@@ -1,39 +1,39 @@
 import { expect } from 'chai';
-import workflowHelpers from '../../src/js/helpers/workflows';
+import { filterArray, handleFilterChange } from '../../src/js/helpers/workflows';
 
 describe("Workflow Helpers from 'helpers/worklfows'", () => {
   it('transforms filter string to an array', () => {
     const filterString = 'running,hidden,last';
-    const filterArray = workflowHelpers.filterArray(filterString);
+    const filter = filterArray(filterString);
 
-    expect(filterArray).to.be.an('array');
-    expect(filterArray).to.have.length(3);
+    expect(filter).to.be.an('array');
+    expect(filter).to.have.length(3);
   });
 
   it('returns array with "all" value when filter is empty', () => {
     const filterString = '';
-    const filterArray = workflowHelpers.filterArray(filterString);
+    const filter = filterArray(filterString);
 
-    expect(filterArray).to.be.an('array');
-    expect(filterArray).to.have.length(1);
-    expect(filterArray[0]).to.equal('all');
+    expect(filter).to.be.an('array');
+    expect(filter).to.have.length(1);
+    expect(filter[0]).to.equal('all');
   });
 
   it('adds a new URL filter', () => {
     const filterString = 'running,hidden';
-    const filterAfter = workflowHelpers.handleFilterChange(filterString, 'last');
+    const filter = handleFilterChange(filterString, 'last');
 
-    expect(filterAfter).to.be.an('array');
-    expect(filterAfter).to.have.length(3);
-    expect(filterAfter[2]).to.equal('last');
+    expect(filter).to.be.an('array');
+    expect(filter).to.have.length(3);
+    expect(filter[2]).to.equal('last');
   });
 
   it('removes a filter if it is already set', () => {
     const filterString = 'running,hidden';
-    const filterAfter = workflowHelpers.handleFilterChange(filterString, 'running');
+    const filter = handleFilterChange(filterString, 'running');
 
-    expect(filterAfter).to.be.an('array');
-    expect(filterAfter).to.have.length(1);
-    expect(filterAfter[0]).to.equal('hidden');
+    expect(filter).to.be.an('array');
+    expect(filter).to.have.length(1);
+    expect(filter[0]).to.equal('hidden');
   });
 });
