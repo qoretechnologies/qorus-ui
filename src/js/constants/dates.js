5f91f50a21102eb7ac760e5de515a87035121089
Filip Witosz <filip.witosz@gmail.com>
2016-04-20 11:14:27 +0200

    Added date constants.

---
+++ src/js/constants/dates.js
@@ -0,0 +1,15 @@
+const DATES = {
+  ALL: 'all',
+  NOW: 'now',
+  PREV_DAY: '24h',
+};
+
+const DATE_FORMATS = {
+  URL_FORMAT: 'YYYYMMDDHHmmss',
+  ALL: '1970/1/1',
+};
+
+export {
+  DATES,
+  DATE_FORMATS,
+};
