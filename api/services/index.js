fdf6db17a4c3072c864689b168f91308cdead773
Petr Chalupny <petr.chalupny@qoretechnologies.com>
2016-05-03 13:57:57 +0200

    Added support for service Control actions to fake api

--- api/services/index.js
+++ api/services/index.js
@@ -1,21 +1,48 @@
 'use strict';
 
-
 /**
  * @module api/services
  */
 
+const findModel = (idAttr) => (id, s) => s[idAttr] === parseInt(id, 10);
+const config = require('../config');
 
 const express = require('express');
 
 const rest = require('../rest');
 
-
 module.exports = () => {
   const data = require('./data')();
 
   const router = new express.Router();
-  router.use(rest(data, (id, s) => s.serviceid === parseInt(id, 10)));
+  router.use(rest(data, findModel('serviceid')));
+
+  router.put('/:id', (req, res) => {
+    const item = data.find(findModel('serviceid').bind(null, req.params.id));
+
+    switch (req.body.action) {
+      case 'disable':
+      case 'enable':
+        item.enabled = !!req.body.enabled;
+        break;
+      case 'setAutostart':
+        item.autostart = !!req.body.autostart;
+        break;
+      case 'load':
+        item.status = 'loaded';
+        break;
+      case 'unload':
+        item.status = 'unloaded';
+        break;
+      default:
+        if (config.env !== 'test') {
+          process.stderr.write(`Unknown action ${req.body.action}.\n`);
+        }
+        break;
+    }
+
+    res.json(item);
+  });
 
   return router;
 };
