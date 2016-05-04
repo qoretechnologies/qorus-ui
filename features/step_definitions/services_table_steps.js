e9e2586bb4b0e3b8f040c4b3593f668a78b6cadf
Filip Witosz <filip.witosz@gmail.com>
2016-05-04 09:35:05 +0200

    Added first batch of tests for services, testing sorting.

---
+++ features/step_definitions/services_table_steps.js
@@ -0,0 +1,42 @@
+import { findElementByText } from './common_steps';
+import firstBy from 'thenby';
+
+const services = [
+  {
+    name: 'info',
+    type: 'system',
+    version: '3.1.9.9',
+    threads: 0,
+    description: 'status information service',
+  },
+  {
+    name: 'test',
+    type: 'system',
+    version: '3.1.0.0',
+    threads: 0,
+    description: 'a simple test to establish the proper balance of your loud speakers',
+  },
+  {
+    name: 'anothertest',
+    type: 'user',
+    version: '1.1.0.0',
+    threads: 2,
+    description: 'another day, another test',
+  },
+];
+
+module.exports = function workFlowTableSteps() {
+  this.Then(/^services are sorted by "([^"]*)" "([^"]*)"$/, async function(header, dir) {
+    const direction = dir === 'asc' ? 1 : -1;
+    const key = header.toLowerCase();
+    const sorted = services.slice().sort(firstBy(w => w[key], direction));
+    const tableData = this.browser.queryAll('tbody > tr');
+    const th = findElementByText(this.browser, `thead th.sort.sort-${dir}`, header);
+
+    this.browser.assert.elements(th, 1);
+
+    tableData.forEach((row, index) => {
+      this.browser.assert.text(row.cells[5], sorted[index].name);
+    });
+  });
+};
