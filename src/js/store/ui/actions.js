87fbaf5491bf9f3e16e5cf26afcf47b7815940c4
Filip Witosz <filip.witosz@gmail.com>
2016-05-05 10:59:18 +0200

    Implemented the UI store for jobs.

--- src/js/store/ui/actions.js
+++ src/js/store/ui/actions.js
@@ -1,2 +1,3 @@
 export * as workflows from './workflows/actions';
 export * as services from './services/actions';
+export * as jobs from './jobs/actions';
--- src/js/store/ui/reducers.js
+++ src/js/store/ui/reducers.js
@@ -1,3 +1,4 @@
 export workflows from './workflows/reducers';
 export services from './services/reducers';
+export jobs from './jobs/reducers';
 
