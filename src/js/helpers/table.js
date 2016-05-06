a5f6e076ff0a906e86e5b48ef8f7340108cd6de2
Filip Witosz <filip.witosz@gmail.com>
2016-05-06 16:27:26 +0200

    Helpers for table.

---
+++ src/js/helpers/table.js
@@ -0,0 +1,39 @@
+import { ORDER_STATES } from '../constants/orders';
+
+const CSVheaders = {
+  workflows: {
+    exec_count: 'Execs',
+    autostart: 'Autostart',
+    id: 'ID',
+    name: 'Name',
+    version: 'Version',
+  },
+  services: {
+    type: 'Type',
+    threads: 'Threads',
+    name: 'Name',
+    version: 'Version',
+    desc: 'Description',
+  },
+  jobs: {
+    name: 'Name',
+    version: 'Version',
+    last_executed: 'Last',
+    next: 'Next',
+    expiry_date: 'Expiry date',
+    COMPLETE: 'Complete',
+    ERORR: 'Error',
+    'IN-PROGRESS': 'In-progress',
+    CRASHED: 'Crash',
+  },
+};
+
+Object.keys(ORDER_STATES).forEach(key => {
+  CSVheaders.workflows[ORDER_STATES[key].name] = ORDER_STATES[key].short;
+});
+
+const getCSVHeaders = (view) => CSVheaders[view];
+
+export {
+  getCSVHeaders,
+};
