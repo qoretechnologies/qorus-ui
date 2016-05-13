c87dcaeeda673ebf0bc14dd64e4fcdbb0f3b7104
Petr Chalupny <pchalupny@roura.prague.qoretechnologies.com>
2016-05-13 17:19:38 +0200

    Improved System Props view - one more fix needed - show correct warning when search is empty

--- src/js/views/system/prop.js
+++ src/js/views/system/prop.js
@@ -6,9 +6,24 @@ define(function(require) {
       utils       = require('utils'),
       Template    = require('tpl!templates/system/prop.html'),
       ConfirmView = require('views/common/confirm'),
-      ServiceView;
+      FilteredCollection = require('backbone.filtered.collection'),
+      PropsCollection = require('collections/props'),
+      // DomainTableTpl = require('tpl!templates/system/props/table.html'),
+      // DomainRowTpl = require('tpl!templates/system/props/row.html'),
+      PropsView;
 
-  ServiceView = Qorus.View.extend({
+  // DomainTable = Qorus.TableView.extend({
+  //   tpl: DomainTableTpl,
+  //   row_template: DomainRowTpl,
+  //
+  //   initialize: function (opts) {
+  //     this.opts = opts || {};
+  //     this.collection = new FilteredCollection(collection);
+  //     this.collection.filterBy('domain', { domain: opts.domain });
+  //   }
+  // });
+
+  PropsView = Qorus.View.extend({
     views: {},
     defaultEvents: {
       'submit': 'doAction',
@@ -31,33 +46,16 @@ define(function(require) {
         }.bind(this)
       };
 
-      this.on('fetch', this.render);
-      this.on('show', this.applySearch);
+      this.collection = new PropsCollection();
 
-      this.getData();
+      this.listenTo(this.collection, 'sync', this.render);
+      this.collection.fetch();
+      this.on('show', this.applySearch);
     },
 
     getUrl: function () {
       return [settings.REST_API_PREFIX, 'system', 'props'].join('/');
     },
-
-    getData: function () {
-      var self = this;
-      var url = this.getUrl();
-
-      $.get(url)
-        .done(function (data) {
-          self.data = data;
-          self.context.data = data;
-          self.trigger('fetch');
-        });
-    },
-
-    // render: function (ctx) {
-    //   this.context.data = this.data;
-    //
-    //   ServiceView.__super__.render.call(this, ctx);
-    // },
 
     doAction: function (ev) {
       var params = {};
@@ -117,30 +115,13 @@ define(function(require) {
 
     search: function (e) {
       var query = this.$('#property-filter').val();
+      this.updateUrl({ q: query });
       this.applySearch(query);
     },
 
-    applySearch: function (query) {
-      query = query || '';
-
-      this.updateUrl({ q: query });
-      if (query === '') {
-        this.$('#property-filter').val('');
-      }
-
-      if (query.length < 1) {
-        this.$('tr').show();
-        return this;
-      }
-
-      this.$('tr').hide();
-      this.$('tr[data-search*='+ query.toLowerCase() +']')
-        .show()
-        .parent()
-        .prev('thead')
-        .find('tr')
-        .show();
-    },
+    applySearch: _.debounce(function (query) {
+      this.render();
+    }, 300),
 
     updateUrl: function (params) {
       var url = utils.getCurrentLocationPath();
@@ -160,13 +141,21 @@ define(function(require) {
       return '';
     },
 
-    onRender: function () {
+    preRender: function () {
       var q = this.getQueryFromUrl();
-      if (q) {
-        this.applySearch(q);
+      var filtered = this.collection.toJSON();
+
+      if (q !== '') {
+        filtered = _.filter(filtered, function (m) {
+          return m.domain.indexOf(q) !== -1;
+        });
       }
+      this.context.domains = _(filtered).pluck('domain').uniq().value();
+
+      this.context.items = filtered;
+      this.context.query = q;
     }
   });
 
-  return ServiceView;
+  return PropsView;
 });
