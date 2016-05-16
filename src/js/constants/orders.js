547f139984d979d5bfa3b756da313f4d63ddf467
Filip Witosz <filip.witosz@gmail.com>
2016-05-16 16:15:02 +0200

    Changed the label for some of the orders and added new constants for the order actions. Each status can have only certain actions.

--- src/js/constants/orders.js
+++ src/js/constants/orders.js
@@ -6,9 +6,32 @@ export const ORDER_STATES = [
   { name: 'EVENT-WAITING', short: 'V', label: 'waiting', title: 'Event-Waiting' },
   { name: 'ASYNC-WAITING', short: 'A', label: 'waiting', title: 'Async-Waiting' },
   { name: 'WAITING', short: 'W', label: 'waiting', title: 'Waiting' },
-  { name: 'RETRY', short: 'R', label: 'error', title: 'Retry' },
-  { name: 'ERROR', short: 'E', label: 'important', title: 'Error' },
+  { name: 'RETRY', short: 'R', label: 'danger', title: 'Retry' },
+  { name: 'ERROR', short: 'E', label: 'danger', title: 'Error' },
   { name: 'IN-PROGRESS', short: 'I', label: 'warning', title: 'In-Progress' },
   { name: 'CANCELED', short: 'X', label: 'canceled', title: 'Canceled' },
-  { name: 'BLOCKED', short: 'B', lable: 'blocked', title: 'Blocked' },
+  { name: 'BLOCKED', short: 'B', label: 'gray', title: 'Blocked' },
 ];
+
+export const ORDER_ACTIONS = {
+  ALL: [
+    { name: 'Block', action: 'block', icon: 'minus-circle', style: 'black' },
+    { name: 'Unblock', action: 'unblock', icon: 'check-circle', style: 'gray' },
+    { name: 'Cancel', action: 'cancel', icon: 'times-circle', style: 'danger' },
+    { name: 'Uncancel', action: 'uncancel', icon: 'times-circle', style: 'warning' },
+    { name: 'Retry', action: 'retry', icon: 'refresh', style: 'success' },
+    { name: 'Schedule', action: 'schedule', icon: 'clock-o', style: 'info' },
+  ],
+  BLOCKED: ['Unblock'],
+  CANCELED: ['Uncancel'],
+  RETRY: ['Block', 'Cancel', 'Retry', 'Schedule'],
+  ERROR: ['Block', 'Cancel', 'Retry'],
+  COMPLETE: [],
+  UNBLOCKING: [],
+  BLOCKING: [],
+  CANCELING: [],
+  UNCANCELING: [],
+  RETRYING: [],
+};
+
+
