9f78e073609456fb702a2f565a2b1e45d3e2ab56
Petr Chalupny <temnoregg@gmail.com>
2016-05-16 14:15:36 +0200

    Fixes #1280 - Library constant/function/class is normalized to lowerCase to make case insensitive search working

--- src/js/views/library/library.js
+++ src/js/views/library/library.js
@@ -10,88 +10,88 @@ define(function (require) {
       ToolbarTpl       = require('tpl!templates/library/toolbar.html'),
       Toolbar          = require('views/toolbars/toolbar'),
       ToolbarView, View, DetailView, RowView;
-      
+
   ToolbarView = Toolbar.extend({
     template: ToolbarTpl
   });
-    
+
   DetailView = Qorus.ModelView.extend({
     template: LibraryDetailTpl,
-    
+
     onRender: function () {
       var div = document.createElement('div'),
           code;
-          
+
       if (this.model.get('body_highlighted')) {
         code = this.model.get('body_highlighted');
       } else {
         code = Prism.highlight(this.model.get('body'), Prism.languages.qore);
         this.model.set({ 'body_highlighted': code }, { silent: true });
       }
-      
+
       div.innerHTML = code;
-      
+
       this.$('code').append(div);
     },
-    
+
     close: function () {
       this.stopListening();
       this.undelegateEvents();
       this.$el.empty();
     }
   });
-  
+
   RowView = Qorus.RowView.extend({
     template: RowTpl,
     attributes: function () {
       return {
-        "data-search": this.model.get('name')
+        "data-search": this.model.get('name').toLowerCase()
       };
     }
   });
-            
+
   View = Qorus.ListView.extend({
     additionalEvents: {
       "click [data-idattr]": "fetch",
       "keyup input.filter": "search",
       "submit": "search"
     },
-      
+
     template: Template,
-    
+
     initialize: function (options) {
       View.__super__.initialize.call(this, options.collection, options.date, options);
       this.listenToOnce(this.collection, 'sync', this.render);
       // this.listenTo(this.collection, 'all', function () { console.log(arguments) });
     },
-    
-    preRender: function () {  
+
+    preRender: function () {
       _.each(this.collection.collections, function (col, name) {
         var view = this.setView(new Qorus.TableView({ collection: col, row_view: RowView, row_template: RowTpl, template: TableTpl, name: name }), '#'+name).render();
         this.listenTo(view, 'row:clicked', this.fetch);
       }, this);
       this.setView(new ToolbarView(), '#toolbar');
     },
-    
+
     onRender: function () {
       this.fixHeights();
       $(window).on('resize.panes', $.proxy(this.fixHeights, this));
     },
-    
+
     fixHeights: function (e) {
       if (!e || !e.target.tagName) {
         var h = $(window).height() - this.$el.offset().top - $('footer').height() - 6;
         this.$el.height(h).addClass('overflow');
-        this.$el.find('.row-fluid > div').height(h);        
+        this.$el.find('.row-fluid > div').height(h);
       }
     },
-    
+
     fetch: function (view) {
       var model = view.model;
-      
+
       if (!model.get('body')) {
         this.listenToOnce(model, 'sync', this.updateDetail);
-        model.fetch();        
+        model.fetch();
       } else {
         this.updateDetail(model);
       }
@@ -101,27 +101,27 @@ define(function (require) {
 
     updateDetail: function (model) {
       var view = this.setView(new DetailView({ model: model }), '#library-info', true);
-      
+
       view.render();
     },
-    
+
     clean: function () {
       $(window).off('resize.panes');
     },
-    
+
     search: function (e) {
       var query = this.$('input.filter').val();
       this.applySearch(query);
       $(window).trigger('resize.panes');
       e.preventDefault();
     },
-    
+
     applySearch: function (query) {
       if (query.length < 1) {
         this.$('tr').show();
         return this;
       }
-      
+
       this.$('tr').hide();
       this.$('tr[data-search*='+ query.toLowerCase() +']')
         .show()
@@ -129,10 +129,10 @@ define(function (require) {
         .prev('thead')
         .find('tr')
         .show();
-      
+
       this.$('#library-info').height('');
     }
   });
-  
+
   return View;
 });
