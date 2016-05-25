7f88b54823b50dee747895255d74fa72037b5c63
Filip Witosz <filip.witosz@gmail.com>
2016-05-25 11:17:00 +0200

    Fixed sorting that was not working properly.

--- src/js/helpers/table.js
+++ src/js/helpers/table.js
@@ -51,12 +51,8 @@ CSVheaders.workflows.total = 'Total';
 const getCSVHeaders = (view) => CSVheaders[view];
 
 const sortFunc = (sort, v1, v2) => {
-  const val1 = v1[sort.sortBy];
-  const val2 = v2[sort.sortBy];
-
-  if (moment(val1).isValid() && moment(val2).isValid()) {
-    return moment(val1).isBefore(val2) ? -1 : 1;
-  }
+  const val1 = v1[sort];
+  const val2 = v2[sort];
 
   if (!val1) {
     return -1;
@@ -66,13 +62,37 @@ const sortFunc = (sort, v1, v2) => {
     return 1;
   }
 
-  return val1.toLowerCase() < val2.toLowerCase() ? -1 : 1;
+  if (moment(new Date(val1)).isValid() && moment(new Date(val2)).isValid()) {
+    if (moment(val1).isBefore(val2)) {
+      return -1;
+    } else if (moment(val1).isAfter(val2)) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  if (val1.toLowerCase() < val2.toLowerCase()) {
+    return -1;
+  } else if (val1.toLowerCase() === val2.toLowerCase()) {
+    return 0;
+  }
+
+  return 1;
 };
 
-const sortTable = (data, sort) => data.slice().sort(
-  firstBy((v1, v2) => sortFunc(sort, v1, v2), sort.sortByKey.direction)
-    .thenBy((v1, v2) => sortFunc(sort, v1, v2), sort.historySortByKey.direction)
-);
+const sortTable = (data, sort) => {
+  if (sort.historySortBy) {
+    return data.slice().sort(
+      firstBy((v1, v2) => sortFunc(sort.sortBy, v1, v2), sort.sortByKey.direction)
+        .thenBy((v1, v2) => sortFunc(sort.historySortBy, v1, v2), sort.historySortByKey.direction)
+    );
+  }
+
+  return data.slice().sort(
+    firstBy((v1, v2) => sortFunc(sort.sortBy, v1, v2), sort.sortByKey.direction)
+  );
+};
 
 export {
   getCSVHeaders,
