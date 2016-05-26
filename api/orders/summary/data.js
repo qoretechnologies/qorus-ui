6c007ebd66eab4e9dd524c914c59274adf0b009b
Filip Witosz <filip.witosz@gmail.com>
2016-05-26 16:14:52 +0200

    Data for the orders processing summary

---
+++ api/orders/summary/data.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Services data.
+ *
+ * Based on `NODE_ENV` exports either fixtures or generated data.
+ *
+ * @module api/orders/data
+ * @see module:api/data.getData
+ */
+
+/**
+ * @return {!Array<!module:types.Order>}
+ */
+module.exports = () => require('../../data').getData('orders/summary');
