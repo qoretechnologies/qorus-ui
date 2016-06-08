7c702ca7ced89eccef2595c667ec34425c4e84fa
Filip Witosz <filip.witosz@gmail.com>
2016-06-08 09:20:13 +0200

    Rest of the unit tests for the chart helpers.

--- test/helpers/chart.spec.js
+++ test/helpers/chart.spec.js
@@ -7,6 +7,7 @@ import {
   createLineDatasets,
   createDoughDatasets,
 } from '../../src/js/helpers/chart';
+import moment from 'moment';
 
 describe('Helpers from helpers/table', () => {
   describe('groupOrders', () => {
@@ -119,8 +120,131 @@ describe('Helpers from helpers/table', () => {
   });
 
   describe('createLineDatasets', () => {
-    it.only('creates the data for the line chart for last 24 hours', () => {
-      
+    it('creates the data and labels for the line chart for last 24 hours', () => {
+      const data = [
+        {
+          maxduration: 5,
+          minduration: 3,
+          avgduration: 4,
+          maxprocessing: 7,
+          minprocessing: 0,
+          avgprocessing: 3,
+          grouping: moment().add(-1, 'hours').format('YYYY-MM-DD HH'),
+        },
+        {
+          maxduration: 2,
+          minduration: 6,
+          avgduration: 7,
+          maxprocessing: 1,
+          minprocessing: 4,
+          avgprocessing: 2,
+          grouping: moment().add(-3, 'hours').format('YYYY-MM-DD HH'),
+        },
+      ];
+      const transformed = createLineDatasets(data, 1);
+      const first = moment().add(-23, 'hours').format('HH');
+      const last = moment().format('HH');
+
+      expect(transformed).to.be.an('object');
+      expect(transformed.labels).to.have.length(24);
+      expect(transformed.labels[0]).to.equal(first);
+      expect(transformed.labels[23]).to.equal(last);
+      expect(transformed.data).to.have.length(6);
+      expect(transformed.data[0].label).to.equal('avgduration');
+      expect(transformed.data[0].data[1]).to.equal(4);
+      expect(transformed.data[0].data[3]).to.equal(7);
+      expect(transformed.data[1].label).to.equal('avgprocessing');
+      expect(transformed.data[1].data[1]).to.equal(3);
+      expect(transformed.data[1].data[3]).to.equal(2);
+      expect(transformed.data[2].label).to.equal('maxduration');
+      expect(transformed.data[2].data[1]).to.equal(5);
+      expect(transformed.data[2].data[3]).to.equal(2);
+      expect(transformed.data[3].label).to.equal('maxprocessing');
+      expect(transformed.data[3].data[1]).to.equal(7);
+      expect(transformed.data[3].data[3]).to.equal(1);
+      expect(transformed.data[4].label).to.equal('minduration');
+      expect(transformed.data[4].data[1]).to.equal(3);
+      expect(transformed.data[4].data[3]).to.equal(6);
+      expect(transformed.data[5].label).to.equal('minprocessing');
+      expect(transformed.data[5].data[1]).to.equal(0);
+      expect(transformed.data[5].data[3]).to.equal(4);
+    });
+
+    it('creates the data and labels for the line chart for last 3 days', () => {
+      const data = [
+        {
+          maxduration: 5,
+          minduration: 3,
+          avgduration: 4,
+          maxprocessing: 7,
+          minprocessing: 0,
+          avgprocessing: 3,
+          grouping: moment().add(-1, 'days').format('YYYY-MM-DD'),
+        },
+        {
+          maxduration: 2,
+          minduration: 6,
+          avgduration: 7,
+          maxprocessing: 1,
+          minprocessing: 4,
+          avgprocessing: 2,
+          grouping: moment().add(-2, 'days').format('YYYY-MM-DD'),
+        },
+      ];
+      const transformed = createLineDatasets(data, 3);
+      const first = moment().add(-2, 'days').format('DD');
+      const last = moment().format('DD');
+
+      expect(transformed).to.be.an('object');
+      expect(transformed.labels).to.have.length(3);
+      expect(transformed.labels[0]).to.equal(first);
+      expect(transformed.labels[2]).to.equal(last);
+      expect(transformed.data).to.have.length(6);
+      expect(transformed.data[0].label).to.equal('avgduration');
+      expect(transformed.data[0].data[1]).to.equal(4);
+      expect(transformed.data[0].data[2]).to.equal(7);
+      expect(transformed.data[1].label).to.equal('avgprocessing');
+      expect(transformed.data[1].data[1]).to.equal(3);
+      expect(transformed.data[1].data[2]).to.equal(2);
+      expect(transformed.data[2].label).to.equal('maxduration');
+      expect(transformed.data[2].data[1]).to.equal(5);
+      expect(transformed.data[2].data[2]).to.equal(2);
+      expect(transformed.data[3].label).to.equal('maxprocessing');
+      expect(transformed.data[3].data[1]).to.equal(7);
+      expect(transformed.data[3].data[2]).to.equal(1);
+      expect(transformed.data[4].label).to.equal('minduration');
+      expect(transformed.data[4].data[1]).to.equal(3);
+      expect(transformed.data[4].data[2]).to.equal(6);
+      expect(transformed.data[5].label).to.equal('minprocessing');
+      expect(transformed.data[5].data[1]).to.equal(0);
+      expect(transformed.data[5].data[2]).to.equal(4);
+    });
+  });
+
+  describe('createLineDatasets', () => {
+    it.only('creates the data for the dough chart', () => {
+      const data = {
+        READY: 1,
+        COMPLETED: 2,
+        ERROR: 5,
+        BLOCKED: 3,
+        INCOMPLETE: 1,
+        WAITING: 2,
+        'ASYNC-WAITING': 3,
+        'EVENT-WAITING': 9,
+        RETRY: 4,
+        SCHEDULED: 7,
+        CANCELED: 0,
+      };
+
+      const transformed = createDoughDatasets(data);
+
+      expect(transformed.labels).to.have.length(5);
+      expect(transformed.data[0].data[0]).to.equal(8);
+      expect(transformed.data[0].data[1]).to.equal(19);
+      expect(transformed.data[0].data[2]).to.equal(8);
+      expect(transformed.data[0].data[3]).to.equal(0);
+      expect(transformed.data[0].data[4]).to.equal(2);
     });
   });
 });
