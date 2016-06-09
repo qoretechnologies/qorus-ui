d0c029c812f0908de0a47746c48a0a0ae346d456
Filip Witosz <filip.witosz@gmail.com>
2016-06-09 13:43:51 +0200

    Order detail tests, first batch.

---
+++ features/order/order_data.feature
@@ -0,0 +1,12 @@
+Feature: Tests functionalities of the Data tab
+
+  Scenario: Tabs with the data are displayed
+    Given I am on order "31380" and "Data" tab
+    Then I should see "3" subtabs
+    And I should see the "Static" data
+
+  Scenario: Tabs with the data are displayed
+    Given I am on order "31380" and "Data" tab
+    When I click the "Dynamic" tab
+    Then the URL changes to "/order/31380/data/dynamic"
+    And I should see the "Dynamic" data
--- features/step_definitions/order_detail_steps.js
+++ features/step_definitions/order_detail_steps.js
@@ -1,4 +1,4 @@
-import { findTableRow } from './common_steps';
+import { selectors, findTableRow, findElementByText } from './common_steps';
 
 module.exports = function orderDetailSteps() {
   this.Given(/^I click on the "([^"]*)" order$/, async function(order) {
@@ -10,6 +10,18 @@ module.exports = function orderDetailSteps() {
 
   this.Given(/^I am on order "([^"]*)"$/, async function(order) {
     return this.browser.visit(`/order/${order}`);
+  });
+
+  this.Given(/^I am on order "([^"]*)" and "([^"]*)" tab$/, async function(order, name) {
+    await this.browser.visit(`/order/${order}`);
+
+    await this.waitForElement('.nav-tabs');
+
+    const tab = findElementByText(this.browser, '.nav-tabs a', name);
+
+    this.browser.click(tab);
+
+    await this.waitForChange(1000);
   });
 
   this.Then(/^there are "([^"]*)" action buttons$/, async function(count) {
@@ -24,5 +36,26 @@ module.exports = function orderDetailSteps() {
     await this.waitForElement('h3.detail-title');
     await this.waitForElement('ul.nav-tabs');
     await this.waitForElement('.order-actions');
+  });
+
+  // STEPS
+  this.Given(/^I click on the "([^"]*)" step$/, async function(step) {
+    const row = this.browser.
+      queryAll(selectors.cmpRows).
+      find(r => r.cells[2].textContent === step) || null;
+
+    return this.browser.click(row);
+  });
+
+  this.Then(/^I should see "([^"]*)" subtabs$/, async function(count) {
+    return this.browser.assert.elements('.tab-pane .nav-pills li', parseInt(count, 10));
+  });
+
+  this.Then(/^I should see the "([^"]*)" data$/, async function(data) {
+    const el = findElementByText(this.browser, '.tab-pane .nav-pills li.active', data);
+
+    this.browser.assert.element(el);
+    this.browser.assert.element('.tree-wrapper');
+    this.browser.assert.element('.button--copy');
   });
 };
