dbe6a09889a4d236079f9f3921b3f4c71b1d16f5
Filip Witosz <filip.witosz@gmail.com>
2016-06-14 14:46:56 +0200

    Added more helpers.

--- src/js/helpers/orders.js
+++ src/js/helpers/orders.js
@@ -1,4 +1,5 @@
-import { ORDER_ACTIONS } from '../constants/orders';
+import { ORDER_ACTIONS, ORDER_STATES, CUSTOM_ORDER_STATES, STATUS_PRIORITY } from '../constants/orders';
+import { indexOf } from 'lodash';
 
 const getActionData = (action, prop) => {
   const actionData = ORDER_ACTIONS.ALL.find(a => a.action === action);
@@ -6,6 +7,32 @@ const getActionData = (action, prop) => {
   return prop ? actionData[prop] : actionData;
 };
 
+const getStatusLabel = (status) => {
+  const states = ORDER_STATES.concat(CUSTOM_ORDER_STATES);
+
+  return states.find(o => o.name === status).label || null;
+};
+
+const groupInstances = (steps) => {
+  const stepGroups = [];
+
+  steps.forEach(step => {
+    const name = step.stepname;
+    const group = stepGroups[name] =
+      stepGroups[name] || { steps: [], name, status: null };
+    const max = Math.max(
+      indexOf(STATUS_PRIORITY, group.status), indexOf(STATUS_PRIORITY, step.stepstatus)
+    );
+
+    group.status = STATUS_PRIORITY[max];
+    group.steps.push(step);
+  });
+
+  return stepGroups;
+};
+
 export {
   getActionData,
+  getStatusLabel,
+  groupInstances,
 };
