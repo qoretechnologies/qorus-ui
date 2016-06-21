cbbd6583d7fb0ee203a1300ae86bf9d38a47f23a
Filip Witosz <filip.witosz@gmail.com>
2016-06-21 15:04:25 +0200

    Added tests for the properties helper.

---
+++ test/helpers/props.spec.js
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import { updateProp, deleteProp } from '../../src/js/store/api/resources/system/props/actions/helper';
+
+const getProps = () => ({
+  omq: {
+    test: 'hello',
+    awesome: 'value',
+  },
+  another: {
+    great: 'object',
+    super: 'great',
+  },
+});
+
+describe('{ updateProp, deleteProp } from store/props/actions/helper', () => {
+  describe('updateProp', () => {
+    it('adds a new prop', () => {
+      const props = getProps();
+      const result = updateProp(props, { domain: 'omq', key: 'newkey', value: 'prop' });
+
+      expect(result).to.be.an('object');
+      expect(Object.keys(result.omq).length).to.equal(3);
+      expect(result.omq.newkey).to.equal('prop');
+    });
+
+    it('updates a new prop', () => {
+      const props = getProps();
+      const result = updateProp(props, { domain: 'omq', key: 'test', value: 'updated' });
+
+      expect(result).to.be.an('object');
+      expect(Object.keys(result.omq).length).to.equal(2);
+      expect(result.omq.test).to.equal('updated');
+    });
+  });
+
+  describe('deleteProp', () => {
+    it('removes a prop', () => {
+      const props = getProps();
+      const result = deleteProp(props, { domain: 'another' });
+
+      expect(result).to.be.an('object');
+      expect(Object.keys(result).length).to.equal(1);
+    });
+
+    it('removes a prop key', () => {
+      const props = getProps();
+      const result = deleteProp(props, { domain: 'another', key: 'super' });
+
+      expect(result).to.be.an('object');
+      expect(Object.keys(result).length).to.equal(2);
+      expect(Object.keys(result.another).length).to.equal(1);
+      expect(result.another.super).to.equal(undefined);
+    });
+  });
+});
