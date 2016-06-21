8f0e754663b623d7d0e2fd8b747cc5e5a43bc438
Filip Witosz <filip.witosz@gmail.com>
2016-06-21 14:56:42 +0200

    Added rest api fake data for the system/properties.

--- api/system/index.js
+++ api/system/index.js
@@ -5,6 +5,8 @@
  */
 
 const express = require('express');
+const updateProp = require('../../src/js/store/api/resources/system/props/actions/helper').updateProp;
+const deleteProp = require('../../src/js/store/api/resources/system/props/actions/helper').deleteProp;
 
 module.exports = () => {
   const data = require('./data')();
@@ -14,6 +16,33 @@ module.exports = () => {
   router.get('/:attr', (req, res) => (
     res.status(data[req.params.attr] ? 200 : 404).json(data[req.params.attr])
   ));
+
+  router.put('/props/:domain/:key', (req, res) => {
+    data.props = updateProp(data.props, {
+      domain: req.params.domain,
+      key: req.params.key,
+      value: req.body.parse_args,
+    });
+
+    res.json(data.props);
+  });
+
+  router.delete('/props/:domain', (req, res) => {
+    data.props = deleteProp(data.props, {
+      domain: req.params.domain,
+    });
+
+    res.json(data.props);
+  });
+
+  router.delete('/props/:domain/:key', (req, res) => {
+    data.props = deleteProp(data.props, {
+      domain: req.params.domain,
+      key: req.params.key,
+    });
+
+    res.json(data.props);
+  });
 
   return router;
 };
