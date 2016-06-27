f4fd00f103c0b5f8a180bb094a17d4416a23323a
Filip Witosz <filip.witosz@gmail.com>
2016-06-27 11:00:05 +0200

    Modal for skipping steps.

---
+++ src/js/views/order/diagram/skip.jsx
@@ -0,0 +1,90 @@
+import React, { Component, PropTypes } from 'react';
+
+import Modal from 'components/modal';
+import { Controls, Control } from 'components/controls';
+
+export default class extends Component {
+  static propTypes = {
+    onClose: PropTypes.func,
+    onSubmit: PropTypes.func,
+    steps: PropTypes.array,
+  };
+
+  componentWillMount() {
+    this.setState({
+      value: '',
+      error: false,
+    });
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    let corr = true;
+    const vals = this.state.value.split(',');
+    const regexVal = /^[0-9]+$/;
+    const regexRange = /^[0-9]+-[0-9]+$/;
+
+    vals.forEach(val => {
+      if (!regexVal.test(val) && !regexRange.test(val)) {
+        corr = false;
+      }
+    });
+
+    if (corr) {
+      this.props.onSubmit(this.state.value);
+
+      this.props.onClose();
+    }
+
+    this.setState({
+      error: !corr,
+    });
+  };
+
+  render() {
+    return (
+      <Modal>
+        <Modal.Header
+          titleId="skip-modal"
+          onClose={this.props.onClose}
+        > Skip step </Modal.Header>
+        <form
+          onSubmit={this.handleFormSubmit}
+        >
+          <Modal.Body>
+            <input
+              type="text"
+              value={this.state.value}
+              className={`form-control ${this.state.error ? 'form-error' : ''}`}
+              onChange={this.handleInputChange}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Controls grouped noControls>
+              <Control
+                type="button"
+                label="Cancel"
+                big
+                btnStyle="default"
+                action={this.props.onClose}
+              />
+              <Control
+                type="submit"
+                label="Save"
+                big
+                btnStyle="success"
+              />
+            </Controls>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    );
+  }
+}
