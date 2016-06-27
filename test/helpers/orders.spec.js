d5e63075cdc2d2860cf67b1d90ceaf515db88720
Filip Witosz <filip.witosz@gmail.com>
2016-06-27 11:00:41 +0200

    Added tests for the orders helpers.

---
+++ test/helpers/orders.spec.js
@@ -0,0 +1,66 @@
+import { expect } from 'chai';
+import { canSkip } from '../../src/js/helpers/orders';
+
+describe('{ canSkip } from helpers/orders', () => {
+  describe('canSkip', () => {
+    it('returns true when the step has type ASYNC has ERROR status and hasnt been skipped yet', () => {
+      const step = {
+        steptype: 'ASYNC',
+        stepstatus: 'ERROR',
+        skip: false,
+      };
+
+      const result = canSkip(step);
+
+      expect(result).to.equal(true);
+    });
+
+    it('returns false when the skip has COMPLETE status and hasnt been skipped yet', () => {
+      const step = {
+        steptype: 'ASYNC',
+        stepstatus: 'COMPLETE',
+        skip: false,
+      };
+
+      const result = canSkip(step);
+
+      expect(result).to.equal(false);
+    });
+
+    it('returns false when the skip has type SUBWORKFLOW, ERROR status and hasnt been skipped yet', () => {
+      const step = {
+        steptype: 'SUBWORKFLOW',
+        stepstatus: 'ERROR',
+        skip: false,
+      };
+
+      const result = canSkip(step);
+
+      expect(result).to.equal(false);
+    });
+
+    it('returns false when the skip has ERROR status but it has been skipped', () => {
+      const step = {
+        steptype: 'ASYNC',
+        stepstatus: 'ERROR',
+        skip: true,
+      };
+
+      const result = canSkip(step);
+
+      expect(result).to.equal(false);
+    });
+
+    it('returns false when the skip has COMPLETE status and it has been skipped', () => {
+      const step = {
+        steptype: 'SUBWORKFLOW',
+        stepstatus: 'COMPLETE',
+        skip: true,
+      };
+
+      const result = canSkip(step);
+
+      expect(result).to.equal(false);
+    });
+  });
+});
