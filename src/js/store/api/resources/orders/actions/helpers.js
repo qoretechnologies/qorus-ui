0df3623ff87a0afd38c27a98c13d5600fa2b827a
Filip Witosz <filip.witosz@gmail.com>
2016-06-27 10:58:08 +0200

    Added new action helper that skips the provides steps based on the string provided.

--- src/js/store/api/resources/orders/actions/helpers.js
+++ src/js/store/api/resources/orders/actions/helpers.js
@@ -1,4 +1,7 @@
 const moment = require('moment');
+const range = require('lodash').range;
+const includes = require('lodash').includes;
+const canSkip = require('helpers/orders').canSkip;
 
 const addNote = (order, saved, username, note) => {
   var notes;
@@ -36,6 +39,44 @@ const unlockOrder = (order, note, username) => {
   return obj;
 };
 
+const skipIndexes = (order, stepid, value) => {
+  var indexes = value.split(',');
+  var steps = order.StepInstances.slice();
+
+  indexes.forEach(i => {
+    var ind = i;
+
+    if (includes(i, '-')) {
+      ind = i.split('-');
+      var rng = range(ind[0], ind[1]);
+      rng.push(ind[1]);
+
+      rng.forEach(r => {
+        steps.forEach((st, index) => {
+          var skipped = st;
+
+          if (st.stepid === stepid && st.ind === parseInt(r, 10) && canSkip(st)) {
+            skipped.skip = true;
+            steps[index] = skipped;
+          }
+        });
+      });
+    } else {
+      steps.forEach((st, index) => {
+        var skipped = st;
+
+        if (st.stepid === stepid && st.ind === parseInt(ind, 10) && canSkip(st)) {
+          skipped.skip = true;
+          steps[index] = skipped;
+        }
+      });
+    }
+  });
+
+  return steps;
+};
+
 module.exports.lockOrder = lockOrder;
 module.exports.unlockOrder = unlockOrder;
 module.exports.addNote = addNote;
+module.exports.skipIndexes = skipIndexes;
