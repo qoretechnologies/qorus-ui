c6bdf2a7bcc3d12c0babba8e9463b547e4e983ca
Filip Witosz <filip.witosz@gmail.com>
2016-06-30 09:10:13 +0200

    Added groups actions.

---
+++ src/js/store/api/resources/groups/actions/index.js
@@ -0,0 +1,33 @@
+export function enable(actions) {
+  return group => dispatch => {
+    dispatch(actions.groups.action({
+      body: JSON.stringify({
+        action: 'enable',
+        enabled: true,
+      }),
+    }, group.name));
+  };
+}
+
+export function disable(actions) {
+  return group => dispatch => {
+    dispatch(actions.groups.action({
+      body: JSON.stringify({
+        action: 'disable',
+        enabled: false,
+      }),
+    }, group.name));
+  };
+}
+
+export function enableBatch(actions) {
+  return names => dispatch => {
+    dispatch(actions.groups.batch_action('setStatus', names.join(','), 'enabled=true', { enabled: true }, 'groups'));
+  };
+}
+
+export function disableBatch(actions) {
+  return names => dispatch => {
+    dispatch(actions.groups.batch_action('setStatus', names.join(','), 'enabled=false', { enabled: false }, 'groups'));
+  };
+}
