2b399b3a6c7c0869d4917d85e7a6945f2cb3a72d
Filip Witosz <filip.witosz@gmail.com>
2016-06-30 09:10:53 +0200

    UI store for groups sorting.

---
+++ src/js/store/ui/groups/actions.js
@@ -0,0 +1,3 @@
+import { createAction } from 'redux-actions';
+
+export const sort = createAction('CHANGE_GROUPS_SORT');
---
+++ src/js/store/ui/groups/reducers.js
@@ -0,0 +1,25 @@
+import { handleActions } from 'redux-actions';
+
+const initialState = {
+  sortBy: 'enabled',
+  sortByKey: { ignoreCase: true, direction: 1 },
+  historySortBy: 'name',
+  historySortByKey: { ignoreCase: true, direction: 1 },
+};
+
+export default handleActions({
+  CHANGE_GROUPS_SORT: {
+    next(state, action) {
+      const payload = action.payload;
+
+      if (action.payload.sortBy !== state.sortBy) {
+        payload.historySortBy = state.sortBy;
+        payload.historySortByKey = state.sortByKey;
+      } else {
+        payload.sortByKey = { ignoreCase: true, direction: state.sortByKey.direction * -1 };
+      }
+
+      return Object.assign({}, state, payload);
+    },
+  },
+}, initialState);
