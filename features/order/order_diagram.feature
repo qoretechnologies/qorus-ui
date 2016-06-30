77356ae45608a2cae09e28d3da640b5cf41887f1
Filip Witosz <filip.witosz@gmail.com>
2016-06-30 09:06:46 +0200

    Working on the diagram tests.

--- features/order/order_diagram.feature
+++ features/order/order_diagram.feature
@@ -2,4 +2,51 @@ Feature: Tests for the Diagram tab
 
   Scenario: Diagram is displayed
     Given I am on order "31380" and "Diagram" tab
-    And diagram gets loaded
+    Then diagram, tables and error pane get loaded
+
+  Scenario: Diagram is correct
+    Given I am on order "31380" and "Diagram" tab
+    And diagram, tables and error pane get loaded
+    Then the diagram has 10 boxes
+    And start box is "error"
+    And there are 2 "complete" boxes
+    And there are 2 "error" boxes
+    And there are 1 "async-waiting" boxes
+    And there are 5 "normal" boxes
+
+  @no-impl
+  Scenario: Clicking the info icon shows the step detail
+    Given I am on order "31380" and "Diagram" tab
+    And diagram, tables and error pane get loaded
+    When I click the info icon on "array_test_async v1.0"
+    Then the step detail table is shown
+
+  Scenario: Changes order priority
+    Given I am on order "31380" and "Diagram" tab
+    And diagram, tables and error pane get loaded
+    Then I change the priority to 900
+
+  Scenario: Errors are loaded correctly
+    Given I am on order "31380" and "Diagram" tab
+    And diagram, tables and error pane get loaded
+    Then there are "51" errors shown
+
+  Scenario: Filtering errors based on severity
+    Given I am on order "31380" and "Diagram" tab
+    And diagram, tables and error pane get loaded
+    When I click the "WARNING" button inside "errors" dropdown
+    And I click the "Filter" button
+    Then there are "2" errors shown
+
+  Scenario: Showing all errors details
+    Given I am on order "31380" and "Diagram" tab
+    And diagram, tables and error pane get loaded
+    When I click the "Show errors detail" button
+    Then there are "102" errors shown
+
+  Scenario: Shows error detail when row is clicked
+    Given I am on order "31380" and "Diagram" tab
+    And diagram, tables and error pane get loaded
+    When I click on a row
+    Then the the copy button is displayed
+
--- features/step_definitions/order_detail_steps.js
+++ features/step_definitions/order_detail_steps.js
@@ -82,7 +82,69 @@ module.exports = function orderDetailSteps() {
     expect(notes[0].children[1].textContent).to.equal('admin');
   });
 
-  this.Given(/^diagram gets loaded$/, async function() {
+  this.Given(/^diagram, tables and error pane get loaded$/, async function() {
     await this.waitForElement('svg.diagram');
+
+    this.browser.assert.elements('.order-info-view table', 2);
+    this.browser.assert.element('.error-pane');
+  });
+
+  this.Then(/^the diagram has (\d+) boxes$/, async function(count) {
+    return this.browser.assert.elements('.diagram .diagram__box', parseInt(count, 10));
+  });
+
+  this.Then(/^start box is "([^"]*)"$/, async function(css) {
+    const el = this.browser.queryAll('.diagram__box')[0];
+
+    this.browser.assert.className(el, `diagram__box diagram__box--${css}`);
+  });
+
+  this.Then(/^there are (\d+) "([^"]*)" boxes$/, async function(count, css) {
+    return this.browser.assert.elements(`.diagram .diagram__box--${css}`, parseInt(count, 10));
+  });
+
+  this.When(/^I click the info icon on "([^"]*)"$/, async function(text) {
+    const box = this.browser.queryAll('.diagram .diagram__box text').find(b => b.textContent === text);
+    const el = box.parentNode.children[3];
+
+    this.browser.click(el);
+
+    await this.waitForChange(500);
+  });
+
+  this.Then(/^the step detail table is shown$/, async function() {
+    this.browser.assert.elements('.order-info-view table', 3);
+  });
+
+  this.Then(/^I change the priority to (\d+)$/, async function(priority) {
+    const table = this.browser.queryAll('.order-info-view table')[0];
+    const cell = table.children[0].rows[2].cells[3];
+
+    this.browser.click(cell);
+
+    const input = cell.children[0].children[0];
+    const submit = cell.children[0].children[1];
+
+    this.browser.fill(input, priority);
+    await this.browser.pressButton(submit);
+
+    expect(input.value).to.equal('900');
+  });
+
+  this.Then(/^there are "([^"]*)" errors shown$/, async function(count) {
+    this.browser.assert.elements('.error-pane table tbody tr', parseInt(count, 10));
+  });
+
+  this.When(/^I click on a row$/, async function() {
+    const row = this.browser.queryAll('.error-pane table tbody tr')[0];
+
+    this.browser.click(row);
+  });
+
+  this.Then(/^the the copy button is displayed$/, async function() {
+    const cell = this.browser.queryAll('.error-pane table tbody tr')[1].cells[0];
+
+    this.browser.assert.elements('.error-pane table tbody tr', 52);
+    this.browser.assert.text(cell, 'Copy error');
   });
 };
