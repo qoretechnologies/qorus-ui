73180cd489b6b24b9205afce21b87e5126bccc6d
Filip Witosz <filip.witosz@gmail.com>
2016-07-21 10:42:00 +0200

    Added some flowtype checks.

--- src/js/components/layout/center.jsx
+++ src/js/components/layout/center.jsx
@@ -1,7 +1,7 @@
 /* @flow */
 import React from 'react';
 
-const CenterWrapper = ({ children }: { children: Array<ReactClass<*>> }) => (
+const CenterWrapper: Function = ({ children }: { children: Array<ReactClass<*>> }): React.Element<any> => (
   <div className="center-wrapper">{children}</div>
 );
 
