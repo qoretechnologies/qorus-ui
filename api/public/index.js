a74371f110d0dd2b03b3cec613239d67bffee783
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-07-22 23:27:16 +0500

    Remove 'action': login from request body

--- api/public/index.js
+++ api/public/index.js
@@ -14,21 +14,14 @@ module.exports = () => {
   const router = new express.Router();
 
   router.post('/login', (req, res) => {
-    let user;
-    switch (req.body.action) {
-      case 'login':
-        user = data.find(u => (
-          u.username === req.body.user && u.password === req.body.pass
-        ));
-        if (user) {
-          const token = user.token;
-          res.status(200).json({ token });
-        } else {
-          res.status(400).json({ error: 'some error' });
-        }
-        break;
-      default:
-        res.status(400).send('no action');
+    const user = data.find(u => (
+      u.username === req.body.user && u.password === req.body.pass
+    ));
+    if (user) {
+      const token = user.token;
+      res.status(200).json({ token });
+    } else {
+      res.status(400).json({ error: 'some error' });
     }
   });
 
