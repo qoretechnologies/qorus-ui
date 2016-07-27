75624756bf810f54cb46cb19af71da44f90ceb31
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-07-27 12:25:17 +0500

    Add health component

---
+++ src/js/components/health/item.jsx
@@ -0,0 +1,11 @@
+/* @flow */
+import React from 'react';
+
+const HealthItem = ({ title, children }: { title: string, children: ?any}) => (
+  <tr>
+    <th>{title}</th>
+    <td>{children}</td>
+  </tr>
+);
+
+export default HealthItem
---
+++ test/components/health.spec.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+
+import { Health, HealthItem } from '../../src/js/components/health';
+
+describe.only('{  Health, HealthItem } from components/health', () => {
+  it('Show all items with health item layout', () => {
+    const wrapper = mount(
+      <Health title="test">
+        <HealthItem title="key-1">value</HealthItem>
+        <HealthItem title="key-2"><strong>Another</strong></HealthItem>
+      </Health>
+    );
+
+    expect(wrapper.find('h6').text()).to.equals('test');
+    expect(wrapper.find('tr').length).to.equals(2);
+    expect(wrapper.find('strong').length).to.equals(1);
+  });
+
+  it('Do not show not HealthItem child', () => {
+    const wrapper = mount(
+      <Health title="test">
+        <HealthItem title="key-1">value</HealthItem>
+        <tr><th>test</th><td>test</td></tr>
+      </Health>
+    );
+
+    expect(wrapper.find('tr').length).to.equals(1);
+  });
+});
