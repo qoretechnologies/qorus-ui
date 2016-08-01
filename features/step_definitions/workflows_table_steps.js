1010b51dff2c81550460ada57e3ade5248ef9bc2
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-08-02 01:03:33 +0500

    Fix workflow_table.feature tests

--- features/step_definitions/workflows_table_steps.js
+++ features/step_definitions/workflows_table_steps.js
@@ -1,4 +1,4 @@
-import { findElementByText } from './common_steps';
+import { findElementByText, selectors } from './common_steps';
 import firstBy from 'thenby';
 
 let workflows = [
@@ -44,8 +44,12 @@ module.exports = function workFlowTableSteps() {
     const direction = dir === 'asc' ? 1 : -1;
     const key = header.toLowerCase();
     const sorted = workflows.slice().sort(firstBy(w => w[key], { ignoreCase: true, direction }));
-    const tableData = this.browser.queryAll('tbody > tr');
-    const th = findElementByText(this.browser, `thead th.sort.sort-${dir}`, header);
+    const tableData = this.browser.queryAll(`${selectors.mainSection} tbody > tr`);
+    const th = findElementByText(
+      this.browser,
+      `${selectors.mainSection} thead th.sort.sort-${dir}`,
+      header
+    );
 
     this.browser.assert.elements(th, 1);
 
