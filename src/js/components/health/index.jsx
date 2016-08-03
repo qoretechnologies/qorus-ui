b137c7dafaf63caedc4bc0b643322cb0b7780a84
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-08-03 16:17:30 +0500

    Use flow to check Health item children

--- src/js/components/health/index.jsx
+++ src/js/components/health/index.jsx
@@ -4,22 +4,21 @@ import React from 'react';
 import HealthItem from './item';
 
 const Health = (
-  { title, className, children }: { title: string, className: string, children?: any}
+  {
+    title,
+    className,
+    children,
+  }: {
+    title: string,
+    className: string,
+    children?: React.Element<HealthItem> | Array<React.Element<HealthItem>>,
+  }
 ) => (
   <div className={className}>
     <h6>{title}</h6>
     <table className="table table-condensed">
       <tbody>
-        {React.Children.map(children, (c: React.Element<*>) => {
-          if (c.type !== HealthItem) {
-            const typeName = c.type.toString();
-            console.warn(
-              `Health component expect children type of 'HealthItem' not '${typeName}'`
-            );
-            return null;
-          }
-          return c;
-        })}
+        {children}
       </tbody>
     </table>
   </div>
