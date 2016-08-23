1787d8d85ba934b97d2d19698df7b38d1c6d933d
Filip Witosz <filip.witosz@gmail.com>
2016-08-23 17:01:44 +0200

    Created a HOC that adds openModal and closeModal props to the provided component.

---
+++ src/js/hocomponents/modal.jsx
@@ -0,0 +1,43 @@
+/* @flow */
+import React, { PropTypes } from 'react';
+import wrapDisplayName from 'recompose/wrapDisplayName';
+
+/**
+ * A high-order component that provides an easy access to
+ * opening and closing a modal
+ */
+export default (): Function => (Component: ReactClass<*>): ReactClass<*> => {
+  class ComponentWithModal extends React.Component {
+    static contextTypes = {
+      openModal: PropTypes.func.isRequired,
+      closeModal: PropTypes.func.isRequired,
+    };
+
+    modal: ?ReactClass<*> = null;
+
+    handleOpenModal: Function = (Modal: ReactClass<*>): void => {
+      this.modal = Modal;
+
+      this.context.openModal(this.modal);
+    };
+
+    handleCloseModal: Function = (): void => {
+      this.context.closeModal(this.modal);
+    };
+
+    render() {
+      return (
+        <Component
+          openModal={this.handleOpenModal}
+          closeModal={this.handleCloseModal}
+          {...this.props}
+        />
+      );
+    }
+  }
+
+  ComponentWithModal.displayName = wrapDisplayName(Component, 'hasModal');
+
+  return ComponentWithModal;
+};
+
