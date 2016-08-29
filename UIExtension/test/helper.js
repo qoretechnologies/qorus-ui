7797c3a1f8e4a7446071c526c1fbdb2f47c6846f
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-08-29 12:37:12 +0500

    Add simple fake extension

---
+++ UIExtension/test/helper.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+(function() {
+  var button = document.getElementById('simpleButton');
+  button.addEventListener('click', function() {
+    var information = document.getElementById('information');
+
+    if (!this.counter) {
+      this.counter = 0;
+    }
+
+    console.log('New event');
+    information.textContent = 'it works! ' + ++this.counter;
+
+  });
+})();
+/* eslint-enable */
