a3f5fbedd003db9c3898db2e6776ef8fdf13a361
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-09-06 15:58:17 +0500

    Use multiple select query

---
+++ src/js/constants/jobs.js
@@ -0,0 +1,6 @@
+export const JOB_STATES =[
+  { name: 'COMPLETE', label: 'success', title: 'Complete' },
+  { name: 'ERROR', label: 'error', title: 'Error' },
+  { name: 'IN-PROGRESS', label: 'in progress', title: 'In-progress' },
+  { name: 'CRASH', label: 'crash', title: 'Crash' },
+];
