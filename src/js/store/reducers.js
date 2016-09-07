f5eb37a6dd93972bfa1663038bd8d5966fa7ee06
Filip Witosz <filip.witosz@gmail.com>
2016-09-07 17:03:36 +0200

    Added the websockets and log reducers to store.

--- src/js/store/reducers.js
+++ src/js/store/reducers.js
@@ -4,10 +4,14 @@ import { reducer as formReducer } from 'redux-form';
 import menu from './menu';
 import api from './api';
 import ui from './ui';
+import ws from './websockets';
+import log from './log';
 
 export default combineReducers({
   menu,
   api,
   ui,
+  ws,
+  log,
   form: formReducer,
 });
