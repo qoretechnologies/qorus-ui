409c91967213818da491fb27a365438d24b3169b
Filip Witosz <filip.witosz@gmail.com>
2016-11-04 20:02:54 +0100

    No need for the compose function.

--- src/js/views/job/tabs/log/index.jsx
+++ src/js/views/job/tabs/log/index.jsx
@@ -1,6 +1,5 @@
 /* @flow */
 import React from 'react';
-import compose from 'recompose/compose';
 import withHandlers from 'recompose/withHandlers';
 
 import LogContainer from '../../../../containers/log';
@@ -12,18 +11,16 @@ const JobLog = ({ job, ...other }: { job: Object }) => (
   />
 );
 
-export default compose(
-  withHandlers({
-    heightUpdater: (): Function => (): number => {
-      const navbar = document.querySelector('.navbar').clientHeight;
-      const footer = document.querySelector('footer').clientHeight;
-      const header = document.querySelector('.job-header').clientHeight;
-      const desc = document.querySelector('.job-description').clientHeight;
-      const tabs = document.querySelector('.nav-tabs').clientHeight;
-      const top = navbar + footer + header + + desc + tabs;
+export default withHandlers({
+  heightUpdater: (): Function => (): number => {
+    const navbar = document.querySelector('.navbar').clientHeight;
+    const footer = document.querySelector('footer').clientHeight;
+    const header = document.querySelector('.job-header').clientHeight;
+    const desc = document.querySelector('.job-description').clientHeight;
+    const tabs = document.querySelector('.nav-tabs').clientHeight;
+    const top = navbar + footer + header + desc + tabs;
 
-      return window.innerHeight - top;
-    },
-  })
-)(JobLog);
+    return window.innerHeight - top;
+  },
+})(JobLog);
 
