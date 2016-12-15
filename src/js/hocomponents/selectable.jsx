ae8cb8b8111c3a7226ced4c8f2196e02cffbdbc3
Filip Witosz <filip.witosz@gmail.com>
2016-12-15 15:12:29 +0100

    High order component for tables with selectable rows. Provides a selected string which can be 'none', 'some' or 'all', also provides array of the selected IDs. This HOC works with the new selecting implementation that works through our store by adding the _selected key to the objects.

---
+++ src/js/hocomponents/selectable.jsx
@@ -0,0 +1,26 @@
+// @flow
+import React from 'react';
+import setDisplayName from 'recompose/setDisplayName';
+
+import { selectedType } from '../helpers/resources';
+
+export default (
+  collection: string,
+): Function => (Component: ReactClass<*>): ReactClass<*> => {
+  const withSelectable: Function = (props: Object): React.Element<any> => {
+    const selected: string = selectedType(props[collection]);
+    const selectedIds: Array<number> = props[collection].map((item: Object) => (
+      item._selected ? item.id : null
+    )).filter((item: Object) => item);
+
+    return (
+      <Component
+        selected={selected}
+        selectedIds={selectedIds}
+        {...props}
+      />
+    );
+  };
+
+  return setDisplayName('withSelectable')(withSelectable);
+};
