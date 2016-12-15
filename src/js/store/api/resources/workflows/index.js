c71ef539a6afa419997aa364e08f2b3156a49916
Filip Witosz <filip.witosz@gmail.com>
2016-12-15 15:18:13 +0100

    Adds _updated by default so when the setUpdatedToNull funciton runs, all the tables row are not updated ( as the function adds _updated = null which triggers a re-render).

--- src/js/store/api/resources/workflows/index.js
+++ src/js/store/api/resources/workflows/index.js
@@ -3,6 +3,7 @@ import { ORDER_STATES } from '../../../../constants/orders';
 const defaults = {};
 
 defaults.TOTAL = 0;
+defaults._updated = null;
 ORDER_STATES.forEach(val => { defaults[val.name] = 0; });
 
 export const DEFAULTS = defaults;
