19f2e3eb57acaf31a26f4fa430e28b4a3831f186
Filip Witosz <filip.witosz@gmail.com>
2017-01-28 19:27:43 +0100

    Returns 'none' if there are no data to compare.

--- src/js/helpers/resources.js
+++ src/js/helpers/resources.js
@@ -1,5 +1,7 @@
 // @flow
 const selectedType: Function = (collection: Array<Object>): string => {
+  if (!collection || collection.length === 0) return 'none';
+
   if (collection.every((w: Object): boolean => w._selected)) {
     return 'all';
   } else if (collection.some((w: Object): boolean => w._selected)) {
