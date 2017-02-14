7546b94fea97324e4c8add93d21627f8418b71fc
Filip Witosz <filip.witosz@gmail.com>
2017-02-14 15:06:23 +0100

    Login has been rewritten to use stateless component.

--- src/js/views/auth/index.jsx
+++ src/js/views/auth/index.jsx
@@ -1,7 +1,11 @@
 // @flow
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Transition from 'react-addons-css-transition-group';
+import getContext from 'recompose/getContext';
+import withHandlers from 'recompose/withHandlers';
+import compose from 'recompose/compose';
+import pure from 'recompose/onlyUpdateForKeys';
 
 import { CenterWrapper } from '../../components/layout';
 import actions from '../../store/api/actions';
@@ -10,60 +14,63 @@ import SystemInfo from '../../containers/system_info';
 import { auth } from '../../helpers/user';
 import logo from '../../../img/qore_logo.png';
 
-class Login extends Component {
-  static contextTypes = {
+type Props = {
+  location: any,
+  sendAuthCredentials: Function,
+  info: Object,
+  router: Object,
+  handleSubmit: Function,
+};
+
+const Login: Function = ({
+  info,
+  handleSubmit,
+}: Props): React.Element<any> => (
+  <div className="full-background">
+    <CenterWrapper>
+      <Transition
+        transitionName="bubble"
+        transitionAppear
+        transitionAppearTimeout={500000}
+        transitionEnter={false}
+        transitionLeave={false}
+        component="div"
+      >
+        <div className="login-wrapper">
+          <h1 className="login-header">
+            <img src={logo} />
+            Qorus Integration Engine
+            <p className="login-instance-wrapper">
+              <span> {info['instance-key']} </span>
+            </p>
+          </h1>
+          <LoginForm onSubmit={handleSubmit} />
+        </div>
+      </Transition>
+    </CenterWrapper>
+    <SystemInfo info={info} />
+  </div>
+);
+
+export default compose(
+  connect(
+    state => ({ info: state.api.info.data }),
+    actions.auth
+  ),
+  getContext({
     router: PropTypes.object,
-  };
+  }),
+  withHandlers({
+    handleSubmit: ({
+      location,
+      router,
+      sendAuthCredentials,
+    }: Props): Function => async ({ login, password }: Object): ?Promise<*> => {
+      await auth(login, password, sendAuthCredentials);
 
-  props: {
-    location: any,
-    sendAuthCredentials: () => Promise<*>,
-    info: Object,
-  };
-
-  handleSubmit = async (
-    { login, password }: { login: string, password: string }
-  ): Promise<*> => {
-    await auth(login, password, this.props.sendAuthCredentials);
-
-    const { router } = this.context;
-    const { location } = this.props;
-
-    const nextUrl = location.query.next || '/';
-    router.push(nextUrl);
-  };
-
-  render() {
-    return (
-      <div className="full-background">
-        <CenterWrapper>
-          <Transition
-            transitionName="bubble"
-            transitionAppear
-            transitionAppearTimeout={500000}
-            transitionEnter={false}
-            transitionLeave={false}
-            component="div"
-          >
-            <div className="login-wrapper">
-              <h1 className="login-header">
-                <img src={logo} />
-                Qorus Integration Engine
-                <p className="login-instance-wrapper">
-                  <span> {this.props.info['instance-key']} </span>
-                </p>
-              </h1>
-              <LoginForm onSubmit={this.handleSubmit} />
-            </div>
-          </Transition>
-        </CenterWrapper>
-        <SystemInfo info={this.props.info}/>
-      </div>
-    );
-  }
-}
-
-export default connect(
-  state => ({ info: state.api.info.data }),
-  actions.auth
+      const nextUrl = location.query.next || '/';
+      router.push(nextUrl);
+    },
+  }),
+  pure(['location', 'info'])
 )(Login);
