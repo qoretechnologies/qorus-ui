cd5bb8ce7513e9b62b411651ed998dcc41fc5e97
Filip Witosz <filip.witosz@gmail.com>
2017-02-22 10:36:40 +0100

    The Td component now supports colspan prop.

--- src/js/components/new_table/td.jsx
+++ src/js/components/new_table/td.jsx
@@ -6,10 +6,14 @@ import compose from 'recompose/compose';
 type Props = {
   className: string,
   children: any,
+  colspan: number,
 }
 
-const Td: Function = ({ className, children }: Props): React.Element<any> => (
-  <td className={className}>
+const Td: Function = ({ colspan, className, children }: Props): React.Element<any> => (
+  <td
+    colSpan={colspan}
+    className={className}
+  >
     { children }
   </td>
 );
@@ -18,5 +22,6 @@ export default compose(
   updateOnlyForKeys([
     'className',
     'children',
+    'colspan',
   ])
 )(Td);
