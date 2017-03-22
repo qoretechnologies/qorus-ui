51f62fc325d544301aab76cfafb83d2cac740bf2
Filip Witosz <filip.witosz@gmail.com>
2017-03-22 10:35:22 +0100

    Update workflows_toolbar.js fixed typo

--- src/js/views/toolbars/workflows_toolbar.js
+++ src/js/views/toolbars/workflows_toolbar.js
@@ -65,9 +65,9 @@ define(function (require) {
     },
 
     setUrl: function (params) {
-      console.log(utils.getCurrentLoctionPath());
-      console.log(utils.getCurrentLoctionPath().slice(1));
-      console.log(utils.getCurrentLoctionPath().slice(2));
+      console.log(utils.getCurrentLocationPath());
+      console.log(utils.getCurrentLocationPath().slice(1));
+      console.log(utils.getCurrentLocationPath().slice(2));
       var path = utils.getCurrentLocationPath().slice(1);
       var parts = path.split('/');
       var date = (parts.length > 2) ? parts[1] : '24h';
