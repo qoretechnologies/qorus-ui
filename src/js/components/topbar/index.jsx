569de89cdc14b492d6e9d2a6b4cf0c103f70d032
Filip Witosz <filip.witosz@gmail.com>
2017-03-23 11:00:18 +0100

    The default react UI shouldn't have the go to old ui button.

--- src/js/components/topbar/index.jsx
+++ src/js/components/topbar/index.jsx
@@ -5,7 +5,6 @@ import { pureRender } from '../utils';
 import UserInfo from '../../containers/user_info';
 import NotificationPanel from '../../containers/system/alerts';
 import { LocalHealth, RemoteHealth } from '../../containers/health';
-import { Control as Button } from '../../components/controls';
 
 /**
  * Display info about Qorus instance and logged in user.
@@ -30,11 +29,6 @@ export default class Topbar extends Component {
   handleExpand = () => {
     this.setState({ expanded: !this.state.expanded });
   };
-
-  handleSwitchClick = () => {
-    document.cookie = 'backbone=true; expires=Tue, 31 Dec 2030 20:47:11 UTC; path=/';
-    window.location.href = '/backbone/';
-  }
 
   /**
    * Returns element for this component.
@@ -75,15 +69,6 @@ export default class Topbar extends Component {
             aria-expanded={this.state.expanded ? 'true' : 'false'}
           >
             <div className="nav nav-bar navbar-right info-nav">
-              <div className="nav-btn-tooltip">
-                <Button
-                  big
-                  btnStyle="warning"
-                  onClick={this.handleSwitchClick}
-                  label="Switch to old UI"
-                  icon="warning"
-                />
-              </div>
               {' '}
               <LocalHealth />
               {' '}
