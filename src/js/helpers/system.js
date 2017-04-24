efb33b6a7811f0b42033ec5f84de41ea8b250d69
Filip Witosz <filip.witosz@gmail.com>
2017-04-24 13:54:55 +0200

    Connections pane now shows Alerts and Dependencies. Closes #1911

--- src/js/helpers/system.js
+++ src/js/helpers/system.js
@@ -56,6 +56,29 @@ const alertTypeToResource: Object = {
   },
 };
 
+const interfaceTypeToResource: Object = {
+  WORKFLOW: {
+    resource: 'workflows',
+    uses: 'workflowid',
+    query: 'paneId',
+  },
+  SERVICE: {
+    resource: 'services',
+    uses: 'serviceid',
+    query: 'paneId',
+  },
+  JOB: {
+    resource: 'jobs',
+    uses: 'jobid',
+    query: 'paneId',
+  },
+  user: {
+    resource: 'services',
+    uses: 'serviceid',
+    query: 'paneId',
+  },
+};
+
 const getAlertObjectLink: Function = (type: string, data: Object): string => {
   const res = alertTypeToResource[type];
 
@@ -63,8 +86,16 @@ const getAlertObjectLink: Function = (type: string, data: Object): string => {
   return `/${res.resource}${res.query ? `?${res.query}=${data[res.uses]}` : `/${data[res.uses]}`}${res.suffix || ''}`;
 };
 
+const getDependencyObjectLink: Function = (type: string, data: Object): string => {
+  const res = interfaceTypeToResource[type];
+
+  // eslint-disable-next-line
+  return `/${res.resource}${res.query ? `?${res.query}=${data[res.uses]}` : `/${data[res.uses]}`}`;
+};
+
 export {
   statusHealth,
   utf8ToB64,
   getAlertObjectLink,
+  getDependencyObjectLink,
 };
