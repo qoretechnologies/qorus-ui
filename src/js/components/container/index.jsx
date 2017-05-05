bc8f8e46dd4bd9febd610eb1563060f3de550260
Filip Witosz <filip.witosz@gmail.com>
2017-05-05 12:21:49 +0200

    Created new component that wraps views content inside and creates a responsible wrapper that is resized along with the window.

---
+++ src/js/components/container/index.jsx
@@ -0,0 +1,65 @@
+// @flow
+import React, { Component } from 'react';
+import pure from 'recompose/onlyUpdateForKeys';
+
+type Props = {
+  children?: any,
+
+};
+
+@pure(['children'])
+export default class Container extends Component {
+  props: Props;
+
+  state: {
+    height?: number,
+  } = {
+    height: 0,
+  };
+
+  componentDidMount(): void {
+    if (this._el) {
+      this.handleResize();
+
+      window.addEventListener('resize', this.handleResize);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  _el: ?Object = null;
+
+  handleRef: Function = (el: Object) => {
+    if (el) {
+      this._el = el;
+    }
+  }
+
+  handleResize: Function = () => {
+    if (this._el) {
+      const { top } = this._el.getBoundingClientRect();
+      const winHeight: number = window.innerHeight;
+      const height: number = winHeight - top - 60;
+
+      this.setState({
+        height,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div
+        ref={this.handleRef}
+        className="container-resizable"
+        style={{
+          height: this.state.height,
+        }}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
