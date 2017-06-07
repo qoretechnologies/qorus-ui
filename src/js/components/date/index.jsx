8ec225ea4de48d168e6e7bd451d04af1ef6c0ca0
Filip Witosz <filip.witosz@gmail.com>
2017-06-07 08:35:45 +0200

    Rewritten date component.

--- src/js/components/date/index.jsx
+++ src/js/components/date/index.jsx
@@ -1,5 +1,6 @@
 /* @flow */
 import React from 'react';
+import pure from 'recompose/onlyUpdateForKeys';
 import moment from 'moment';
 
 import { DATE_FORMATS } from '../../constants/dates';
@@ -9,16 +10,13 @@ type Props = {
   format?: string,
 }
 
-export default function DateComponent(props: Props): React.Element<any> {
-  if (props.date) {
-    return (
-      <span>{ moment(props.date).format(props.format) }</span>
-    );
-  }
+const DateComponent: Function = ({
+  date,
+  format: format = DATE_FORMATS.DISPLAY,
+}: Props): React.Element<any> => (
+  date ?
+    <span>{ moment(date).format(format) }</span> :
+    <span />
+);
 
-  return <span />;
-}
-
-DateComponent.defaultProps = {
-  format: DATE_FORMATS.DISPLAY,
-};
+export default pure(['date', 'format'])(DateComponent);
