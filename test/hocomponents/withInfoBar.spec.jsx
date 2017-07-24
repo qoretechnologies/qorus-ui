bd5d5fbcc44f33bae7aa53b9b787db5d79ed6082
Filip Witosz <filip.witosz@gmail.com>
2017-07-24 11:43:05 +0200

    Added a high order component that can be used in jonjunction with the infobar component. This HoC gathers common data from collections such as the number of items with alerts, the number of enabled items etc.

---
+++ test/hocomponents/withInfoBar.spec.jsx
@@ -0,0 +1,47 @@
+import React from 'React';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+
+import withInfoBar from '../../src/js/hocomponents/withInfoBar';
+
+describe('withInfoBar from "hocomponents/withInfoBar"', () => {
+  const ActualComp = (props: Object): React.Element<any> => (
+    <div>
+      {props.infoTotalCount}
+      {props.infoWithAlerts}
+      {props.infoEnabled}
+    </div>
+  );
+
+  const testData = [
+    {
+      has_alerts: true,
+      enabled: true,
+    },
+    {
+      has_alerts: true,
+      enabled: true,
+    },
+    {
+      has_alerts: true,
+      enabled: true,
+    },
+    {
+      has_alerts: true,
+      enabled: false,
+    },
+    {
+      has_alerts: false,
+      enabled: false,
+    },
+  ];
+
+  it('renders the component with all props', () => {
+    const Comp = withInfoBar('testData')(ActualComp);
+    const wrapper = mount(
+      <Comp {...{ testData }} />
+    );
+
+    expect(wrapper.find('div').text()).to.equal('543');
+  });
+});
