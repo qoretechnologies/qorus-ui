4aa13d3c35bd419fc6d6766f41769ffbbb8612ea
Filip Witosz <filip.witosz@gmail.com>
2017-09-04 13:17:18 +0200

    Added a manual link to the "next" destination. Increased the check interval to 30 seconds.

--- src/js/error.jsx
+++ src/js/error.jsx
@@ -3,21 +3,30 @@ import React from 'react';
 import Icon from './components/icon';
 import lifecycle from 'recompose/lifecycle';
 import compose from 'recompose/compose';
+import mapProps from 'recompose/mapProps';
 
 import { fetchResponse } from './store/api/utils';
 import settings from './settings';
 
-const ErrorView: Function = (): React.Element<any> => (
+type Props = {
+  next: string,
+  location: Object,
+};
+
+const ErrorView: Function = ({ next }: Props): React.Element<any> => (
   <div className="center-wrapper default-wrapper">
     <h3 className="main-error-text"><Icon icon="warning" /> Oooops... </h3>
     <div className="error-wrapper">
       The Qorus server cannot be reached at the moment. We will reload the page once the server
-      becomes available again.
+      becomes available again. <a href={next}>Reload manually</a>
     </div>
   </div>
 );
 
 export default compose(
+  mapProps(({ location: { query: { next } } }: Props) => ({
+    next: !next || next === '' || next === '/error' ? '/' : next,
+  })),
   lifecycle({
     componentDidMount() {
       setInterval(async () => {
@@ -29,12 +38,12 @@ export default compose(
           false
         );
 
-        const { next } = this.props.location.query;
+        const { next } = this.props;
 
         if (res.status !== 500) {
           window.location.href = !next || next === '' || next === '/error' ? '/' : next;
         }
-      }, 5000);
+      }, 30000);
     },
   }),
 )(ErrorView);
