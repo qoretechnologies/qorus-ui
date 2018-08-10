d45e21aba69bcb19ff64a35efe8176472a9c2fd9
Filip Witosz <filip.witosz@gmail.com>
2018-08-10 15:57:13 +0200

    Fixed modal opening & resizing issue. Closes #2322.

--- src/js/views/services/detail/tabs/methods/modal_run.jsx
+++ src/js/views/services/detail/tabs/methods/modal_run.jsx
@@ -8,7 +8,6 @@ import settings from '../../../../../settings';
 import { fetchJson } from '../../../../../store/api/utils';
 import { pureRender } from '../../../../../components/utils';
 
-
 @pureRender
 export default class ModalRun extends Component {
   static propTypes = {
@@ -39,12 +38,12 @@ export default class ModalRun extends Component {
   _request = null;
   _response = null;
 
-  handleCancel = (ev) => {
+  handleCancel = ev => {
     ev.preventDefault();
     this.props.onClose();
   };
 
-  handleCommit = (ev) => {
+  handleCommit = ev => {
     const request = this._request.value;
     const { service } = this.props;
     const { activeMethod } = this.state;
@@ -57,8 +56,7 @@ export default class ModalRun extends Component {
       `${settings.REST_BASE_URL}/services/${service.name}/${activeMethod.name}`,
       { body: JSON.stringify({ action: 'call', parse_args: request }) },
       true
-    ).
-    then(response => {
+    ).then(response => {
       if (!response) {
         this.setState({
           error: 'Server returned null',
@@ -77,35 +75,39 @@ export default class ModalRun extends Component {
     });
   };
 
-  handleMethodChange = (ev) => {
+  handleMethodChange = ev => {
     this.setState({
-      activeMethod: this.props.service.methods.find(m => m.name === ev.target.value),
+      activeMethod: this.props.service.methods.find(
+        m => m.name === ev.target.value
+      ),
     });
   };
 
   handleModalResize = () => {
     if (this._request && this._response) {
-      const { height: formHeight } = document.
-        querySelectorAll('.method-form')[0].getBoundingClientRect();
-      const { height: bodyHeight } = document.
-        querySelectorAll('.modal-body')[0].getBoundingClientRect();
+      const { height: formHeight } = document
+        .querySelectorAll('.method-form')[0]
+        .getBoundingClientRect();
+      const { height: bodyHeight } = document
+        .querySelectorAll('.pt-dialog-body')[0]
+        .getBoundingClientRect();
       const difference = bodyHeight - formHeight;
 
       const { height: requestHeight } = this._request.getBoundingClientRect();
       const { height: responseHeight } = this._response.getBoundingClientRect();
 
       this.setState({
-        requestHeight: requestHeight + (difference / 2) - 70,
-        responseHeight: responseHeight + (difference / 2) - 70,
+        requestHeight: requestHeight + difference / 2 - 70,
+        responseHeight: responseHeight + difference / 2 - 70,
       });
     }
   };
 
-  requestRef = (s) => {
+  requestRef = s => {
     this._request = s;
   };
 
-  selectRef = (s) => {
+  selectRef = s => {
     this._select = s;
   };
 
@@ -122,20 +124,20 @@ export default class ModalRun extends Component {
         ref={this.selectRef}
         className="form-control"
       >
-        { service.methods.map((mtd, idx) => (
-            <option value={mtd.name} key={idx}>
-              { mtd.name }
-            </option>
+        {service.methods.map((mtd, idx) => (
+          <option value={mtd.name} key={idx}>
+            {mtd.name}
+          </option>
         ))}
       </select>
     );
   }
 
-  respRef = (el) => {
+  respRef = el => {
     if (el) {
       this._response = el;
     }
-  }
+  };
 
   /**
    * @return {ReactElement}
@@ -144,51 +146,51 @@ export default class ModalRun extends Component {
     const { response, request, activeMethod, error } = this.state;
 
     return (
-      <Modal
-        hasFooter
-        onResizeStop={this.handleModalResize}
-      >
-          <Modal.Header
-            titleId="errorsTableModalLabel"
-            onClose={this.handleCancel}
-          >
-            Method execution for {this.props.service.name} service
-          </Modal.Header>
-          <Modal.Body>
-            <div className="content">
-              { error && (
-                <Alert bsStyle="danger">
-                  <h4> Error occured </h4>
-                  { error }
-                </Alert>
-              )}
-              <form
-                onSubmit={this.handleCommit}
-                noValidate
-                className="method-form"
-              >
-                <div className="form-group">
-                  <label htmlFor="method">Method</label>
-                  { this.renderOptions() }
+      <Modal hasFooter onResizeStop={this.handleModalResize}>
+        <Modal.Header
+          titleId="errorsTableModalLabel"
+          onClose={this.handleCancel}
+        >
+          Method execution for {this.props.service.name} service
+        </Modal.Header>
+        <Modal.Body>
+          <div className="content">
+            {error && (
+              <Alert bsStyle="danger">
+                <h4> Error occured </h4>
+                {error}
+              </Alert>
+            )}
+            <form
+              onSubmit={this.handleCommit}
+              noValidate
+              className="method-form"
+            >
+              <div className="form-group">
+                <label htmlFor="method">Method</label>
+                {this.renderOptions()}
+              </div>
+              <p>
+                <em className="text-muted">{activeMethod.description}</em>
+              </p>
+              <div ref={this.textWrapRef}>
+                <div style={{ overflow: 'hidden' }}>
+                  <label htmlFor="args">Arguments</label>
+                  <textarea
+                    id="args"
+                    name="args"
+                    className="col-md-12 form-control"
+                    rows="5"
+                    ref={this.requestRef}
+                    defaultValue={request || ''}
+                    style={{
+                      height: this.state.requestHeight || 'auto',
+                    }}
+                  />
                 </div>
-                <p><em className="text-muted">{activeMethod.description}</em></p>
-                <div ref={this.textWrapRef}>
-                  <div>
-                    <label htmlFor="args">Arguments</label>
-                    <textarea
-                      id="args"
-                      name="args"
-                      className="col-md-12 form-control"
-                      rows="5"
-                      ref={this.requestRef}
-                      defaultValue={ request || '' }
-                      style={{
-                        height: this.state.requestHeight || 'auto',
-                      }}
-                    />
-                  </div>
-                  <Tabs type="pills" navAtBottom>
-                    <Pane name="yaml">
+                <div>
+                  <Tabs active="yaml" noContainer>
+                    <Pane name="YAML">
                       <textarea
                         id="response-yaml"
                         name="response-yaml"
@@ -197,13 +199,13 @@ export default class ModalRun extends Component {
                         readOnly
                         rows="5"
                         ref={this.respRef}
-                        value={ (response) ? yaml.dump(response) : ''}
+                        value={response ? yaml.dump(response) : ''}
                         style={{
                           height: this.state.responseHeight || 'auto',
                         }}
                       />
                     </Pane>
-                    <Pane name="json">
+                    <Pane name="JSON">
                       <textarea
                         id="response-json"
                         name="response-json"
@@ -211,7 +213,9 @@ export default class ModalRun extends Component {
                         placeholder="Response"
                         readOnly
                         rows="5"
-                        value={ (response) ? JSON.stringify(response, null, 4) : ''}
+                        value={
+                          response ? JSON.stringify(response, null, 4) : ''
+                        }
                         style={{
                           height: this.state.responseHeight || 'auto',
                         }}
@@ -219,24 +223,22 @@ export default class ModalRun extends Component {
                     </Pane>
                   </Tabs>
                 </div>
-              </form>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <button
-              className="btn"
-              onClick={this.handleCancel}
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="btn btn-success"
-              onClick={this.handleCommit}
-            >
-              Call
-            </button>
-          </Modal.Footer>
+              </div>
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn" onClick={this.handleCancel}>
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="btn btn-success"
+            onClick={this.handleCommit}
+          >
+            Call
+          </button>
+        </Modal.Footer>
       </Modal>
     );
   }
