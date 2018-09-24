a829ed3a9fe4eb23bba72690c6cd5d9146d12f47
Filip Witosz <filip.witosz@gmail.com>
2018-09-24 09:39:26 +0200

    Fixed the remote health WS events. Closes #2420.

--- src/js/store/api/resources/health/actions.js
+++ src/js/store/api/resources/health/actions.js
@@ -1,1 +1,8 @@
+import { createAction } from 'redux-actions';
 
+const remoteHealthChanged: Function = createAction(
+  'HEALTH_CHANGED',
+  events => ({ events })
+);
+
+export { remoteHealthChanged };
