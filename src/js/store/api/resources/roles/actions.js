baca259912d350e08581b51bbf9c2bab08070291
Filip Witosz <filip.witosz@gmail.com>
2018-10-09 09:33:41 +0200

    Roles now support user visual feedback.

--- src/js/store/api/resources/roles/actions.js
+++ src/js/store/api/resources/roles/actions.js
@@ -1,145 +1,111 @@
 /* @flow */
 import { createAction } from 'redux-actions';
-import { fetchJson } from '../../utils';
+import { fetchJson, fetchWithNotifications } from '../../utils';
 import settings from '../../../../settings';
 import { unSyncUsers } from '../users/actions';
 import { unSyncCurrentUser } from '../currentUser/actions';
 import actions from '../../actions';
 
-const createOptimistic: Function = (
-  role: string,
-  desc: string,
-  perms: Array<string>,
-  groups: Array<string>
-): Object => ({
-  role,
-  desc,
-  perms,
-  groups,
-});
-
-const create: Function = (
-  role: string,
-  desc: string,
-  perms: Array<string>,
-  groups: Array<string>,
-): Object => (
-  fetchJson(
-    'POST',
-    `${settings.REST_BASE_URL}/roles/`,
-    {
-      body: JSON.stringify({
+const create: Function = createAction(
+  'ROLES_CREATE',
+  async (
+    role: string,
+    desc: string,
+    perms: Array<string>,
+    groups: Array<string>,
+    dispatch: Function
+  ): ?Object => {
+    if (!dispatch) {
+      return {
         role,
         desc,
         perms,
         groups,
-      }),
+      };
     }
-  )
+
+    await fetchWithNotifications(
+      async () =>
+        await fetchJson('POST', `${settings.REST_BASE_URL}/roles/`, {
+          body: JSON.stringify({
+            role,
+            desc,
+            perms,
+            groups,
+          }),
+        }),
+      'Creating role...',
+      'Role successfuly created',
+      dispatch
+    );
+
+    dispatch(unSyncUsers());
+    dispatch(unSyncCurrentUser());
+    dispatch(actions.currentUser.fetch());
+  }
 );
 
-const removeOptimistic: Function = (role: string) => ({ role });
-const remove: Function = (role: string): Object => (
-  fetchJson(
-    'DELETE',
-    `${settings.REST_BASE_URL}/roles/${role}`
-  )
+const remove: Function = createAction(
+  'ROLES_REMOVE',
+  async (role: string, dispatch: Function): ?Object => {
+    if (!dispatch) {
+      return { role };
+    }
+
+    await fetchWithNotifications(
+      async () =>
+        await fetchJson('DELETE', `${settings.REST_BASE_URL}/roles/${role}`),
+      'Deleting role...',
+      'Role successfuly deleted',
+      dispatch
+    );
+
+    dispatch(unSyncUsers());
+    dispatch(unSyncCurrentUser());
+    dispatch(actions.currentUser.fetch());
+  }
 );
 
-const updateOptimistic = (
-  role: string, desc: string, perms: Array<string>, groups: Array<string>
-): Object => ({
-  desc,
-  role,
-  perms,
-  groups,
-});
-const update: Function = (
-  role: string, desc: string, perms: Array<string>, groups: Array<string>
-): Object => (
-  fetchJson(
-    'PUT',
-    `${settings.REST_BASE_URL}/roles/${role}`,
-    {
-      body: JSON.stringify({
+const update: Function = createAction(
+  'ROLES_UPDATE',
+  async (
+    role: string,
+    desc: string,
+    perms: Array<string>,
+    groups: Array<string>,
+    dispatch: Function
+  ): ?Object => {
+    if (!dispatch) {
+      return {
         desc,
         role,
         perms,
         groups,
-      }),
+      };
     }
-  )
+
+    await fetchWithNotifications(
+      async () =>
+        await fetchJson('PUT', `${settings.REST_BASE_URL}/roles/${role}`, {
+          body: JSON.stringify({
+            desc,
+            role,
+            perms,
+            groups,
+          }),
+        }),
+
+      'Updating role...',
+      'Role successfuly updated',
+      dispatch
+    );
+
+    dispatch(unSyncUsers());
+    dispatch(unSyncCurrentUser());
+    dispatch(actions.currentUser.fetch());
+  }
 );
 
-const createOptimisticCall: Function = createAction(
-  'ROLES_CREATEROLEOPTIMISTIC',
-  createOptimistic
-);
-const createCall: Function = createAction(
-  'ROLES_CREATEROLE',
-  create
-);
+const unSyncRoles: Function = createAction('USERS_UNSYNCROLES');
 
-const removeOptimisticCall: Function = createAction(
-  'ROLES_REMOVEROLEOPTIMISTIC',
-  removeOptimistic
-);
-const removeCall: Function = createAction(
-  'ROLES_REMOVEROLE',
-  remove
-);
-
-const updateOptimisticCall: Function = createAction(
-  'ROLES_UPDATEROLEOPTIMISTIC',
-  updateOptimistic
-);
-const updateCall: Function = createAction(
-  'ROLES_UPDATEROLE',
-  update
-);
-
-const unSyncRoles: Function = createAction(
-  'USERS_UNSYNCROLES'
-);
-
-const createRole: Function = (
-  role: string, desc: string, perms: Array<string>, groups: Array<string>
-) => dispatch => {
-  dispatch(createCall(role, desc, perms, groups));
-  dispatch(createOptimisticCall(role, desc, perms, groups));
-  dispatch(unSyncUsers());
-  dispatch(unSyncCurrentUser());
-  dispatch(actions.currentUser.fetch());
-};
-
-const updateRole: Function = (
-  role: string, desc: string, perms: Array<string>, groups: Array<string>
-) => dispatch => {
-  dispatch(updateCall(role, desc, perms, groups));
-  dispatch(updateOptimisticCall(role, desc, perms, groups));
-  dispatch(unSyncUsers());
-  dispatch(unSyncCurrentUser());
-  dispatch(actions.currentUser.fetch());
-};
-
-const removeRole: Function = (role: string) => dispatch => {
-  dispatch(removeCall(role));
-  dispatch(removeOptimisticCall(role));
-  dispatch(unSyncUsers());
-  dispatch(unSyncCurrentUser());
-  dispatch(actions.currentUser.fetch());
-};
-
-const removeRoleOptimistic: Function = () => () => true;
-const createRoleOptimistic: Function = () => () => true;
-const updateRoleOptimistic: Function = () => () => true;
-
-export {
-  createRole,
-  createRoleOptimistic,
-  removeRole,
-  removeRoleOptimistic,
-  updateRole,
-  updateRoleOptimistic,
-  unSyncRoles,
-};
+export { create, remove, update, unSyncRoles };
