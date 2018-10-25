b79902f07c30a368d6f2d5a529b1a6340db783e8
Filip Witosz <filip.witosz@gmail.com>
2018-10-26 01:34:59 +0200

    SQLCache page now uses new headbar and new expandable item components.

--- src/js/views/system/sqlcache/table.jsx
+++ src/js/views/system/sqlcache/table.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import compose from 'recompose/compose';
 import pure from 'recompose/onlyUpdateForKeys';
-import withState from 'recompose/withState';
-import withHandlers from 'recompose/withHandlers';
-import { Button, Intent } from '@blueprintjs/core';
 
 import { Table, Tbody, Thead, Tr, Th } from '../../../components/new_table';
-import { Breadcrumbs, Crumb } from '../../../components/breadcrumbs';
 import CacheRow from './row';
+import Pull from '../../../components/Pull';
+import NoDataIf from '../../../components/NoDataIf';
+import ExpandableItem from '../../../components/ExpandableItem';
+import { Control as Button } from '../../../components/controls';
+import Headbar from '../../../components/Headbar';
 
 type Props = {
   name: string,
@@ -26,66 +27,54 @@ const SQLCacheTable: Function = ({
   data,
   onClick,
   onSingleClick,
-  expanded,
-  handleExpandClick,
 }: Props): React.Element<any> => {
   const handleClick: Function = (): void => {
     onClick(name);
   };
 
   return (
-    <div>
-      <Breadcrumbs onClick={handleExpandClick}>
-        <Crumb active={expanded}>{name}</Crumb>
-      </Breadcrumbs>
-      <div className="pull-right">
-        <Button
-          intent={Intent.DANGER}
-          text="Clear datasource"
-          iconName="trash"
-          onClick={handleClick}
-          className="pt-small"
-        />
-      </div>
-
-      {Object.keys(data).length > 0 && expanded ? (
-        <Table condensed striped>
-          <Thead>
-            <Tr>
-              <Th className="name">Name</Th>
-              <Th className="narrow">Count</Th>
-              <Th className="big">Created</Th>
-              <Th className="narrow"> Actions </Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {Object.keys(data).map((cache, index) => (
-              <CacheRow
-                key={index}
-                datasource={name}
-                name={cache}
-                count={data[cache].count}
-                created={data[cache].created}
-                onClick={onSingleClick}
-              />
-            ))}
-          </Tbody>
-        </Table>
-      ) : null}
-      {Object.keys(data).length <= 0 && <p className="no-data"> No data </p>}
-    </div>
+    <ExpandableItem title={name} show>
+      <NoDataIf condition={Object.keys(data).length <= 0}>
+        {() => (
+          <div>
+            <Headbar>
+              <Pull right>
+                <Button
+                  text="Clear datasource"
+                  iconName="trash"
+                  onClick={handleClick}
+                  btnStyle="danger"
+                  big
+                />
+              </Pull>
+            </Headbar>
+            <Table condensed striped>
+              <Thead>
+                <Tr>
+                  <Th className="name">Name</Th>
+                  <Th className="narrow">Count</Th>
+                  <Th className="big">Created</Th>
+                  <Th className="narrow"> Actions </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {Object.keys(data).map((cache, index) => (
+                  <CacheRow
+                    key={index}
+                    datasource={name}
+                    name={cache}
+                    count={data[cache].count}
+                    created={data[cache].created}
+                    onClick={onSingleClick}
+                  />
+                ))}
+              </Tbody>
+            </Table>
+          </div>
+        )}
+      </NoDataIf>
+    </ExpandableItem>
   );
 };
 
-export default compose(
-  withState('expanded', 'setExpanded', true),
-  withHandlers({
-    handleExpandClick: ({
-      expanded,
-      setExpanded,
-    }: Props): Function => (): void => {
-      setExpanded(() => !expanded);
-    },
-  }),
-  pure(['expanded', 'dataLen'])
-)(SQLCacheTable);
+export default compose(pure(['dataLen']))(SQLCacheTable);
