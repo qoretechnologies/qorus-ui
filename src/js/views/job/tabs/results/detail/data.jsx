24c96ccd984fde36e5a36e04b28e37cdec5b7394
Filip Witosz <filip.witosz@gmail.com>
2018-11-05 23:55:34 +0100

    Fixed a bug in the job instance detail pane that prevented it from showing if the info key was not present.

--- src/js/views/job/tabs/results/detail/data.jsx
+++ src/js/views/job/tabs/results/detail/data.jsx
@@ -5,11 +5,14 @@ import ErrorTable from './errors';
 import AuditTable from './audit';
 import Tabs, { Pane } from '../../../../../components/tabs';
 import InfoTable from '../../../../../components/info_table';
+import NoDataIf from '../../../../../components/NoDataIf';
 
 const ResultData = ({ result }: { result: Object }) => (
   <Tabs active="info" noContainer>
     <Pane name="Info">
-      <InfoTable object={result.info} />
+      <NoDataIf condition={!result.info}>
+        {() => <InfoTable object={result.info} />}
+      </NoDataIf>
     </Pane>
     <Pane name="Error">
       <ErrorTable errors={result.errors} />
