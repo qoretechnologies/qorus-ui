4443fc9821af1e8a1fa84ea761301d7f3d087ddb
Filip Witosz <filip.witosz@gmail.com>
2018-11-26 13:50:05 +0100

    Added new tabs to alerts to Alerts with indicators.

--- src/js/views/system/alerts/index.jsx
+++ src/js/views/system/alerts/index.jsx
@@ -1,10 +1,13 @@
 // @flow
 import React from 'react';
+import { createSelector } from 'reselect';
+import { flowRight } from 'lodash';
+import { resourceSelector, querySelector } from '../../../selectors';
+import { connect } from 'react-redux';
 
 import Box from '../../../components/box';
 import { Breadcrumbs, Crumb, CrumbTabs } from '../../../components/breadcrumbs';
 import AlertsTable from './table';
-import Tabs, { Pane } from '../../../components/tabs';
 import withTabs from '../../../hocomponents/withTabs';
 import Headbar from '../../../components/Headbar';
 import Pull from '../../../components/Pull';
@@ -12,8 +15,9 @@ import compose from 'recompose/compose';
 import queryControl from '../../../hocomponents/queryControl';
 import capitalize from 'lodash/capitalize';
 import Search from '../../../containers/search';
-import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
 import { SimpleTabs, SimpleTab } from '../../../components/SimpleTabs';
+import sync from '../../../hocomponents/sync';
+import actions from '../../../store/api/actions';
 
 type Props = {
   location: Object,
@@ -23,20 +27,27 @@ type Props = {
 
 const Alerts: Function = ({
   tabQuery,
-  handleTabChange,
   location,
+  ongoingAlerts,
+  transientAlerts,
   ...rest
 }: Props): React.Element<any> => (
   <div>
     <Headbar>
       <Breadcrumbs>
         <Crumb> Alerts </Crumb>
-        <CrumbTabs tabs={['Ongoing', 'Transient']} defaultTab="ongoing" />
+        <CrumbTabs
+          tabs={[
+            { title: 'Ongoing', suffix: `(${ongoingAlerts.length})` },
+            { title: 'Transient', suffix: `(${transientAlerts.length})` },
+          ]}
+          defaultTab="ongoing"
+        />
       </Breadcrumbs>
       <Pull right>
         <Search
           defaultValue={rest[`${tabQuery}SearchQuery`]}
-          onSearchUpdate={rest[`change${capitalize(tabQuery)}searchQuery`]}
+          onSearchUpdate={rest[`change${capitalize(tabQuery)}SearchQuery`]}
           resource="alerts"
         />
       </Pull>
@@ -44,17 +55,66 @@ const Alerts: Function = ({
     <Box top noPadding>
       <SimpleTabs activeTab={tabQuery}>
         <SimpleTab name="ongoing">
-          <AlertsTable location={location} type="ongoing" />
+          <AlertsTable
+            location={location}
+            type="ongoing"
+            alerts={ongoingAlerts}
+            query={rest[`${tabQuery}SearchQuery`]}
+          />
         </SimpleTab>
         <SimpleTab name="transient">
-          <AlertsTable location={location} type="transient" />
+          <AlertsTable
+            location={location}
+            type="transient"
+            alerts={transientAlerts}
+            query={rest[`${tabQuery}SearchQuery`]}
+          />
         </SimpleTab>
       </SimpleTabs>
     </Box>
   </div>
 );
 
+const filterCollection: Function = (type: string): Function => (
+  alerts: Array<Object>
+): Array<Object> =>
+  alerts.filter(
+    (alert: Object): boolean => alert.alerttype.toLowerCase() === type
+  );
+
+const ongiongCollectionSelector = createSelector(
+  [resourceSelector('alerts')],
+  alerts => flowRight(filterCollection('ongoing'))(alerts.data)
+);
+
+const transientCollectionSelector = createSelector(
+  [resourceSelector('alerts')],
+  alerts => flowRight(filterCollection('transient'))(alerts.data)
+);
+
+const viewSelector = createSelector(
+  [
+    resourceSelector('alerts'),
+    ongiongCollectionSelector,
+    transientCollectionSelector,
+  ],
+  (meta, ongoingAlerts, transientAlerts) => ({
+    meta,
+    ongoingAlerts,
+    transientAlerts,
+  })
+);
+
 export default compose(
   withTabs('ongoing'),
+  queryControl('ongoingSearch'),
+  queryControl('transientSearch'),
+  connect(
+    viewSelector,
+    {
+      load: actions.alerts.fetch,
+    }
+  ),
+  sync('meta'),
   queryControl(({ tabQuery }) => `${tabQuery}Search`)
 )(Alerts);
