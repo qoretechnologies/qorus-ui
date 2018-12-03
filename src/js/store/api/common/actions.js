9d75296300d18c48405afa991b2d0d8d7b23d7b1
Filip Witosz <filip.witosz@gmail.com>
2018-12-03 23:18:18 +0100

    Created new file for common redux actions.

---
+++ src/js/store/api/common/actions.js
@@ -0,0 +1,37 @@
+// @flow
+import { createAction } from 'redux-actions';
+import { fetchWithNotifications, put } from '../utils';
+import settings from '../../../settings';
+
+const updateConfigItemAction: Function = (intfc: string): Function =>
+  createAction(
+    `${intfc}_UPDATE_CONFIG_ITEM`,
+    (
+      id: number | string,
+      configItemName: string,
+      newValue: any,
+      dispatch: Function
+    ): void => {
+      console.log(dispatch);
+
+      const intfcToApiPath: Object = {
+        WORKFLOWS: 'steps',
+        SERVICES: 'services',
+        JOBS: 'jobs',
+      };
+
+      fetchWithNotifications(
+        async (): Promise<*> =>
+          await put(
+            `${settings.REST_BASE_URL}/${
+              intfcToApiPath[intfc]
+            }/${id}/config/${configItemName}?value=${newValue}`
+          ),
+        `Updating value for ${configItemName}...`,
+        `${configItemName} value updated successfuly`,
+        dispatch
+      );
+    }
+  );
+
+export { updateConfigItemAction };
