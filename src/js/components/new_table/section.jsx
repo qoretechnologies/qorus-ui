bb80e38c27f80825b804f17e40e1dab01158fc4e
Filip Witosz <filip.witosz@gmail.com>
2018-12-05 18:43:58 +0100

    The table section no longer needs to update itself with javascript based on the window size calculations!! Big big upgrade in performance and code usability.

--- src/js/components/new_table/section.jsx
+++ src/js/components/new_table/section.jsx
@@ -1,10 +1,11 @@
 /* @flow */
-import React, { Component } from 'react';
+import React from 'react';
 import mapProps from 'recompose/mapProps';
 import updateOnlyForKeys from 'recompose/onlyUpdateForKeys';
 import setDisplayName from 'recompose/setDisplayName';
 import compose from 'recompose/compose';
 import classNames from 'classnames';
+import Flex from '../Flex';
 
 type Props = {
   type: string,
@@ -20,129 +21,69 @@ type Props = {
   hasFooter: boolean,
 };
 
-class Section extends Component {
-  props: Props;
-
-  state: {
-    height: string | number,
-    initHeight: number,
-  } = {
-    height: this.props.height || 'auto',
-    initHeight: 0,
-  };
-
-  componentDidMount() {
-    window.addEventListener('resize', this.adjustHeight);
+let Section: Function = ({
+  type,
+  hover,
+  striped,
+  children,
+  className,
+  fixed,
+  Tag,
+  bordered,
+  height,
+}: Props): React.Element<any> => {
+  if (!fixed) {
+    return <Tag>{children}</Tag>;
   }
 
-  componentWillReceiveProps(nextProps: Props): void {
-    if (this.props.marginBottom !== nextProps.marginBottom) {
-      this.adjustHeight(null, nextProps.marginBottom);
-    }
+  if (type === 'header') {
+    return (
+      <Flex flex="0 1 auto" className="table-header-wrapper">
+        {children}
+      </Flex>
+    );
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.adjustHeight);
-  }
-
-  _el: ?Object = null;
-
-  handleRef: Function = (element: Object): void => {
-    if (!this.props.height && element) {
-      this._el = element;
-      const { height } = this._el.getBoundingClientRect();
-
-      this.setState({
-        initHeight: height,
-      });
-
-      this.adjustHeight(height);
-    }
-  };
-
-  adjustHeight: Function = (
-    initHeight: ?number,
-    marginBottom: ?number
-  ): void => {
-    if (this._el) {
-      const { top } = this._el.getBoundingClientRect();
-      const winHeight = window.innerHeight;
-      const mb: number =
-        marginBottom || marginBottom === 0
-          ? marginBottom
-          : this.props.marginBottom;
-      const footer: number = this.props.hasFooter ? 22 : 0;
-      const height: number = winHeight - top - 37 - mb - footer;
-      const h =
-        typeof initHeight === 'number' ? initHeight : this.state.initHeight;
-
-      if (h > height) {
-        this.setState({
-          height,
-        });
-      }
-    }
-  };
-
-  render() {
-    const {
-      type,
-      hover,
-      striped,
-      children,
-      className,
-      fixed,
-      Tag,
-      bordered,
-    } = this.props;
-    const { height } = this.state;
-
-    if (!fixed) {
-      return <Tag>{children}</Tag>;
-    }
-
-    if (type === 'header') {
-      return <div className="table-header-wrapper">{children}</div>;
-    }
-
-    if (type === 'body') {
-      return (
-        <div
-          ref={this.handleRef}
-          className="table-body-wrapper"
-          style={{
-            maxHeight: height,
-          }}
+  if (type === 'body') {
+    return (
+      <Flex
+        className="table-body-wrapper"
+        style={{
+          maxHeight: height || 'auto',
+        }}
+      >
+        <table
+          className={classNames(
+            'table table-condensed table--data table-body',
+            {
+              'table-hover': hover,
+              'table-striped': striped,
+              'table-bordered-our': bordered,
+              'fixed-table': true,
+            },
+            className
+          )}
         >
-          <table
-            className={classNames(
-              'table table-condensed table--data table-body',
-              {
-                'table-hover': hover,
-                'table-striped': striped,
-                'table-bordered-our': bordered,
-                'fixed-table': true,
-              },
-              className
-            )}
+          <tbody
+            style={{
+              maxHeight: height || 'auto',
+            }}
           >
-            <tbody
-              style={{
-                maxHeight: height,
-              }}
-            >
-              {children}
-            </tbody>
-          </table>
-        </div>
-      );
-    }
-
-    return <div className="table-footer-wrapper">{children}</div>;
+            {children}
+          </tbody>
+        </table>
+      </Flex>
+    );
   }
-}
 
-Section = updateOnlyForKeys(['children', 'height'])(Section);
+  return (
+    <Flex flexStyle="0 1 auto" className="table-footer-wrapper">
+      {children}
+    </Flex>
+  );
+};
+
+// Section = updateOnlyForKeys(['children', 'height'])(Section);
 
 const Thead = compose(
   setDisplayName('Thead'),
