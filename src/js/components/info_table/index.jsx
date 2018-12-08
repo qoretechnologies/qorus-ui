152644ce7ff70193da3e8c4b6dc00e52bf5bfb78
Filip Witosz <filip.witosz@gmail.com>
2018-12-08 03:26:43 +0100

    Replaced the info table with our Tree component.

--- src/js/components/info_table/index.jsx
+++ src/js/components/info_table/index.jsx
@@ -1,71 +1,20 @@
-/* @flow */
-import React, { Component } from 'react';
-
-import { Table, Tbody, Tr, Th, Td } from '../new_table';
-
-import AutoComponent from '../autocomponent';
-
-import _ from 'lodash';
+// @flow
+import React from 'react';
+import compose from 'recompose/compose';
 import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
+import Tree from '../tree';
+import lodashOmit from 'lodash/omit';
 
-@onlyUpdateForKeys(['object', 'omit', 'pick'])
-export default class InfoTable extends Component {
-  props: {
-    object: Object,
-    omit?: Array<string | number>,
-    pick?: Array<string | number>,
-  };
+type InfoTableProps = {
+  omit?: Array<string>,
+  object: Object,
+};
 
-  /**
-   * Returns object attribute filter based on `omit` or `pick` props.
-   *
-   * @return {function(attr: string): boolean}
-   */
-  getAttrFilter(): Function {
-    const { omit = [], pick = [] } = this.props;
-    if (omit.length > 0) return attr => omit.indexOf(attr) < 0;
-    if (pick.length > 0) return attr => pick.indexOf(attr) >= 0;
-    return () => true;
-  }
+const InfoTable: Function = ({
+  omit,
+  object,
+}: InfoTableProps): React.Element<any> => (
+  <Tree data={omit ? lodashOmit(object, omit) : object} />
+);
 
-  /**
-   * Returns attribute-value pairs from `object` props.
-   *
-   * It takes into account either omitted or picked attributes if set.
-   *
-   * @return {Array<AttrValuePair>}
-   * @see getAttrFilter
-   */
-  getData(): Array<Object> {
-    return Object.keys(this.props.object)
-      .filter(this.getAttrFilter())
-      .map(attr => ({
-        attr,
-        value: this.props.object[attr],
-      }));
-  }
-
-  /**
-   * Returns element for this component.
-   *
-   * @return {ReactElement}
-   */
-  render(): React.Element<Table> {
-    return (
-      <Table condensed striped info>
-        <Tbody>
-          {this.getData().map(
-            (datum: Object): React.Element<Tr> => (
-              <Tr>
-                <Th>{_.upperFirst(datum.attr.replace(/_/g, ' '))}</Th>
-                <Td>
-                  <AutoComponent>{datum.value}</AutoComponent>
-                </Td>
-              </Tr>
-            )
-          )}
-        </Tbody>
-      </Table>
-    );
-  }
-}
+export default compose(onlyUpdateForKeys(['object', 'omit']))(InfoTable);
