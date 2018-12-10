f3ee3a0e2d6f5d98fbcb4063004c4de45ea6f7fd
Filip Witosz <filip.witosz@gmail.com>
2018-12-10 02:22:29 +0100

    The performance tab now uses flex layout.

--- src/js/views/workflows/tabs/performance/index.jsx
+++ src/js/views/workflows/tabs/performance/index.jsx
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import ChartView from './chart';
-import Container from '../../../../components/container';
 import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
 import Box from '../../../../components/box';
 
@@ -9,13 +8,11 @@ const PerformanceTab: Function = ({
   workflow,
 }: {
   workflow: Object,
-}): React.Element<Container> => (
-  <Box top>
-    <Container>
-      <ChartView workflow={workflow} days={1} />
-      <ChartView workflow={workflow} days={7} />
-      <ChartView workflow={workflow} days={30} />
-    </Container>
+}): React.Element<Box> => (
+  <Box top fill scrollY>
+    <ChartView workflow={workflow} days={1} />
+    <ChartView workflow={workflow} days={7} />
+    <ChartView workflow={workflow} days={30} />
   </Box>
 );
 
