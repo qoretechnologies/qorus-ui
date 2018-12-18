5845ab982eb7a0533ed9fc6ec2fb8ef346d72147
Filip Witosz <filip.witosz@gmail.com>
2018-12-18 13:34:18 +0100

    Modified RBAC users table.

--- src/js/views/system/rbac/users/table.jsx
+++ src/js/views/system/rbac/users/table.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import compose from 'recompose/compose';
 import pure from 'recompose/onlyUpdateForKeys';
+import size from 'lodash/size';
 
 import UsersRow from './row';
 import {
@@ -11,14 +12,18 @@ import {
   FixedRow,
   Th,
 } from '../../../../components/new_table';
-import sort from '../../../../hocomponents/sort';
-import check from '../../../../hocomponents/check-no-data';
 import { sortDefaults } from '../../../../constants/sort';
 import Pull from '../../../../components/Pull';
 import { Control as Button } from '../../../../components/controls';
+import LoadMore from '../../../../components/LoadMore';
+import EnhancedTable from '../../../../components/EnhancedTable';
+import type { EnhancedTableProps } from '../../../../components/EnhancedTable';
+import { ActionColumnHeader } from '../../../../components/ActionColumn';
+import { NameColumnHeader } from '../../../../components/NameColumn';
+import DataOrEmptyTable from '../../../../components/DataOrEmptyTable';
 
 type Props = {
-  collection: Array<Object>,
+  users: Array<Object>,
   onEditClick: Function,
   canEdit: boolean,
   onDeleteClick: Function,
@@ -30,62 +35,84 @@ type Props = {
 };
 
 const UsersTable: Function = ({
-  collection,
+  users,
   onEditClick,
   onDeleteClick,
   canEdit,
   canDelete,
-  onSortChange,
-  sortData,
   onAddUserClick,
   canAdd,
 }: Props): React.Element<Table> => (
-  <Table consensed striped fixed key={`users_table-${collection.length}`}>
-    <Thead>
-      <FixedRow className="toolbar-row">
-        <Th collspan="full">
-          <Pull>
-            <Button
-              disabled={!canAdd}
-              text="Add user"
-              iconName="plus"
-              onClick={onAddUserClick}
-              big
-            />
-          </Pull>
-        </Th>
-      </FixedRow>
-      <FixedRow {...{ onSortChange, sortData }}>
-        <Th className="name" name="name">
-          Name
-        </Th>
-        <Th className="text big" name="username">
-          Username
-        </Th>
-        <Th className="text">Roles</Th>
-        <Th className="text narrow">Actions</Th>
-      </FixedRow>
-    </Thead>
-    <Tbody>
-      {collection.map(
-        (user: Object, index: number): React.Element<any> => (
-          <UsersRow
-            first={index === 0}
-            key={index}
-            model={user}
-            canEdit={canEdit}
-            canDelete={canDelete}
-            onEditClick={onEditClick}
-            onDeleteClick={onDeleteClick}
-          />
-        )
-      )}
-    </Tbody>
-  </Table>
+  <EnhancedTable
+    collection={users}
+    sortDefault={sortDefaults.rbacUsers}
+    tableId="rbacUsers"
+  >
+    {({
+      canLoadMore,
+      handleLoadAll,
+      handleLoadMore,
+      limit,
+      sortData,
+      onSortChange,
+      collection,
+    }: EnhancedTableProps) => (
+      <Table consensed striped fixed>
+        <Thead>
+          <FixedRow className="toolbar-row">
+            <Th collspan="full">
+              <Pull>
+                <Button
+                  disabled={!canAdd}
+                  text="Add user"
+                  iconName="plus"
+                  onClick={onAddUserClick}
+                  big
+                />
+              </Pull>
+              <Pull right>
+                <LoadMore
+                  canLoadMore={canLoadMore}
+                  onLoadMore={handleLoadMore}
+                  onLoadAll={handleLoadAll}
+                  limit={limit}
+                />
+              </Pull>
+            </Th>
+          </FixedRow>
+          <FixedRow {...{ onSortChange, sortData }}>
+            <NameColumnHeader />
+            <ActionColumnHeader />
+            <Th className="text big" name="username" icon="user">
+              Username
+            </Th>
+            <Th className="text" icon="contrast">
+              Roles
+            </Th>
+          </FixedRow>
+        </Thead>
+        <DataOrEmptyTable condition={size(collection) === 0} cols={4}>
+          {props => (
+            <Tbody {...props}>
+              {collection.map(
+                (user: Object, index: number): React.Element<any> => (
+                  <UsersRow
+                    first={index === 0}
+                    key={index}
+                    model={user}
+                    canEdit={canEdit}
+                    canDelete={canDelete}
+                    onEditClick={onEditClick}
+                    onDeleteClick={onDeleteClick}
+                  />
+                )
+              )}
+            </Tbody>
+          )}
+        </DataOrEmptyTable>
+      </Table>
+    )}
+  </EnhancedTable>
 );
 
-export default compose(
-  check(({ collection }): boolean => collection && collection.length),
-  sort('rbacusers', 'collection', sortDefaults.rbacUsers),
-  pure(['sortData', 'collection'])
-)(UsersTable);
+export default compose(pure(['users']))(UsersTable);
