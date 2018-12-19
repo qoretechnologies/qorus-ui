07a383b523f04b10a1590e291834075213a9c5ac
Filip Witosz <filip.witosz@gmail.com>
2018-12-19 15:19:39 +0100

    Created new author column.

---
+++ src/js/components/AuthorColumn/index.jsx
@@ -0,0 +1,45 @@
+// @flow
+import React from 'react';
+import compose from 'recompose/compose';
+import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
+import { Td, Th } from '../new_table';
+import Text from '../text';
+
+type AuthorColumnProps = {
+  children: any,
+  className: string,
+};
+
+const AuthorColumn: Function = compose(onlyUpdateForKeys(['children']))(
+  ({
+    children,
+    className: className = 'text',
+  }: AuthorColumnProps): React.Element<any> => (
+    <Td className={className}>
+      <Text text={children || '-'} />
+    </Td>
+  )
+);
+
+type AuthorColumnHeaderProps = {
+  children: any,
+  name: string,
+  icon: string,
+};
+
+const AuthorColumnHeader: Function = compose(
+  onlyUpdateForKeys(['children', 'sortData'])
+)(
+  ({
+    children: children = 'Author',
+    name: name = 'author',
+    icon: icon = 'person',
+    ...rest
+  }: AuthorColumnHeaderProps): React.Element<any> => (
+    <Th icon={icon} name={name} className="text" {...rest}>
+      {children}
+    </Th>
+  )
+);
+
+export { AuthorColumn, AuthorColumnHeader };
