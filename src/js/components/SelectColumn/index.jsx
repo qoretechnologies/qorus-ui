bf678b5e8f6a6cdd8357891bb5f73b1827faa5da
Filip Witosz <filip.witosz@gmail.com>
2018-12-20 08:42:37 +0100

    Created new column with checkboxes.

---
+++ src/js/components/SelectColumn/index.jsx
@@ -0,0 +1,44 @@
+// @flow
+import React from 'react';
+import compose from 'recompose/compose';
+import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
+import { Td, Th } from '../new_table';
+import Checkbox from '../checkbox';
+
+type SelectColumnProps = {
+  onClick: Function,
+  className: string,
+  checked: boolean,
+};
+
+const SelectColumn: Function = compose(onlyUpdateForKeys(['children']))(
+  ({
+    onClick,
+    checked,
+    className: className = 'tiny',
+  }: SelectColumnProps): React.Element<any> => (
+    <Td className={className}>
+      <Checkbox action={onClick} checked={checked ? 'CHECKED' : 'UNCHECKED'} />
+    </Td>
+  )
+);
+
+type SelectColumnHeaderProps = {
+  children: any,
+  name: string,
+  icon: string,
+};
+
+const SelectColumnHeader: Function = compose(
+  onlyUpdateForKeys(['children', 'sortData'])
+)(
+  ({
+    name: name = '_selected',
+    icon: icon = 'small-tick',
+    ...rest
+  }: SelectColumnHeaderProps): React.Element<any> => (
+    <Th name={name} icon={icon} {...rest} />
+  )
+);
+
+export { SelectColumn, SelectColumnHeader };
