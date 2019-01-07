15f19190db390f5f5c4cc5f81b2fb465a7ec740d
Filip Witosz <filip.witosz@gmail.com>
2019-01-07 19:54:43 +0100

    Added flex wrapper to the process table, scrolling was not working.

--- src/js/components/ProcessTable/index.jsx
+++ src/js/components/ProcessTable/index.jsx
@@ -7,6 +7,7 @@ import Tree from '../tree';
 import PaneItem from '../pane_item';
 import { Controls as ButtonGroup, Control as Button } from '../controls';
 import withProcessKill from '../../hocomponents/withProcessKill';
+import Flex from '../Flex';
 
 type ProcessTableProps = {
   model: Object,
@@ -23,29 +24,30 @@ const ProcessTable: Function = ({
     content="This interface is not running under a process"
   >
     {() => (
-      <PaneItem
-        title="Process Info"
-        label={
-          <ButtonGroup>
-            <Button
-              btnStyle="danger"
-              icon="cross"
-              onClick={() => {
-                handleKillClick(model.process.id);
-              }}
-            >
-              Kill
-            </Button>
-          </ButtonGroup>
-        }
-      >
+      <Flex>
+        <PaneItem
+          title="Process Info"
+          label={
+            <ButtonGroup>
+              <Button
+                btnStyle="danger"
+                icon="cross"
+                onClick={() => {
+                  handleKillClick(model.process.id);
+                }}
+              >
+                Kill
+              </Button>
+            </ButtonGroup>
+          }
+        />
         <Tree
           data={{
             ...model.process,
             ...{ memory: model.process.priv_str },
           }}
         />
-      </PaneItem>
+      </Flex>
     )}
   </NoDataIf>
 );
