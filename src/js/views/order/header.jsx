c0a005326615dce134aabdecdfaf9d0c3069e35c
Filip Witosz <filip.witosz@gmail.com>
2019-01-26 03:18:55 +0100

    Moved the order actions from dropdown to buttons, Block / Unblock & Cancel / Uncancel actions are now using the same icon.

--- src/js/views/order/header.jsx
+++ src/js/views/order/header.jsx
@@ -33,68 +33,6 @@ export default class OrderHeader extends Component {
     prevQueryQuery: string,
   } = this.props;
 
-  static contextTypes = {
-    openModal: PropTypes.func,
-    closeModal: PropTypes.func,
-  };
-
-  handleLockClick = model => () => {
-    const label = model.operator_lock ? 'Unlock' : 'Lock';
-
-    this._modal = (
-      <Lock
-        onClose={this.handleModalCloseClick}
-        data={model}
-        label={label}
-        username={this.props.username}
-      />
-    );
-
-    this.context.openModal(this._modal);
-  };
-
-  handleScheduleClick = order => {
-    this._modal = (
-      <Reschedule data={order} onClose={this.handleModalCloseClick} />
-    );
-
-    this.context.openModal(this._modal);
-  };
-
-  handleModalCloseClick = () => {
-    this.context.closeModal(this._modal);
-  };
-
-  renderLock() {
-    const { data } = this.props;
-    const icon = data.operator_lock ? 'lock' : 'unlock';
-    const itemIcon = data.operator_lock ? 'unlock' : 'lock';
-    const locked = data.operator_lock || '';
-    const title = data.operator_lock ? 'Unlock' : 'Lock';
-    const style = data.operator_lock ? 'danger' : 'success';
-    const disabled =
-      data.operator_lock && data.operator_lock !== this.props.username;
-
-    return (
-      <Dropdown>
-        <Control disabled={disabled} small btnStyle={style}>
-          <i className={`fa fa-${icon}`} /> {locked}
-        </Control>
-        <Item
-          iconName={itemIcon}
-          title={title}
-          action={this.handleLockClick(data)}
-        />
-      </Dropdown>
-    );
-  }
-
-  handleBackClick = ev => {
-    ev.preventDefault();
-
-    history.go(-1);
-  };
-
   render() {
     const { prevQueryQuery, targetQuery, workflow, data } = this.props;
 
@@ -150,6 +88,15 @@ export default class OrderHeader extends Component {
           />
         </Breadcrumbs>
         <Pull right>
+          <OrderControls
+            id={this.props.data.id}
+            workflowstatus={this.props.data.workflowstatus}
+          />
+          <Lock
+            id={this.props.data.id}
+            lock={this.props.data.operator_lock}
+            big
+          />
           <WorkflowControls
             id={this.props.workflow.id}
             enabled={this.props.workflow.enabled}
@@ -161,15 +108,6 @@ export default class OrderHeader extends Component {
             autostart={this.props.workflow.autostart}
             execCount={this.props.workflow.exec_count}
             withExec
-            big
-          />
-          <OrderControls
-            id={this.props.data.id}
-            workflowstatus={this.props.data.workflowstatus}
-          />
-          <Lock
-            id={this.props.data.id}
-            lock={this.props.data.operator_lock}
             big
           />
         </Pull>
