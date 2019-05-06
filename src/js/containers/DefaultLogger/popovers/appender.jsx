f70172776cc0ed7fbd11751c1811e5fd5ab4e050
Filip Witosz <filip.witosz@gmail.com>
2019-05-06 12:05:10 +0200

    Split the Logger containers to concrete and default logger containers.

---
+++ src/js/containers/DefaultLogger/popovers/appender.jsx
@@ -0,0 +1,293 @@
+// @flow
+import React from 'react';
+import compose from 'recompose/compose';
+import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
+import Box from '../../../components/box';
+import { FormGroup, InputGroup } from '@blueprintjs/core';
+import withState from 'recompose/withState';
+import Alert from '../../../components/alert';
+import withHandlers from 'recompose/withHandlers';
+import includes from 'lodash/includes';
+import {
+  Controls as ButtonGroup,
+  Control as Button,
+} from '../../../components/controls';
+import { connect } from 'react-redux';
+import { fetchWithNotifications, post } from '../../../store/api/utils';
+import settings from '../../../settings';
+import Dropdown, { Item, Control } from '../../../components/dropdown';
+
+type NewAppenderPopoverProps = {
+  error?: string,
+  name: string,
+  level?: string,
+  additivity: boolean,
+  handleNameChange: Function,
+  handleLevelChange: Function,
+  handleAdditivityChange: Function,
+  onCancel: Function,
+  loggerLevels: Object,
+  resource: string,
+  id: number | string,
+  changeError: Function,
+  handleSubmit: Function,
+  dispatch: Function,
+  changeAdding: Function,
+  isAdding: boolean,
+  data?: Object,
+};
+
+const NewAppenderPopover: Function = ({
+  error,
+  name,
+  handleNameChange,
+  layoutPattern,
+  handleLayoutPatternChange,
+  filename,
+  handleFilenameChange,
+  encoding,
+  handleEncodingChange,
+  appenderType,
+  handleAppenderTypeChange,
+  rotationCount,
+  handleRotationCountChange,
+  archivePattern,
+  handleArchivePatternChange,
+  onCancel,
+  handleSubmit,
+  changeError,
+  dispatch,
+  isAdding,
+  appendersTypes,
+  appendersFields,
+}: NewAppenderPopoverProps): React.Element<any> => (
+  <Box fill top style={{ minWidth: '350px' }}>
+    {error && <Alert bsStyle="danger">{error}</Alert>}
+    <FormGroup label="Name " labelFor="appender-name">
+      <InputGroup
+        name="appender-name"
+        id="appender-name"
+        value={name}
+        onChange={handleNameChange}
+      />
+    </FormGroup>
+
+    <FormGroup label="Appender type " requiredLabel>
+      <Dropdown>
+        <Control>{appenderType || 'Please select'}</Control>
+        {appendersTypes.map((appType: string) => (
+          <Item
+            key={appType}
+            title={appType}
+            onClick={handleAppenderTypeChange}
+          />
+        ))}
+      </Dropdown>
+    </FormGroup>
+
+    {includes(appendersFields['layoutPattern'], appenderType) && (
+      <FormGroup label="Layout pattern" labelFor="appender-layout">
+        <InputGroup
+          name="appender-layout"
+          id="appender-layout"
+          value={layoutPattern}
+          onChange={handleLayoutPatternChange}
+        />
+      </FormGroup>
+    )}
+
+    {includes(appendersFields['filename'], appenderType) && (
+      <FormGroup label="Filename " labelFor="appender-filename" requiredLabel>
+        <InputGroup
+          name="appender-filename"
+          id="appender-filename"
+          value={filename}
+          onChange={handleFilenameChange}
+        />
+      </FormGroup>
+    )}
+
+    {includes(appendersFields['encoding'], appenderType) && (
+      <FormGroup label="Encoding" labelFor="appender-encoding">
+        <InputGroup
+          name="appender-encoding"
+          id="appender-encoding"
+          value={encoding}
+          onChange={handleEncodingChange}
+        />
+      </FormGroup>
+    )}
+
+    {includes(appendersFields['rotationCount'], appenderType) && (
+      <FormGroup label="Rotation count" labelFor="appender-rotation">
+        <InputGroup
+          name="appender-rotation"
+          id="appender-rotation"
+          value={rotationCount}
+          onChange={handleRotationCountChange}
+        />
+      </FormGroup>
+    )}
+
+    {includes(appendersFields['archivePattern'], appenderType) && (
+      <FormGroup label="Archive pattern" labelFor="appender-archive">
+        <InputGroup
+          name="appender-archive"
+          id="appender-archive"
+          value={archivePattern}
+          onChange={handleArchivePatternChange}
+        />
+      </FormGroup>
+    )}
+
+    <ButtonGroup className="pt-fill">
+      <Button
+        text="Cancel"
+        icon="cross"
+        onClick={onCancel}
+        disabled={isAdding}
+      />
+      <Button
+        disabled={isAdding}
+        btnStyle="success"
+        text="Submit"
+        icon="small-tick"
+        onClick={handleSubmit}
+      />
+    </ButtonGroup>
+  </Box>
+);
+
+export default compose(
+  connect(
+    (state: Object): Object => ({
+      appendersTypes: state.api.system.data.loggerParams.appenders_types,
+      appendersFields: state.api.system.data.loggerParams.appenders_fields,
+    })
+  ),
+  withState('name', 'changeName', ''),
+  withState('layoutPattern', 'changeLayoutPattern', null),
+  withState('filename', 'changeFilename', null),
+  withState('encoding', 'changeEncoding', null),
+  withState('appenderType', 'changeAppenderType', null),
+  withState('rotationCount', 'changeRotationCount', 0),
+  withState('archivePattern', 'changeArchivePattern', null),
+  withState('error', 'changeError', null),
+  withState('isAdding', 'changeAdding', false),
+  withHandlers({
+    handleNameChange: ({ changeName }): Function => (event: Object): void => {
+      event.persist();
+
+      changeName(() => event.target.value);
+    },
+    handleLayoutPatternChange: ({ changeLayoutPattern }): Function => (
+      event: Object
+    ): void => {
+      event.persist();
+
+      changeLayoutPattern(() => event.target.value);
+    },
+    handleFilenameChange: ({ changeFilename }): Function => (
+      event: Object
+    ): void => {
+      event.persist();
+
+      changeFilename(() => event.target.value);
+    },
+    handleEncodingChange: ({ changeEncoding }): Function => (
+      event: Object
+    ): void => {
+      event.persist();
+
+      changeEncoding(() => event.target.value);
+    },
+    handleAppenderTypeChange: ({ changeAppenderType }): Function => (
+      event: Object,
+      appenderType: string
+    ): void => {
+      event.persist();
+
+      changeAppenderType(() => appenderType);
+    },
+    handleRotationCountChange: ({ changeRotationCount }): Function => (
+      event: Object
+    ): void => {
+      event.persist();
+
+      changeRotationCount(() => event.target.value);
+    },
+    handleArchivePatternChange: ({ changeArchivePattern }): Function => (
+      event: Object
+    ): void => {
+      event.persist();
+
+      changeArchivePattern(() => event.target.value);
+    },
+    handleSubmit: ({
+      name,
+      layoutPattern,
+      filename,
+      encoding,
+      appenderType,
+      rotationCount,
+      archivePattern,
+      changeError,
+      id,
+      resource,
+      dispatch,
+      onCancel,
+      changeAdding,
+    }: NewAppenderPopoverProps): Function => async (): any => {
+      if (!appenderType) {
+        changeError(() => 'Appender Type field is required.');
+      } else if (
+        appenderType !== 'LoggerAppenderStdOut' &&
+        (!filename || filename === '')
+      ) {
+        changeError(() => 'Filename is required for this type of appender');
+      } else {
+        changeAdding(() => true);
+
+        const appendersPath: string = id
+          ? `${id}/logger/appenders`
+          : 'logger/appenders';
+        const fetchRes: Object = await fetchWithNotifications(
+          async () =>
+            post(`${settings.REST_BASE_URL}/${resource}/${appendersPath}`, {
+              body: JSON.stringify({
+                name,
+                layoutPattern,
+                filename,
+                encoding,
+                appenderType,
+                rotationCount,
+                archivePattern,
+              }),
+            }),
+          `Adding new appender...`,
+          `New appender successfuly added`,
+          dispatch
+        );
+
+        if (fetchRes.err) {
+          changeAdding(() => false);
+          changeError(() => fetchRes.desc);
+        } else {
+          onCancel();
+        }
+      }
+    },
+  }),
+  onlyUpdateForKeys([
+    'appendersTypes',
+    'appendersFields',
+    'name',
+    'layoutPattern',
+    'filename',
+    'encoding',
+    'appenderType',
+    'error',
+    'rotationCount',
+    'archivePattern',
+  ])
+)(NewAppenderPopover);
--- src/js/containers/Logger/index.jsx
+++ src/js/containers/Logger/index.jsx
@@ -1,286 +1,50 @@
 // @flow
 import React from 'react';
 import compose from 'recompose/compose';
-import { connect } from 'react-redux';
 import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
-import size from 'lodash/size';
-
-import Flex from '../../components/Flex';
-import actions from '../../store/api/actions';
-import withDispatch from '../../hocomponents/withDispatch';
-import showIfPassed from '../../hocomponents/show-if-passed';
 import lifecycle from 'recompose/lifecycle';
+import actions from '../../store/api/actions';
+import showIfPassed from '../../hocomponents/show-if-passed';
 import Loader from '../../components/loader';
-import PaneItem from '../../components/pane_item';
-import DataOrEmptyTable from '../../components/DataOrEmptyTable';
-import {
-  Table,
-  Thead,
-  FixedRow,
-  Th,
-  Tbody,
-  Tr,
-  Td,
-} from '../../components/new_table';
-import NameColumn, { NameColumnHeader } from '../../components/NameColumn';
-import ContentByType from '../../components/ContentByType';
-import NewLoggerPopover from './popovers/logger';
-import NewAppenderPopover from './popovers/appender';
-import {
-  Controls as ButtonGroup,
-  Control as Button,
-} from '../../components/controls';
-import Alert from '../../components/alert';
-import { Popover, Position } from '@blueprintjs/core';
+import withDispatch from '../../hocomponents/withDispatch';
 import withState from 'recompose/withState';
-import withHandlers from 'recompose/withHandlers';
-import { fetchWithNotifications, del, post } from '../../store/api/utils';
-import settings from '../../settings';
+import DefaultLogger from '../DefaultLogger';
+import ConcreteLogger from '../ConcreteLogger';
+import { connect } from 'react-redux';
 
-type LoggerContainerProps = {
-  logger: Object,
-  appenders: Array<Object>,
-  id: number,
-  resource: string,
-  handleLoggerDeleteClick: Function,
-  handleDeleteAppenderClick: Function,
-  handleLoggerDuplicateClick: Function,
-};
+type LoggerContainerProps = {};
 
 const LoggerContainer: Function = ({
-  logger,
-  appenders,
-  isLoggerPopoverOpen,
-  isAppenderPopoverOpen,
-  toggleLoggerPopover,
-  toggleAppenderPopover,
-  handleDeleteAppenderClick,
-  resource,
-  id,
-  handleLoggerDeleteClick,
-  handleLoggerDuplicateClick,
-}: LoggerContainerProps): React.Element<any> => (
-  <Flex>
-    {logger.isDefault && (
-      <React.Fragment>
-        <Alert bsStyle="warning">This interface is using default logger</Alert>
-      </React.Fragment>
-    )}
-    <PaneItem
-      title="Logger"
-      label={
-        logger.isDefault || logger === 'empty' ? (
-          <React.Fragment>
-            <Popover
-              content={
-                <NewLoggerPopover
-                  resource={resource}
-                  id={id}
-                  onCancel={() => toggleLoggerPopover(() => false)}
-                />
-              }
-              position={Position.LEFT_TOP}
-              isOpen={isLoggerPopoverOpen}
-            >
-              <ButtonGroup>
-                <Button
-                  text="Add new logger"
-                  icon="add"
-                  stopPropagation
-                  onClick={() => toggleLoggerPopover(() => true)}
-                />
-              </ButtonGroup>
-            </Popover>
-            {logger !== 'empty' && (
-              <ButtonGroup>
-                <Button
-                  text="Clone logger"
-                  icon="duplicate"
-                  stopPropagation
-                  onClick={handleLoggerDuplicateClick}
-                />
-              </ButtonGroup>
-            )}
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <Popover
-              content={
-                <NewLoggerPopover
-                  resource={resource}
-                  id={id}
-                  data={logger}
-                  onCancel={() => toggleLoggerPopover(() => false)}
-                />
-              }
-              position={Position.LEFT_TOP}
-              isOpen={isLoggerPopoverOpen}
-            >
-              <ButtonGroup>
-                <Button
-                  text="Edit logger"
-                  icon="edit"
-                  stopPropagation
-                  onClick={() => toggleLoggerPopover(() => true)}
-                />
-              </ButtonGroup>
-            </Popover>
-            <ButtonGroup>
-              <Button
-                text="Delete logger"
-                btnStyle="danger"
-                icon="remove"
-                onClick={handleLoggerDeleteClick}
-              />
-            </ButtonGroup>
-          </React.Fragment>
-        )
-      }
-    >
-      {logger !== 'empty' ? (
-        <Table fixed striped>
-          <Thead>
-            <FixedRow>
-              <NameColumnHeader />
-              <Th icon="info-sign">Level</Th>
-              <Th icon="info-sign">Additivity</Th>
-            </FixedRow>
-          </Thead>
-          <Tbody>
-            <Tr first>
-              <NameColumn name={logger.name} />
-              <Td>{Object.keys(logger.level)[0]}</Td>
-              <Td>
-                <ContentByType content={logger.additivity} />
-              </Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      ) : (
-        <Alert bsStyle="info">No logger found for system</Alert>
-      )}
-    </PaneItem>
-    {logger !== 'empty' && (
-      <PaneItem
-        title="Appenders"
-        label={
-          !logger.isDefault && (
-            <Popover
-              content={
-                <NewAppenderPopover
-                  resource={resource}
-                  id={id}
-                  onCancel={() => toggleAppenderPopover(() => false)}
-                />
-              }
-              position={Position.LEFT_TOP}
-              isOpen={isAppenderPopoverOpen}
-            >
-              <Button
-                text="Add appender"
-                icon="add"
-                stopPropagation
-                onClick={() => toggleAppenderPopover(() => true)}
-              />
-            </Popover>
-          )
-        }
-      >
-        <Table fixed condensed striped>
-          <Thead>
-            <FixedRow>
-              <NameColumnHeader />
-              <Th className="text" icon="info-sign">
-                Type
-              </Th>
-              <Th className="text" icon="info-sign">
-                Filename
-              </Th>
-              <Th className="text" icon="info-sign">
-                Encoding
-              </Th>
-              <Th className="text" icon="info-sign">
-                Layout Pattern
-              </Th>
-              <Th icon="refresh" title="Rotation count" />
-              {!logger.isDefault && (
-                <Th icon="remove" title="Remove appender" />
-              )}
-            </FixedRow>
-          </Thead>
-          <DataOrEmptyTable condition={size(appenders) === 0} cols={7}>
-            {(props: Object) => (
-              <Tbody {...props}>
-                {appenders.map(
-                  (appender: Object, index: number): any => (
-                    <Tr first={index === 0} key={appender.id}>
-                      <NameColumn name={appender.name} />
-                      <Td className="text">
-                        <ContentByType content={appender.type} />
-                      </Td>
-                      <Td className="text">
-                        <ContentByType content={appender.filename} />
-                      </Td>
-                      <Td className="text">
-                        <ContentByType content={appender.encoding} />
-                      </Td>
-                      <Td className="text">
-                        <ContentByType content={appender.layoutPattern} />
-                      </Td>
-                      <Td className="narrow">
-                        <ContentByType content={appender.rotationCount} />
-                      </Td>
-                      {!logger.isDefault && (
-                        <Td className="tiny">
-                          <ButtonGroup>
-                            <Button
-                              title="Remove appender"
-                              btnStyle="danger"
-                              icon="remove"
-                              onClick={() =>
-                                handleDeleteAppenderClick(appender.id)
-                              }
-                            />
-                          </ButtonGroup>
-                        </Td>
-                      )}
-                    </Tr>
-                  )
-                )}
-              </Tbody>
-            )}
-          </DataOrEmptyTable>
-        </Table>
-      </PaneItem>
-    )}
-  </Flex>
-);
+  isUsingDefaultLogger,
+  ...rest
+}: LoggerContainerProps): React.Element<any> =>
+  isUsingDefaultLogger ? (
+    <DefaultLogger {...rest} />
+  ) : (
+    <ConcreteLogger {...rest} />
+  );
 
 export default compose(
   connect((state, ownProps) => {
-    let { loggerData: logger, appenders }: Object =
-      ownProps.resource === 'system'
-        ? state.api.system.data
-        : state.api[ownProps.resource].data.find(
-          (res: Object): boolean => res.id === ownProps.id
-        );
+    let { isUsingDefaultLogger, loggerData }: Object = state.api[
+      ownProps.resource
+    ][ownProps.resource === 'system' ? 'logs' : 'data'].find(
+      (res: Object): boolean => res.id === ownProps.id
+    );
 
-    if (ownProps.resource === 'system') {
-      logger = logger?.[ownProps.id];
-      appenders = logger?.appenders;
-    }
+    console.log(isUsingDefaultLogger);
 
     return {
-      logger,
-      appenders,
+      isUsingDefaultLogger,
+      loggerData,
     };
   }),
   withDispatch(),
   lifecycle({
-    componentDidMount () {
-      const { id, resource, dispatchAction, logger } = this.props;
+    async componentDidMount () {
+      const { id, resource, dispatchAction, loggerData } = this.props;
 
-      if (!logger) {
+      if (!loggerData) {
         dispatchAction(actions[resource].fetchLogger, id);
       }
     },
@@ -293,70 +57,6 @@ export default compose(
       }
     },
   }),
-  showIfPassed(({ logger }) => logger, <Loader />),
-  withState('isLoggerPopoverOpen', 'toggleLoggerPopover', false),
-  withState('isAppenderPopoverOpen', 'toggleAppenderPopover', false),
-  withHandlers({
-    handleLoggerDeleteClick: ({
-      dispatch,
-      resource,
-      id,
-    }: LoggerContainerProps): Function => (): void => {
-      fetchWithNotifications(
-        async () => {
-          const loggerPath = `${id}/logger`;
-
-          return del(`${settings.REST_BASE_URL}/${resource}/${loggerPath}`);
-        },
-        `Removing logger...`,
-        `Logger successfuly removed`,
-        dispatch
-      );
-    },
-    handleLoggerDuplicateClick: ({
-      dispatch,
-      resource,
-      id,
-    }: LoggerContainerProps): Function => (): void => {
-      fetchWithNotifications(
-        async () =>
-          post(
-            `${
-              settings.REST_BASE_URL
-            }/${resource}/${id}/logger?cloneDefault=true`
-          ),
-        `Duplicating logger...`,
-        `Logger successfuly duplicated`,
-        dispatch
-      );
-    },
-    handleDeleteAppenderClick: ({
-      dispatch,
-      resource,
-      id,
-    }: LoggerContainerProps): Function => (appenderId): void => {
-      fetchWithNotifications(
-        async () => {
-          const appendersPath = id
-            ? `${id}/logger/appenders`
-            : 'logger/appenders';
-
-          return del(`${settings.REST_BASE_URL}/${resource}/${appendersPath}`, {
-            body: JSON.stringify({
-              id: appenderId,
-            }),
-          });
-        },
-        `Removing appender...`,
-        `Appender successfuly removed`,
-        dispatch
-      );
-    },
-  }),
-  onlyUpdateForKeys([
-    'logger',
-    'appenders',
-    'isLoggerPopoverOpen',
-    'isAppenderPopoverOpen',
-  ])
+  showIfPassed(({ loggerData }) => loggerData, <Loader />),
+  onlyUpdateForKeys(['id', 'loggerData', 'isUsingDefaultLogger'])
 )(LoggerContainer);
