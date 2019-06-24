d148f5047f4fc992672b9c311c09c3bf81bd91f1
Filip Witosz <filip.witosz@gmail.com>
2019-06-24 11:02:00 +0200

    Removed synthetic groups from role modal.

--- src/js/hocomponents/patchFuncArgs.jsx
+++ src/js/hocomponents/patchFuncArgs.jsx
@@ -1,15 +1,14 @@
 /* @flow */
 import React from 'react';
 
-export default (
-  funcName: string,
-  funcArgsList: Array<any>
-) => (Component: any) => {
+export default (funcName: string, funcArgsList: Array<any>) => (
+  Component: any
+) => {
   const WrappedComponent: Function = (props): React.Element<any> => {
     const func = props[funcName];
-    const funcArgs = funcArgsList.map(arg => (
+    const funcArgs = funcArgsList.map(arg =>
       props[arg] === 0 ? 0 : props[arg] || arg
-    ));
+    );
     const updatedFunc = (...args) => func(...funcArgs, ...args);
     const newProps = Object.assign({}, props, {
       [funcName]: updatedFunc,
