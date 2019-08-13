722728490c8c085db2a2c994d49cf9a138f450e0
Filip Witosz <filip.witosz@gmail.com>
2019-08-13 09:59:47 +0200

    Mapper fixes.

--- src/js/helpers/mapper.js
+++ src/js/helpers/mapper.js
@@ -4,10 +4,16 @@ import trimEnd from 'lodash/trimEnd';
 
 const formatFieldSource: Function = (fieldSource: string): Object => {
   let newSource = fieldSource;
+  newSource = trimStart(newSource, '{');
+  newSource = trimEnd(newSource, '}');
   newSource = trimStart(newSource, '(');
   newSource = trimEnd(newSource, ') ');
 
-  if (newSource.indexOf('runtime') !== -1 && newSource !== '{}' && newSource !== '{},') {
+  if (
+    newSource.indexOf('runtime') !== -1 &&
+    newSource !== '{}' &&
+    newSource !== '{},'
+  ) {
     newSource = trimStart(newSource, '{');
     newSource = trimEnd(newSource, '}');
   }
@@ -15,7 +21,7 @@ const formatFieldSource: Function = (fieldSource: string): Object => {
   // Check if there is any code in the fieldsource
   const code = newSource.substring(
     newSource.lastIndexOf('"code"'),
-    newSource.lastIndexOf('}') + 1,
+    newSource.lastIndexOf('}') + 1
   );
 
   // Pull it out if there is
@@ -34,38 +40,40 @@ const formatFieldSource: Function = (fieldSource: string): Object => {
   // Split the fieldsource by commas
   const sourceArray = newSource.split(',');
 
-  const data = sourceArray.map((attr) => {
-    if (attr === '' || attr === ' ' || attr === '{}') {
-      if (code !== '' && code !== '{}') {
-        return {
-          key: 'code',
-          value: 'Click to view code',
-        };
+  const data = sourceArray
+    .map(attr => {
+      if (attr === '' || attr === ' ' || attr === '{}') {
+        if (code !== '' && code !== '{}') {
+          return {
+            key: 'code',
+            value: 'Click to view code',
+          };
+        }
+
+        return null;
       }
 
-      return null;
-    }
+      // Split the string by key and value
+      const newAttr = attr.split(':');
 
-    // Split the string by key and value
-    const newAttr = attr.split(':');
+      // If there is no key, the key is value and key is Unknown
+      if (!newAttr[1]) {
+        newAttr[1] = newAttr[0];
+        newAttr[0] = 'Unknown';
+      }
 
-    // If there is no key, the key is value and key is Unknown
-    if (!newAttr[1]) {
-      newAttr[1] = newAttr[0];
-      newAttr[0] = 'Unknown';
-    }
+      // Remove all quotes
+      const key = newAttr[0];
+      const value = newAttr[1];
 
-    // Remove all quotes
-    const key = newAttr[0];
-    const value = newAttr[1];
+      // Do not display name
+      if (key === 'name') {
+        return null;
+      }
 
-    // Do not display name
-    if (key === 'name') {
-      return null;
-    }
-
-    return { key, value };
-  }).filter(item => item);
+      return { key, value };
+    })
+    .filter(item => item);
 
   return {
     data,
@@ -73,6 +81,4 @@ const formatFieldSource: Function = (fieldSource: string): Object => {
   };
 };
 
-export {
-  formatFieldSource,
-};
+export { formatFieldSource };
