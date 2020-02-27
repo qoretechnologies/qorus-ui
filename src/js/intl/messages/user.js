21dc21c380f488a8cce909d629fe5974a05a8168
David Nichols <david@qore.org>
2020-02-27 16:55:07 +0100

    added new strings for reset password

--- src/js/intl/messages/user.js
+++ src/js/intl/messages/user.js
@@ -3,20 +3,24 @@ export default {
     'user.clear-storage': 'Clear storage',
     'user.member-of-def-grp': 'Member of DEFAULT group with no restrictions; all interfaces are accessible',
     'user.storage-data': 'Storage data',
+    'user.reset-password': 'Reset password',
   },
   'cs-CZ': {
     'user.clear-storage': 'Vyčistit úložiště',
     'user.member-of-def-grp': 'Člen skupiny DEFAULT bez omezení; všechny jednotky jsou přístupné',
     'user.storage-data': 'Data v úložišti',
+    'user.reset-password': 'Resetovat heslo',
   },
   'ja-JP': {
     'user.clear-storage': 'ストレージをクリア',
     'user.member-of-def-grp': '制限のないDEFAULTグループのメンバー。すべてのインターフェースにアクセス可能',
     'user.storage-data': 'ストレージデータ',
+    'user.reset-password': 'パスワードを再設定する',
   },
   'ko-KR': {
     'user.clear-storage': '저장 공간을 비우기',
     'user.member-of-def-grp': '제한이없는 DEFAULT 그룹의 구성원 모든 인터페이스에 접근 가능',
     'user.storage-data': '저장 데이터',
+    'user.reset-password': '암호를 재설정',
   },
 };
