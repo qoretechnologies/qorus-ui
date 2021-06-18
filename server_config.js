c1f16d5a4e3016a22ba7cf4a89666988c620135f
Filip Witosz <filip.witosz@gmail.com>
2021-06-18 08:25:40 +0000

    Update server_config.js

--- server_config.js
+++ server_config.js
@@ -4,7 +4,9 @@ const LOCAL_PORT: number = process.env.LOCAL_PORT || 3004;
 const LOCAL_HOST: string = process.env.LOCAL_HOST || 'localhost';
 const PROTO: string = process.env.PROTO || 'https';
 const WS_PROTO: string = PROTO === 'https' ? 'wss' : 'ws';
-const API_HOST: string = process.env.API_HOST || 'rippy';
+const API_HOST: string = process.env.API_HOST ||
+  window?.location?.hostname ||
+  'hq.qoretechnologies.com';
 
 module.exports = {
   SERVER_URL: `${PROTO}://${LOCAL_HOST}:${LOCAL_PORT}`,
