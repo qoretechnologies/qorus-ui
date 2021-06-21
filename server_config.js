c16f5129eeb65f46783fcc82b1ff3e4c09964380
Filip Witosz <filip.witosz@gmail.com>
2021-06-21 08:15:02 +0000

    Update server_config.js

--- server_config.js
+++ server_config.js
@@ -1,19 +1,21 @@
 // @ flow
-const API_PORT: number = process.env.API_PORT || window?.location?.port || 8092;
+const API_PORT: number = process.env.API_PORT || window?.location?.port;
 const LOCAL_PORT: number = process.env.LOCAL_PORT || 3004;
 const LOCAL_HOST: string = process.env.LOCAL_HOST || 'localhost';
 const PROTO: string = process.env.PROTO || 'https';
 const WS_PROTO: string = PROTO === 'https' ? 'wss' : 'ws';
-const API_HOST: string = process.env.API_HOST ||
+const API_HOST: string =
+  process.env.API_HOST ||
   window?.location?.hostname ||
   'hq.qoretechnologies.com';
 
 module.exports = {
-  SERVER_URL: `${PROTO}://${LOCAL_HOST}:${LOCAL_PORT}`,
-  API_URL: `${PROTO}://${API_HOST}:${API_PORT}`,
+  SERVER_URL: `${PROTO}://${LOCAL_HOST}${LOCAL_PORT}`,
+  API_URL: `${PROTO}://${API_HOST}${API_PORT ? `:${API_PORT}` : ''}`,
   SERVER_WS_URL: `${WS_PROTO}://${LOCAL_HOST}:${LOCAL_PORT}`,
-  API_WS_URL: `${WS_PROTO}://${API_HOST}:${API_PORT}`,
+  API_WS_URL: `${WS_PROTO}://${API_HOST}${API_PORT ? `:${API_PORT}` : ''}`,
   LOCAL_HOST,
   LOCAL_PORT,
+  WEB_IDE_URL: '/ide/',
   IS_SECURE: PROTO === 'https',
 };
