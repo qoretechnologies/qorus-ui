9fe2d56ea83a592a57fb888e659fc076188e6e5a
Foxhoundn <filip.witosz@gmail.com>
2023-08-11 07:30:29 +0000

    HTTP uploading and CI/CD fixes

---
+++ playwright-http.config.ts
@@ -0,0 +1,21 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import config from './playwright.config';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const httpConfig: PlaywrightTestConfig = {
+  ...config,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI
+    ? [['html', { open: 'never', outputFolder: 'test-results-http/report' }]]
+    : 'line',
+};
+
+export default httpConfig;
