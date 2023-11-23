c1bd7eb8964cea867c3c830741aada5ff7f4109f
Foxhoundn <filip.witosz@gmail.com>
2023-11-23 12:56:03 +0000

    Refactor code to use Reqore UI components

--- src/js/views/code/register.tsx
+++ src/js/views/code/register.tsx
@@ -1,16 +1,38 @@
-import { ReqoreSpinner } from '@qoretechnologies/reqore';
+import {
+  ReqoreColumn,
+  ReqoreColumns,
+  ReqoreContent,
+  ReqoreLayoutContent,
+  ReqoreSpinner,
+  ReqoreUIProvider,
+} from '@qoretechnologies/reqore';
 import { useMount } from 'react-use';
+import { API_URL } from '../../../server_config';
 import queryControl from '../../hocomponents/queryControl';
 
 const RegisterCodeView = ({ tokenQuery }) => {
   useMount(() => {
     const token = localStorage.getItem('token') || tokenQuery;
 
+    console.log(token);
+
     document.cookie = `Qorus-Auth-Context=${token}; path=/`;
-    window.location.href = `https://hq.qoretechnologies.com:8092/code/?token=${token}`;
+    window.location.href = `${API_URL}/code/?token=${token}`;
   });
 
-  return <ReqoreSpinner centered>Loading...</ReqoreSpinner>;
+  return (
+    <ReqoreUIProvider>
+      <ReqoreLayoutContent>
+        <ReqoreContent style={{ height: '100%' }}>
+          <ReqoreColumns style={{ gridAutoRows: '1fr', height: '100%' }}>
+            <ReqoreColumn justifyContent="center" alignItems="center" flexFlow="column">
+              <ReqoreSpinner centered>Loading...</ReqoreSpinner>
+            </ReqoreColumn>
+          </ReqoreColumns>
+        </ReqoreContent>
+      </ReqoreLayoutContent>
+    </ReqoreUIProvider>
+  );
 };
 
 export default queryControl('token')(RegisterCodeView);
